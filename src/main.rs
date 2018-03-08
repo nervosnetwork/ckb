5a38e7ea19cb0a787392941759e962dcd52eb731
zhangsoledad <787953403@qq.com>
2018-03-08 11:38:05 +0800

    feat: Introduce subcommand in cli

--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,7 @@ extern crate toml;
 
 mod config;
 mod adapter;
-
-use adapter::{ChainToNetAndPoolAdapter, NetToChainAndPoolAdapter};
-use bigint::H256;
-use chain::chain::Chain;
-use chain::store::ChainKVStore;
-use config::Config;
-use db::kvdb::MemoryKeyValueDB;
-use miner::miner::Miner;
-use network::Network;
-use pool::TransactionPool;
-use std::sync::Arc;
-use std::thread;
-use util::{Condvar, Mutex};
+mod cli;
 
 fn main() {
     // Always print backtrace on panic.
@@ -42,74 +30,21 @@ fn main() {
         (version: "0.1")
         (author: "Nervos <dev@nervos.org>")
         (about: "Nervos")
-        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
+        (@subcommand run =>
+            (about: "run nervos")
+            (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
+        )
+        (@subcommand new =>
+            (about: "new nervos config")
+        )
     ).get_matches();
 
-    let config_path = matches.value_of("config").unwrap_or("default.toml");
-    let config = Config::load(config_path);
-
-    logger::init(config.logger_config()).expect("Init Logger");
-
-    info!(target: "main", "Value for config: {:?}", config);
-
-    let db = MemoryKeyValueDB::default();
-    let store = ChainKVStore { db: Box::new(db) };
-
-    let tx_pool = Arc::new(TransactionPool::default());
-
-    let chain_adapter = Arc::new(ChainToNetAndPoolAdapter::new(tx_pool.clone()));
-    let chain = Arc::new(
-        Chain::init(
-            Arc::new(store),
-            chain_adapter.clone(),
-            &chain::genesis::genesis_dev(),
-        ).unwrap(),
-    );
-
-    let kg = Arc::new(config.key_group());
-
-    let net_adapter = NetToChainAndPoolAdapter::new(kg, chain.clone(), tx_pool.clone());
-
-    let network = Arc::new(Network::init(net_adapter, config.network).unwrap());
-
-    chain_adapter.init(network.clone());
-
-    let miner = Miner {
-        chain: chain,
-        tx_pool: tx_pool,
-        miner_key: config.miner_private_key,
-        signer_key: config.signer_private_key,
-    };
-
-    let _ = thread::Builder::new()
-        .name("network".to_string())
-        .spawn(move || {
-            network.start();
-        });
-
-    let _ = thread::Builder::new()
-        .name("miner".to_string())
-        .spawn(move || {
-            miner.run_loop();
-        });
-
-    wait_for_exit();
-
-    info!(target: "main", "Finishing work, please wait...");
-
-    logger::flush();
-}
-
-pub fn wait_for_exit() {
-    let exit = Arc::new((Mutex::new(()), Condvar::new()));
-
-    // Handle possible exits
-    let e = exit.clone();
-    let _ = ctrlc::set_handler(move || {
-        e.1.notify_all();
-    });
-
-    // Wait for signal
-    let mut l = exit.0.lock();
-    exit.1.wait(&mut l);
+    match matches.subcommand() {
+        ("run", Some(run_cmd)) => {
+            let config_path = run_cmd.value_of("config").unwrap_or("default.toml");
+            cli::run(config_path);
+        }
+        ("new", Some(_new_cmd)) => {}
+        _ => {}
+    }
 }
