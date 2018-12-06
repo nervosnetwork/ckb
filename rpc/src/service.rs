7878d1605413bd922c2b2fd8e55c7422bea1dc4e
ian <me@iany.me>
2018-12-06 22:03:37 +0800

    Update service.rs

--- rpc/src/service.rs
+++ rpc/src/service.rs
@@ -38,8 +38,8 @@ pub struct RpcReceivers {
     get_block_template_receiver: Receiver<Request<BlockTemplateArgs, BlockTemplateReturn>>,
 }
 
-// TODO: MinerService should be dependent on RpcService
-// To do this, we need to add the following api:
+// TODO: MinerService should depend on RpcService.
+// To do this, we need to add the following apis:
 //   * get_block
 //   * get_block_hash
 //   * get_transaction
