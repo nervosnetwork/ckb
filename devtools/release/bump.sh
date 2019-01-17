a503f97a50bd8d3554af8a7e77e4e1d4f3986076
ian <ian@nervos.org>
2019-01-17 11:55:08 +0800

    chore: make bump.sh compatible for Linux
    Change `-i .bak` to `-i.bak`, which works for both BSD and Linux.

--- devtools/release/bump.sh
+++ devtools/release/bump.sh
@@ -10,7 +10,7 @@ main() {
     exit 1
   fi
   local v="$1"
-  find . -name 'Cargo.toml' -print0 | xargs -0 sed -i .bak 's/^version = .*/version = "'"$v"'"/'
+  find . -name 'Cargo.toml' -print0 | xargs -0 sed -i.bak 's/^version = .*/version = "'"$v"'"/'
   find . -name 'Cargo.toml.bak' -exec rm -f {} \;
   cargo check
 }
