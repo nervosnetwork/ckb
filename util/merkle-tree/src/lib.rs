f48d872a09e0895a6b8d45c6a06cb27403d996d4
Ke Wang <github@bll.io>
2019-01-24 15:40:08 +0800

    use new merkle tree

--- util/merkle-tree/src/lib.rs
+++ util/merkle-tree/src/lib.rs
@@ -1,7 +1,16 @@
-mod hash;
-mod proof;
-mod tree;
+use merkle_tree::{new_cbmt, MerkleProof, MerkleTree, H256};
 
-pub use crate::hash::Merge;
-pub use crate::proof::Proof;
-pub use crate::tree::Tree;
+pub fn merkle_root(leaves: &[H256]) -> H256 {
+    let cbmt = new_cbmt::<H256>();
+    cbmt.build_merkle_root(leaves)
+}
+
+pub fn build_merkle_tree(leaves: &[H256]) -> MerkleTree<H256> {
+    let cbmt = new_cbmt::<H256>();
+    cbmt.build_merkle_tree(leaves)
+}
+
+pub fn build_merkle_proof(leaves: &[H256], indices: Vec<usize>) -> Option<MerkleProof<H256>> {
+    let cbmt = new_cbmt::<H256>();
+    cbmt.build_merkle_proof(leaves, indices)
+}
