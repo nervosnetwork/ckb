9fa25d4afd63768d91511edd4f076a4fa9ee3906
quake wang <quake.wang@gmail.com>
2019-03-15 14:26:55 +0900

    fix: use reference script in integration test

--- test/src/node.rs
+++ test/src/node.rs
@@ -175,16 +175,27 @@ impl Node {
     }
 
     pub fn new_transaction(&self, hash: H256) -> Transaction {
-        let mut file = File::open(
-            PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-                .join("fixtures/nodes_template/spec/cells/always_success"),
-        )
-        .expect("faild to load script");
-        let mut buffer = Vec::new();
-        file.read_to_end(&mut buffer).unwrap();
-        let script = Script::new(0, Vec::new(), None, Some(buffer), Vec::new());
+        // OutPoint and Script reference hash values are from spec#always_success_type_hash test
+        let out_point = OutPoint::new(
+            H256::from_hex_str("06d185ca44a1426b01d8809738c84259b86dc33bfe99f271938432a9de4cc3aa")
+                .unwrap(),
+            0,
+        );
+        let script = Script::new(
+            0,
+            vec![],
+            Some(
+                H256::from_hex_str(
+                    "61d7e01908bafa29d742e37b470dc906fb05c2115b0beba7b1c4fa3e66ca3e44",
+                )
+                .unwrap(),
+            ),
+            None,
+            vec![],
+        );
 
         TransactionBuilder::default()
+            .dep(out_point)
             .output(CellOutput::new(50000, vec![], script.type_hash(), None))
             .input(CellInput::new(OutPoint::new(hash, 0), script))
             .build()
