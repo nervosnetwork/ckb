2ebc3fe98ba73da002624061a4edbe9d9790c5cc
Linfeng Qian <thewawar@gmail.com>
2019-04-11 12:45:50 +0800

    chore: Reduce log level

--- network/src/protocols/mod.rs
+++ network/src/protocols/mod.rs
@@ -358,7 +358,10 @@ impl CKBProtocolContext for DefaultCKBProtocolContext {
             .ok_or_else(|| PeerError::NotFound(peer_id.to_owned()))
             .and_then(|peer| {
                 peer.protocol_version(protocol_id)
-                    .ok_or_else(|| PeerError::ProtocolNotFound(peer_id.to_owned(), protocol_id))
+                    .ok_or_else(|| {
+                        warn!(target: "network", "can not get protocol version: {:?}", peer);
+                        PeerError::ProtocolNotFound(peer_id.to_owned(), protocol_id)
+                    })
                     .map(|_| peer.session_id)
             })?;
 
