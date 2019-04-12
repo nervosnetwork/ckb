0800b17079a2c93ac09d7d3b9e3fbae8ed92c933
ian <ian@nervos.org>
2019-04-12 22:09:30 +0800

    chore: setup integration test in ci
    - Use Travis Branch Trigger to run integration test.
    - Use Travis Pull Request Trigger to run unit test.
    - Run both tests in master branch.

--- devtools/ci/script.sh
+++ devtools/ci/script.sh
@@ -3,21 +3,36 @@ set -ev
 
 cargo sweep -s
 
-if [ "$FMT" = true ]; then
-  make fmt
-fi
-if [ "$CHECK" = true ]; then
-  make check
-  make clippy
-fi
-if [ "$TEST" = true ]; then
-  make test
+# Run test only in master branch and pull requests
+RUN_TEST=false
+# Run integration only in master, develop and rc branches
+RUN_INTEGRATION=false
+if [ "$TRAVIS_PULL_REQUEST" != false ]; then
+  RUN_TEST=true
+else
+  RUN_INTEGRATION=true
+  if [ "$TRAVIS_BRANCH" = master ]; then
+    RUN_TEST=true
+  fi
 fi
 
-git diff --exit-code Cargo.lock
+if [ "$RUN_TEST" = true ]; then
+  if [ "$FMT" = true ]; then
+    make fmt
+  fi
+  if [ "$CHECK" = true ]; then
+    make check
+    make clippy
+  fi
+  if [ "$TEST" = true ]; then
+    make test
+  fi
+
+  git diff --exit-code Cargo.lock
+fi
 
 # We'll create PR for develop and rc branches to trigger the integration test.
-if [ "$TRAVIS_BRANCH" = master ]; then
+if [ "$RUN_INTEGRATION" = true ]; then
   echo "Running integration test..."
   make integration
 
