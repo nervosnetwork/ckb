c1f2d27cd214f0e1e9d5ec4cf79ccb5cd8f3949c
Boyu Yang <yangby@cryptape.com>
2019-04-15 17:44:44 +0800

    chore: let all jsonrpc types be public

--- util/jsonrpc-types/src/lib.rs
+++ util/jsonrpc-types/src/lib.rs
@@ -6,12 +6,20 @@ mod net;
 mod proposal_short_id;
 mod trace;
 
+pub type BlockNumber = String;
+pub type Capacity = String;
+pub type Cycle = u64;
+
 pub use self::block_template::{
     BlockTemplate, CellbaseTemplate, TransactionTemplate, UncleTemplate,
 };
-pub use self::blockchain::{Block, Header, OutPoint, Transaction, UncleBlock};
+pub use self::blockchain::{
+    Block, CellInput, CellOutput, Header, OutPoint, Script, Seal, Transaction, UncleBlock, Witness,
+};
 pub use self::bytes::Bytes;
 pub use self::cell::{CellOutputWithOutPoint, CellWithStatus};
 pub use self::net::{Node, NodeAddress};
+pub use self::proposal_short_id::ProposalShortId;
 pub use self::trace::{Action, TxTrace};
+pub use ckb_core::Version;
 pub use jsonrpc_core::types::{error, id, params, request, response, version};
