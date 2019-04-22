deee2c2266d2d62916643deb7f1856f4a0e26ad5
Boyu Yang <yangby@cryptape.com>
2019-04-23 01:33:14 +0800

    ci: test if only `Capacity` was changed

--- util/occupied-capacity/core/src/units.rs
+++ util/occupied-capacity/core/src/units.rs
@@ -1,12 +1,94 @@
-use serde_derive::{Deserialize, Serialize};
+use serde;
 
 use crate::OccupiedCapacity;
 
 // The inner is the amount of `Shannons`.
-#[derive(
-    Debug, Clone, Copy, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize,
-)]
+#[derive(Debug, Clone, Copy, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Capacity(u64);
+
+impl serde::Serialize for Capacity {
+    fn serialize<S>(&self, serializer: S) -> ::std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_newtype_struct("Capacity", &(self.0 / 100_000_000))
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Capacity {
+    fn deserialize<D>(deserializer: D) -> ::std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_u64(CapacityVisitor)
+    }
+}
+
+struct CapacityVisitor;
+
+impl<'de> serde::de::Visitor<'de> for CapacityVisitor {
+    type Value = Capacity;
+
+    fn expecting(&self, formatter: &mut ::std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("an integer between 0 and 2^64")
+    }
+
+    fn visit_u8<E>(self, value: u8) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity(u64::from(value) * 100_000_000))
+    }
+
+    fn visit_u16<E>(self, value: u16) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity(u64::from(value) * 100_000_000))
+    }
+
+    fn visit_u32<E>(self, value: u32) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity(u64::from(value) * 100_000_000))
+    }
+
+    fn visit_u64<E>(self, value: u64) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity(value * 100_000_000))
+    }
+
+    fn visit_i8<E>(self, value: i8) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity((value as u64) * 100_000_000))
+    }
+
+    fn visit_i16<E>(self, value: i16) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity((value as u64) * 100_000_000))
+    }
+
+    fn visit_i32<E>(self, value: i32) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity((value as u64) * 100_000_000))
+    }
+
+    fn visit_i64<E>(self, value: i64) -> std::result::Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Capacity((value as u64) * 100_000_000))
+    }
+}
 
 // Be careful: if the inner type of `Capacity` was changed, update this!
 impl OccupiedCapacity for Capacity {
