1a6471032777a8eea4e18fd1c198ea4f318c98ed
jjy <jjyruby@gmail.com>
2019-04-30 14:31:37 +0800

    refactor: avoid locking on read

--- db/src/cachedb.rs
+++ db/src/cachedb.rs
@@ -5,7 +5,7 @@ use lru_cache::LruCache;
 use std::ops::Range;
 use std::sync::Arc;
 
-type CacheTable = FnvHashMap<Col, LruCache<Vec<u8>, Vec<u8>>>;
+type CacheTable = FnvHashMap<Col, Mutex<LruCache<Vec<u8>, Vec<u8>>>>;
 pub type CacheCols = (u32, usize);
 
 enum BatchOperation {
@@ -25,12 +25,12 @@ where
     T: DbBatch,
 {
     inner: T,
-    cache: Arc<Mutex<CacheTable>>,
+    cache: Arc<CacheTable>,
     operations: Vec<BatchOperation>,
 }
 
 impl<T: DbBatch> CacheDBBatch<T> {
-    fn new(inner: T, cache: Arc<Mutex<CacheTable>>) -> CacheDBBatch<T> {
+    fn new(inner: T, cache: Arc<CacheTable>) -> CacheDBBatch<T> {
         Self {
             inner,
             cache,
@@ -42,7 +42,7 @@ impl<T: DbBatch> CacheDBBatch<T> {
 impl<T: DbBatch> DbBatch for CacheDBBatch<T> {
     fn insert(&mut self, col: Col, key: &[u8], value: &[u8]) -> Result<()> {
         self.inner.insert(col, key, value)?;
-        if self.cache.lock().contains_key(&col) {
+        if self.cache.contains_key(&col) {
             self.operations.push(BatchOperation::Insert {
                 col,
                 key: key.to_vec(),
@@ -54,7 +54,7 @@ impl<T: DbBatch> DbBatch for CacheDBBatch<T> {
 
     fn delete(&mut self, col: Col, key: &[u8]) -> Result<()> {
         self.inner.delete(col, key)?;
-        if self.cache.lock().contains_key(&col) {
+        if self.cache.contains_key(&col) {
             self.operations.push(BatchOperation::Delete {
                 col,
                 key: key.to_vec(),
@@ -65,18 +65,19 @@ impl<T: DbBatch> DbBatch for CacheDBBatch<T> {
 
     fn commit(self) -> Result<()> {
         self.inner.commit()?;
-        let mut cache_guard = self.cache.lock();
         for op in self.operations {
             match op {
                 BatchOperation::Insert { col, key, value } => {
-                    cache_guard
-                        .get_mut(&col)
-                        .and_then(|cache| cache.insert(key, value));
+                    if let Some(cache) = self.cache.get(&col) {
+                        let mut cache_guard = cache.lock();
+                        cache_guard.insert(key, value);
+                    }
                 }
                 BatchOperation::Delete { col, key } => {
-                    cache_guard
-                        .get_mut(&col)
-                        .and_then(|cache| cache.remove(&key));
+                    if let Some(cache) = self.cache.get(&col) {
+                        let mut cache_guard = cache.lock();
+                        cache_guard.remove(&key);
+                    }
                 }
             }
         }
@@ -89,7 +90,7 @@ where
     T: KeyValueDB,
 {
     db: T,
-    cache: Arc<Mutex<CacheTable>>,
+    cache: Arc<CacheTable>,
 }
 
 impl<T> CacheDB<T>
@@ -99,11 +100,11 @@ where
     pub fn new(db: T, cols: &[CacheCols]) -> Self {
         let mut table = FnvHashMap::with_capacity_and_hasher(cols.len(), Default::default());
         for (idx, capacity) in cols {
-            table.insert(*idx, LruCache::new(*capacity));
+            table.insert(*idx, Mutex::new(LruCache::new(*capacity)));
         }
         CacheDB {
             db,
-            cache: Arc::new(Mutex::new(table)),
+            cache: Arc::new(table),
         }
     }
 }
@@ -115,11 +116,10 @@ where
     type Batch = CacheDBBatch<T::Batch>;
 
     fn read(&self, col: Col, key: &[u8]) -> Result<Option<Vec<u8>>> {
-        let mut cache_guard = self.cache.lock();
-        if let Some(value) = cache_guard
-            .get_mut(&col)
-            .and_then(|cache| cache.get_refresh(key))
-            .cloned()
+        if let Some(value) = self
+            .cache
+            .get(&col)
+            .and_then(|cache| cache.lock().get_refresh(key).cloned())
         {
             return Ok(Some(value));
         }
@@ -127,12 +127,11 @@ where
     }
 
     fn partial_read(&self, col: Col, key: &[u8], range: &Range<usize>) -> Result<Option<Vec<u8>>> {
-        let mut cache_guard = self.cache.lock();
-        if let Some(data) = cache_guard
-            .get_mut(&col)
-            .and_then(|cache| cache.get_refresh(key))
-        {
-            return Ok(data.get(range.start..range.end).map(|slice| slice.to_vec()));
+        if let Some(cache) = self.cache.get(&col) {
+            let mut cache_guard = cache.lock();
+            if let Some(data) = cache_guard.get_refresh(key) {
+                return Ok(data.get(range.start..range.end).map(|slice| slice.to_vec()));
+            }
         }
         self.db.partial_read(col, key, range)
     }
