c564027580ee6fa3c9479cd83fac885460ccd51f
zhangsoledad <787953403@qq.com>
2019-05-05 17:34:16 +0800

    fix: block assembler not respect cycles limit

--- miner/src/block_assembler.rs
+++ miner/src/block_assembler.rs
@@ -358,7 +358,7 @@ impl<CS: ChainStore + 'static> BlockAssembler<CS> {
 
         let proposals = chain_state.get_proposals(proposals_limit as usize);
         let txs_size_limit = self.calculate_txs_size_limit(bytes_limit, &uncles, &proposals);
-        let transactions = chain_state.get_staging_txs(txs_size_limit);
+        let transactions = chain_state.get_staging_txs(txs_size_limit, cycles_limit);
 
         // Release the lock as soon as possible, let other services do their work
         drop(chain_state);
