5e5fc813643edd8b6f5f5f112f628ee680be3871
James Chen <james@ashchan.com>
2019-05-07 17:20:32 +0900

    refactor: Use the String alias type EpochNumber

--- util/jsonrpc-types/src/blockchain.rs
+++ util/jsonrpc-types/src/blockchain.rs
@@ -494,7 +494,7 @@ impl TryFrom<Block> for CoreBlock {
 
 #[derive(Clone, Default, Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
 pub struct EpochExt {
-    pub number: String,
+    pub number: EpochNumber,
     pub block_reward: String,
     pub last_block_hash_in_previous_epoch: H256,
     pub start_number: BlockNumber,
