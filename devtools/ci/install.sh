8f6e9f873d946e232851465c43e6791c27423add
ian <ian@nervos.org>
2019-05-08 11:03:41 +0800

    chore: ci round robin

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -3,11 +3,10 @@ set -ev
 
 cargo sweep --version || cargo install --git https://github.com/holmgr/cargo-sweep --rev 4770deda37a2203c783e301b8c0c895964e8971e
 
-if [ "$FMT" = true ]; then
+SUB_JOB_NUMBER="${TRAVIS_JOB_NUMBER##*.}"
+# Run fmt and check evenly between osx and linux
+if (( TRAVIS_BUILD_NUMBER % 2 == SUB_JOB_NUMBER - 1 )); then
   cargo fmt --version || rustup component add rustfmt
-fi
-
-if [ "$CHECK" = true ]; then
   cargo clippy --version || rustup component add clippy
   cargo audit --version || cargo install cargo-audit
 fi
