168792ac3f3268bda64343f6a492287cb79503e6
keroro520 <keroroxx520@gmail.com>
2019-05-12 10:43:24 +0800

    test: complete rpc-client

--- test/src/rpc.rs
+++ test/src/rpc.rs
@@ -1,32 +1,42 @@
 use jsonrpc_client_core::{expand_params, jsonrpc_client};
 use jsonrpc_types::{
-    Block, BlockTemplate, BlockView, HeaderView, Node, Transaction, TransactionWithStatus,
-    TxPoolInfo,
+    Block, BlockNumber, BlockTemplate, BlockView, CellOutputWithOutPoint, CellWithStatus,
+    ChainInfo, DryRunResult, EpochExt, EpochNumber, HeaderView, Node, OutPoint, PeerState,
+    Transaction, TransactionWithStatus, TxPoolInfo, Unsigned, Version,
 };
 use numext_fixed_hash::H256;
 
 jsonrpc_client!(pub struct RpcClient {
+    pub fn get_block(&mut self, _hash: H256) -> RpcRequest<Option<BlockView>>;
+    pub fn get_block_by_number(&mut self, _number: BlockNumber) -> RpcRequest<Option<BlockView>>;
+    pub fn get_transaction(&mut self, _hash: H256) -> RpcRequest<Option<TransactionWithStatus>>;
+    pub fn get_block_hash(&mut self, _number: BlockNumber) -> RpcRequest<Option<H256>>;
+    pub fn get_tip_header(&mut self) -> RpcRequest<HeaderView>;
+    pub fn get_cells_by_lock_hash(
+        &mut self,
+        _lock_hash: H256,
+        _from: BlockNumber,
+        _to: BlockNumber
+    ) -> RpcRequest<Vec<CellOutputWithOutPoint>>;
+    pub fn get_live_cell(&mut self, _out_point: OutPoint) -> RpcRequest<CellWithStatus>;
+    pub fn get_tip_block_number(&mut self) -> RpcRequest<BlockNumber>;
+    pub fn get_current_epoch(&mut self) -> RpcRequest<EpochExt>;
+    pub fn get_epoch_by_number(&mut self, number: EpochNumber) -> RpcRequest<Option<EpochExt>>;
     pub fn local_node_info(&mut self) -> RpcRequest<Node>;
     pub fn get_peers(&mut self) -> RpcRequest<Vec<Node>>;
-
-    pub fn add_node(&mut self, peer_id: String, address: String) -> RpcRequest<()>;
-
     pub fn get_block_template(
         &mut self,
-        bytes_limit: Option<String>,
-        proposals_limit: Option<String>,
-        max_version: Option<u32>
+        bytes_limit: Option<Unsigned>,
+        proposals_limit: Option<Unsigned>,
+        max_version: Option<Version>
     ) -> RpcRequest<BlockTemplate>;
-
-    pub fn submit_block(&mut self, work_id: String, data: Block) -> RpcRequest<Option<H256>>;
-
+    pub fn submit_block(&mut self, _work_id: String, _data: Block) -> RpcRequest<Option<H256>>;
+    pub fn get_blockchain_info(&mut self) -> RpcRequest<ChainInfo>;
+    pub fn get_peers_state(&mut self) -> RpcRequest<Vec<PeerState>>;
+    pub fn compute_transaction_hash(&mut self, tx: Transaction) -> RpcRequest<H256>;
+    pub fn dry_run_transaction(&mut self, _tx: Transaction) -> RpcRequest<DryRunResult>;
     pub fn send_transaction(&mut self, tx: Transaction) -> RpcRequest<H256>;
     pub fn tx_pool_info(&mut self) -> RpcRequest<TxPoolInfo>;
-
-    pub fn get_block(&mut self, hash: H256) -> RpcRequest<Option<BlockView>>;
-    pub fn get_transaction(&mut self, hash: H256) -> RpcRequest<Option<TransactionWithStatus>>;
-    pub fn get_block_hash(&mut self, number: String) -> RpcRequest<Option<H256>>;
-    pub fn get_tip_header(&mut self) -> RpcRequest<HeaderView>;
-    pub fn get_tip_block_number(&mut self) -> RpcRequest<String>;
+    pub fn add_node(&mut self, peer_id: String, address: String) -> RpcRequest<()>;
     pub fn enqueue_test_transaction(&mut self, tx: Transaction) -> RpcRequest<H256>;
 });
