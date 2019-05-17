36f7b8505ef2a257d57e3b5d82ac78691099eec2
Quake Wang <quake.wang@gmail.com>
2019-05-17 16:18:16 +0800

    test: add integration for sync timeout (#813)

--- test/src/specs/sync/mod.rs
+++ test/src/specs/sync/mod.rs
@@ -1,7 +1,9 @@
 mod block_sync;
 mod chain_forks;
 mod invalid_locator_size;
+mod sync_timeout;
 
 pub use block_sync::BlockSyncBasic;
 pub use chain_forks::*;
 pub use invalid_locator_size::InvalidLocatorSize;
+pub use sync_timeout::SyncTimeout;
