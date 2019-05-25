8da78d89b0ebb1c999346e10073b38ad2afb2275
ian <ian@nervos.org>
2019-05-25 19:27:05 +0800

    chore: update ops scripts

--- devtools/ci/centos-docker.sh
+++ devtools/ci/centos-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = run ]; then
-  docker run --rm -it -v $(pwd):/ckb centos:7 bash /ckb/devtools/ci/centos-docker.sh
+  docker run --rm -it -v $(pwd):/ckb nervos/ckb-docker-builder:centos-7-rust-1.34.2 bash /ckb/devtools/ci/centos-docker.sh
   devtools/ci/package.sh target/release/ckb
   exit 0
 fi
@@ -10,9 +10,4 @@ cd /ckb
 
 set -eu
 
-yum install -y centos-release-scl
-yum install -y git curl make gcc-c++ openssl-devel llvm-toolset-7
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain 1.34.2 -y
-
-source $HOME/.cargo/env
 scl enable llvm-toolset-7 'make prod'
