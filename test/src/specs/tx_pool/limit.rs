fd11082cffc94072c81c53761bd7c40cd22c6d9a
Xuejie Xiao <xxuejie@gmail.com>
2019-05-29 02:44:30 +0000

    fix: integration tests

--- test/src/specs/tx_pool/limit.rs
+++ test/src/specs/tx_pool/limit.rs
@@ -32,9 +32,9 @@ impl Spec for SizeLimit {
             .unwrap_err();
         assert_regex_match(&error.to_string(), r"LimitReached");
 
-        // 124 * 5
+        // 132 * 5
         // 12 * 5
-        node.assert_tx_pool_statics(620, 60);
+        node.assert_tx_pool_statics(660, 60);
         (0..DEFAULT_TX_PROPOSAL_WINDOW.0).for_each(|_| {
             node.generate_block();
         });
@@ -48,7 +48,7 @@ impl Spec for SizeLimit {
 
     fn modify_ckb_config(&self) -> Box<dyn Fn(&mut CKBAppConfig) -> ()> {
         Box::new(|config| {
-            config.tx_pool.max_mem_size = 620;
+            config.tx_pool.max_mem_size = 660;
             config.tx_pool.max_cycles = 200_000_000_000;
         })
     }
@@ -84,9 +84,9 @@ impl Spec for CyclesLimit {
             .unwrap_err();
         assert_regex_match(&error.to_string(), r"LimitReached");
 
-        // 124 * 5
+        // 132 * 5
         // 12 * 5
-        node.assert_tx_pool_statics(620, 60);
+        node.assert_tx_pool_statics(660, 60);
         (0..DEFAULT_TX_PROPOSAL_WINDOW.0).for_each(|_| {
             node.generate_block();
         });
