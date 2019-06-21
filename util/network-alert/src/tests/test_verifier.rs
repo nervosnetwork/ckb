f5b5c0770302b7e1826d5198b98e6e736dda4afe
jjy <jjyruby@gmail.com>
2019-06-21 11:25:16 +0800

    test: util/network-alert

---
+++ util/network-alert/src/tests/test_verifier.rs
@@ -0,0 +1,33 @@
+use crate::config::Config;
+use crate::verifier::Verifier;
+use ckb_core::alert::AlertBuilder;
+use ckb_crypto::secp::Generator;
+use ckb_jsonrpc_types::JsonBytes;
+
+#[test]
+fn test_veirifer() {
+    let keypairs: Vec<_> = (0..3)
+        .map(move |_| Generator::new().random_keypair())
+        .collect::<Result<Vec<_>, _>>()
+        .expect("random keypair");
+    let config = Config {
+        signatures_threshold: 2,
+        public_keys: keypairs
+            .iter()
+            .map(|(_, pubkey)| JsonBytes::from_vec(pubkey.serialize()))
+            .collect(),
+    };
+    let verifier = Verifier::new(config);
+    let mut alert = AlertBuilder::default().id(1).build();
+    let hash = alert.hash();
+    let signatures = keypairs
+        .iter()
+        .map(|(privkey, _)| privkey.sign_recoverable(&hash))
+        .collect::<Result<Vec<_>, _>>()
+        .expect("sign alert");
+    alert.signatures = signatures
+        .into_iter()
+        .map(|s| s.serialize().into())
+        .collect();
+    assert!(verifier.verify_signatures(&alert).is_ok());
+}
