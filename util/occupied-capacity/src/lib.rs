0872859fe04e836f8154edc0b6145186f7450ff1
u2 <zhangyaning1985@gmail.com>
2019-06-27 17:35:56 +0800

    test: add RewardCalculator test

--- util/occupied-capacity/src/lib.rs
+++ util/occupied-capacity/src/lib.rs
@@ -2,7 +2,7 @@
 
 use proc_macro_hack::proc_macro_hack;
 
-pub use ckb_occupied_capacity_core::{Capacity, Error, Ratio, Result};
+pub use ckb_occupied_capacity_core::{AsCapacity, Capacity, Error, Ratio, Result};
 
 #[proc_macro_hack]
 pub use ckb_occupied_capacity_macros::capacity_bytes;
