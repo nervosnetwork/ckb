9bf0f65db92e0e7d170c26c04bb8667b67f37f6b
ian <ian@nervos.org>
2019-07-01 20:11:57 +0800

    chore: package different version ckb-cli

--- devtools/ci/package.sh
+++ devtools/ci/package.sh
@@ -2,6 +2,7 @@
 set -eu
 
 TRAVIS_TAG="${TRAVIS_TAG:-"$(git describe)"}"
+CKB_CLI_VERSION="${CKB_CLI_VERSION:-"$TRAVIS_TAG"}"
 if [ -z "${REL_PKG:-}" ]; then
   if [ "$(uname)" = Darwin ]; then
     REL_PKG=x86_64-apple-darwin.zip
@@ -23,13 +24,13 @@ cp -R devtools/init "releases/$PKG_NAME"
 cp -R docs "releases/$PKG_NAME"
 cp rpc/README.md "releases/$PKG_NAME/docs/rpc.md"
 
-curl -LO "https://github.com/nervosnetwork/ckb-cli/releases/download/${TRAVIS_TAG}/ckb-cli_${TRAVIS_TAG}_${REL_PKG}"
+curl -LO "https://github.com/nervosnetwork/ckb-cli/releases/download/${CKB_CLI_VERSION}/ckb-cli_${CKB_CLI_VERSION}_${REL_PKG}"
 if [ "${REL_PKG##*.}" = "zip" ]; then
-  unzip "ckb-cli_${TRAVIS_TAG}_${REL_PKG}"
+  unzip "ckb-cli_${CKB_CLI_VERSION}_${REL_PKG}"
 else
-  tar -xzf "ckb-cli_${TRAVIS_TAG}_${REL_PKG}"
+  tar -xzf "ckb-cli_${CKB_CLI_VERSION}_${REL_PKG}"
 fi
-mv "ckb-cli_${TRAVIS_TAG}_${REL_PKG%%.*}/ckb-cli" "releases/$PKG_NAME/ckb-cli"
+mv "ckb-cli_${CKB_CLI_VERSION}_${REL_PKG%%.*}/ckb-cli" "releases/$PKG_NAME/ckb-cli"
 
 pushd releases
 if [ "${REL_PKG#*.}" = "tar.gz" ]; then
