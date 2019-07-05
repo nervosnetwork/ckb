91e4e971e12aee4eb87d2cb195895d471094eea9
zhangsoledad <787953403@qq.com>
2019-07-05 10:48:09 +0800

    fix: peer best header

--- sync/src/synchronizer/headers_process.rs
+++ sync/src/synchronizer/headers_process.rs
@@ -169,13 +169,7 @@ where
             return Ok(());
         }
 
-        let shared_best_known = self.synchronizer.shared.shared_best_header();
         if headers.len() == 0 {
-            // Update peer's best known header
-            self.synchronizer
-                .peers()
-                .set_best_known_header(self.peer, shared_best_known);
-
             // Reset headers sync timeout
             self.synchronizer
                 .peers()
@@ -239,6 +233,7 @@ where
             // Regain the updated best known
             let shared_best_known = self.synchronizer.shared.shared_best_header();
             let chain_state = self.synchronizer.shared.lock_chain_state();
+            let shared_best_known = self.synchronizer.shared.shared_best_header();
             let peer_best_known = self.synchronizer.peers().get_best_known_header(self.peer);
             debug!(
                 "chain: num={}, diff={:#x};",
