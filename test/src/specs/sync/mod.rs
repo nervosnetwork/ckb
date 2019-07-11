8952c90b3b061a1e05e6232963630ede285bbc23
keroro520 <keroroxx520@gmail.com>
2019-07-11 20:29:38 +0800

    test: sync fork contains invalid block

--- test/src/specs/sync/mod.rs
+++ test/src/specs/sync/mod.rs
@@ -1,9 +1,11 @@
 mod block_sync;
 mod chain_forks;
+mod invalid_block;
 mod invalid_locator_size;
 mod sync_timeout;
 
 pub use block_sync::*;
 pub use chain_forks::*;
+pub use invalid_block::{ChainContainsInvalidBlock, ForkContainsInvalidBlock};
 pub use invalid_locator_size::InvalidLocatorSize;
 pub use sync_timeout::SyncTimeout;
