ed2df24790086b6b5ceda8e71b2aa1db4e0adfae
caojiafeng <funfriendcjf@gmail.com>
2019-07-14 19:15:19 +0800

    fix(relayer): should check tx from tx_pool when the short_id set is not empty

--- sync/src/relayer/mod.rs
+++ sync/src/relayer/mod.rs
@@ -267,7 +267,7 @@ impl<CS: ChainStore + 'static> Relayer<CS> {
             })
             .collect();
 
-        if short_ids_set.is_empty() {
+        if !short_ids_set.is_empty() {
             let tx_pool = chain_state.tx_pool();
             for entry in tx_pool.proposed_txs_iter() {
                 let short_id = short_transaction_id(key0, key1, &entry.transaction.witness_hash());
