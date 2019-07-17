c7d3136a8decae912df2a75d7671ad916ab3ded9
keroro520 <keroroxx520@gmail.com>
2019-07-17 18:16:48 +0800

    chore: Make BlockStatus more readable

--- sync/src/block_status.rs
+++ sync/src/block_status.rs
@@ -4,12 +4,12 @@ bitflags! {
     pub struct BlockStatus: u32 {
         const UNKNOWN                 =     0;
 
-        const HEADER_VALID            =     0b0000_0000_0000_0001;
-        const BLOCK_RECEIVED          =     Self::HEADER_VALID.bits | 0b0000_0000_0000_0010;
-        const BLOCK_STORED            =     Self::HEADER_VALID.bits | Self::BLOCK_RECEIVED.bits | 0b0000_0000_0000_1000;
-        const BLOCK_VALID             =     Self::HEADER_VALID.bits | Self::BLOCK_RECEIVED.bits | Self::BLOCK_STORED.bits | 0b0000_0000_0001_0000;
+        const HEADER_VALID            =     1;
+        const BLOCK_RECEIVED          =     Self::HEADER_VALID.bits | 1 << 1;
+        const BLOCK_STORED            =     Self::HEADER_VALID.bits | Self::BLOCK_RECEIVED.bits | 1 << 3;
+        const BLOCK_VALID             =     Self::HEADER_VALID.bits | Self::BLOCK_RECEIVED.bits | Self::BLOCK_STORED.bits | 1 << 4;
 
-        const BLOCK_INVALID           =     0b0010_0000_0000_0000;
+        const BLOCK_INVALID           =     1 << 12;
     }
 }
 
