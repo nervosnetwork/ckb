bcd39022a44efed8c0c4ee6d9dc89731f792fd39
driftluo <driftluo@foxmail.com>
2019-07-25 18:49:23 +0800

    test: add test for in ibd message

--- test/src/specs/sync/mod.rs
+++ test/src/specs/sync/mod.rs
@@ -1,11 +1,13 @@
 mod block_sync;
 mod chain_forks;
+mod ibd_process;
 mod invalid_block;
 mod invalid_locator_size;
 mod sync_timeout;
 
 pub use block_sync::*;
 pub use chain_forks::*;
+pub use ibd_process::IBDProcess;
 pub use invalid_block::{ChainContainsInvalidBlock, ForkContainsInvalidBlock};
 pub use invalid_locator_size::InvalidLocatorSize;
 pub use sync_timeout::SyncTimeout;
