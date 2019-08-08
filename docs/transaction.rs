3466494fbf670f40278f1eec8d095b7f61a87a80
ian <ian@nervos.org>
2019-08-08 15:38:01 +0800

    propose the tx structure changes for discussion

---
+++ docs/transaction.rs
@@ -0,0 +1,41 @@
+pub struct Transaction {
+    version: Version,
+    cell_deps: Vec<OutPoint>,
+    header_deps: Vec<H256>,
+    inputs: Vec<CellInput>,
+    outputs: Vec<CellOutput>,
+    witnesses: Vec<Vec<Bytes>>,
+}
+
+pub struct OutPoint {
+    tx_hash: H256,
+    index: u32,
+}
+
+pub enum ScriptHashType {
+    Data = 0,
+    Type = 1,
+}
+
+pub struct Script {
+    args: Vec<Bytes>,
+    code_hash: H256,
+    hash_type: ScriptHashType,
+}
+
+pub struct CellInput {
+    previous_output: OutPoint,
+    lock_script: Script,
+    since: u64,
+}
+
+pub struct CellOutput {
+    properties: CellProperties,
+    data: H256,
+}
+
+pub struct CellProperties {
+    capacity: Capacity,
+    lock_hash: H256,
+    type_script: Option<Script>,
+}
