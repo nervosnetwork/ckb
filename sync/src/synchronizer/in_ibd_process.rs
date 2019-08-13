fa396ee88f1fdff0d9807de33def154735ec6b1b
keroro520 <keroroxx520@gmail.com>
2019-08-13 18:56:25 +0800

    feat: Expose overflow of n_sync_started

--- sync/src/synchronizer/in_ibd_process.rs
+++ sync/src/synchronizer/in_ibd_process.rs
@@ -1,11 +1,8 @@
 use crate::synchronizer::Synchronizer;
-use crate::PROTECT_STOP_SYNC_TIME;
 use ckb_logger::{debug, info};
 use ckb_network::{CKBProtocolContext, PeerIndex};
 use ckb_protocol::InIBD;
 use failure::Error as FailureError;
-use faketime::unix_time_as_millis;
-use std::sync::atomic::Ordering;
 
 pub struct InIBDProcess<'a> {
     _message: &'a InIBD<'a>,
@@ -47,27 +44,18 @@ impl<'a> InIBDProcess<'a> {
                 return Ok(());
             }
 
-            let now = unix_time_as_millis();
             // The node itself needs to ensure the validity of the outbound connection.
             //
             // If outbound is a ibd node(non-whitelist, non-protect), it should be disconnected automatically.
             // If inbound is a ibd node, just mark the node does not pass header sync authentication.
             if state.peer_flags.is_outbound {
                 if state.peer_flags.is_whitelist || state.peer_flags.is_protect {
-                    state.stop_sync(now + PROTECT_STOP_SYNC_TIME);
-                    self.synchronizer
-                        .shared()
-                        .n_sync_started()
-                        .fetch_sub(1, Ordering::Release);
+                    self.synchronizer.shared().suspend_sync(state);
                 } else if let Err(err) = self.nc.disconnect(self.peer, "outbound in ibd") {
                     debug!("synchronizer disconnect error: {:?}", err);
                 }
             } else {
-                state.stop_sync(now + PROTECT_STOP_SYNC_TIME);
-                self.synchronizer
-                    .shared()
-                    .n_sync_started()
-                    .fetch_sub(1, Ordering::Release);
+                self.synchronizer.shared().suspend_sync(state);
             }
         }
         Ok(())
