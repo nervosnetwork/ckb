dbadf484cea6bdba0329d58102726068be997a50
Linfeng Qian <thewawar@gmail.com>
2019-08-20 23:41:20 +0800

    test: Add integration test for secp dep group

--- test/src/specs/tx_pool/mod.rs
+++ test/src/specs/tx_pool/mod.rs
@@ -5,6 +5,7 @@ mod different_txs_with_same_input;
 mod limit;
 mod pool_reconcile;
 mod pool_resurrect;
+mod send_secp_tx;
 mod valid_since;
 
 pub use cellbase_maturity::CellbaseMaturity;
@@ -17,4 +18,5 @@ pub use different_txs_with_same_input::DifferentTxsWithSameInput;
 pub use limit::{CyclesLimit, SizeLimit};
 pub use pool_reconcile::PoolReconcile;
 pub use pool_resurrect::PoolResurrect;
+pub use send_secp_tx::SendSecpTxUseDepGroup;
 pub use valid_since::ValidSince;
---
+++ test/src/specs/tx_pool/send_secp_tx.rs
@@ -0,0 +1,86 @@
+use crate::{Net, Spec};
+use ckb_app_config::CKBAppConfig;
+use ckb_crypto::secp::{Generator, Privkey};
+use ckb_hash::blake2b_256;
+use ckb_jsonrpc_types::JsonBytes;
+use ckb_miner::BlockAssemblerConfig;
+use ckb_resource::CODE_HASH_SECP256K1_BLAKE160_SIGHASH_ALL;
+use ckb_types::{
+    bytes::Bytes,
+    core::{capacity_bytes, Capacity, ScriptHashType, TransactionBuilder},
+    packed::{CellDep, CellInput, CellOutput, OutPoint},
+    prelude::*,
+    H256,
+};
+use log::info;
+
+pub struct SendSecpTxUseDepGroup {
+    privkey: Privkey,
+}
+
+impl Default for SendSecpTxUseDepGroup {
+    fn default() -> Self {
+        let privkey = Generator::new().random_privkey();
+        SendSecpTxUseDepGroup { privkey }
+    }
+}
+
+impl Spec for SendSecpTxUseDepGroup {
+    crate::name!("send_secp_tx_use_dep_group");
+
+    fn run(&self, net: Net) {
+        let node = &net.nodes[0];
+
+        info!("Generate 20 block on node");
+        node.generate_blocks(20);
+
+        let secp_out_point = OutPoint::new(node.dep_group_tx_hash().clone(), 0);
+        let block = node.get_tip_block();
+        let cellbase_hash: H256 = block.transactions()[0].hash().to_owned().unpack();
+
+        let cell_dep = CellDep::new(secp_out_point, true);
+        let output = CellOutput::new_builder()
+            .capacity(capacity_bytes!(100).pack())
+            .lock(node.always_success_script())
+            .build();
+        let input = CellInput::new(OutPoint::new(cellbase_hash, 0), 0);
+        let tx = TransactionBuilder::default()
+            .cell_dep(cell_dep.clone())
+            .input(input.clone())
+            .output(output.clone())
+            .output_data(Default::default())
+            .build();
+
+        let tx_hash: H256 = tx.hash().unpack();
+        let message = H256::from(blake2b_256(&tx_hash));
+        let sig = self.privkey.sign_recoverable(&message).expect("sign");
+        let witness = vec![Bytes::from(sig.serialize()).pack()].pack();
+        let tx = TransactionBuilder::default()
+            .cell_dep(cell_dep)
+            .input(input)
+            .output(output)
+            .output_data(Default::default())
+            .witness(witness)
+            .build();
+        info!("Send 1 secp tx use dep group");
+        node.rpc_client().send_transaction(tx.data().into());
+    }
+
+    fn modify_ckb_config(&self) -> Box<dyn Fn(&mut CKBAppConfig) -> ()> {
+        let pubkey_data = self
+            .privkey
+            .pubkey()
+            .expect("Get pubkey failed")
+            .serialize();
+        let lock_arg = Bytes::from(&blake2b_256(&pubkey_data)[0..20]);
+        Box::new(move |config| {
+            let block_assembler = BlockAssemblerConfig {
+                code_hash: CODE_HASH_SECP256K1_BLAKE160_SIGHASH_ALL.clone(),
+                hash_type: ScriptHashType::Data.into(),
+                args: vec![JsonBytes::from_bytes(lock_arg.clone())],
+                data: Default::default(),
+            };
+            config.block_assembler = Some(block_assembler);
+        })
+    }
+}
