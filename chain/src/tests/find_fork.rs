211ebdd0cb8290d45e2d6be3fa13c22e0fda595f
zhangsoledad <787953403@qq.com>
2019-08-26 16:13:22 +0800

    fix: uncle header shouldn't be deleted when block detach from mainchain

--- chain/src/tests/find_fork.rs
+++ chain/src/tests/find_fork.rs
@@ -6,7 +6,8 @@ use ckb_shared::shared::SharedBuilder;
 use ckb_store::ChainStore;
 use ckb_traits::ChainProvider;
 use ckb_types::{
-    core::{BlockExt, BlockView},
+    core::{BlockBuilder, BlockExt, BlockView},
+    prelude::Pack,
     U256,
 };
 use faketime::unix_time_as_millis;
@@ -305,4 +306,107 @@ fn test_find_fork_case4() {
         attached_blocks,
         HashSet::from_iter(fork.attached_blocks.iter().cloned())
     );
+}
+
+// this case is create for issuse from https://github.com/nervosnetwork/ckb/pull/1470
+#[test]
+fn repeatedly_switch_fork() {
+    ckb_store::set_cache_enable(false);
+    let (shared, _) = SharedBuilder::default()
+        .consensus(Consensus::default())
+        .build()
+        .unwrap();
+    let genesis = shared
+        .store()
+        .get_block_header(&shared.store().get_block_hash(0).unwrap())
+        .unwrap();
+    let mock_store = MockStore::new(&genesis, shared.store());
+    let mut fork1 = MockChain::new(genesis.clone(), shared.consensus());
+    let mut fork2 = MockChain::new(genesis.clone(), shared.consensus());
+
+    let notify = NotifyService::default().start::<&str>(None);
+    let (shared, table) = SharedBuilder::default()
+        .consensus(Consensus::default())
+        .build()
+        .unwrap();
+    let mut chain_service = ChainService::new(shared, table, notify);
+
+    for _ in 0..2 {
+        fork1.gen_empty_block_with_nonce(1u64, &mock_store);
+    }
+
+    for _ in 0..2 {
+        fork2.gen_empty_block_with_nonce(2u64, &mock_store);
+    }
+
+    for blk in fork1.blocks() {
+        chain_service
+            .process_block(Arc::new(blk.clone()), false)
+            .unwrap();
+    }
+
+    for blk in fork2.blocks() {
+        chain_service
+            .process_block(Arc::new(blk.clone()), false)
+            .unwrap();
+    }
+
+    //switch fork1
+    let uncle = fork2.blocks().last().cloned().unwrap().as_uncle();
+    let parent = fork1.blocks().last().cloned().unwrap();
+    let new_block1 = BlockBuilder::default()
+        .parent_hash(parent.hash().to_owned())
+        .number((parent.number() + 1).pack())
+        .difficulty(parent.difficulty().pack())
+        .nonce(1u64.pack())
+        .uncle(uncle)
+        .build();
+    chain_service
+        .process_block(Arc::new(new_block1.clone()), false)
+        .unwrap();
+
+    //switch fork2
+    let mut parent = fork2.blocks().last().cloned().unwrap();
+    let new_block2 = BlockBuilder::default()
+        .parent_hash(parent.hash().to_owned())
+        .number((parent.number() + 1).pack())
+        .difficulty(parent.difficulty().pack())
+        .nonce(2u64.pack())
+        .build();
+    parent = new_block2.clone();
+    chain_service
+        .process_block(Arc::new(new_block2), false)
+        .unwrap();
+    let new_block3 = BlockBuilder::default()
+        .parent_hash(parent.hash().to_owned())
+        .number((parent.number() + 1).pack())
+        .difficulty(parent.difficulty().pack())
+        .nonce(2u64.pack())
+        .build();
+    chain_service
+        .process_block(Arc::new(new_block3), false)
+        .unwrap();
+
+    //switch fork1
+    parent = new_block1.clone();
+    let new_block4 = BlockBuilder::default()
+        .parent_hash(parent.hash().to_owned())
+        .number((parent.number() + 1).pack())
+        .difficulty(parent.difficulty().pack())
+        .nonce(1u64.pack())
+        .build();
+    chain_service
+        .process_block(Arc::new(new_block4.clone()), false)
+        .unwrap();
+
+    parent = new_block4.clone();
+    let new_block5 = BlockBuilder::default()
+        .parent_hash(parent.hash().to_owned())
+        .number((parent.number() + 1).pack())
+        .difficulty(parent.difficulty().pack())
+        .nonce(1u64.pack())
+        .build();
+    chain_service
+        .process_block(Arc::new(new_block5), false)
+        .unwrap();
 }
