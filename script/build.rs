24642739df23055aa017d95a25f401f3313b31d8
Xuejie Xiao <xxuejie@gmail.com>
2019-09-12 06:38:24 +0000

    feat: script package build adjustment
    This commit adds 2 changes:
    
    * It adds a feature to allow disabling logging, hence saving us a
      lot of depdendencies
    * It adds a feature to allow compiling without assembly interpreter
      mode. In CKB we won't allow this since we want only one interpreter,
      but when integrating with alternative projects, such as web
      debugger, this can be quite helpful

---
+++ script/build.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+fn main() {
+    let target_pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap();
+    let target_family = env::var("CARGO_CFG_TARGET_FAMILY").unwrap_or_default();
+    let is_windows = target_family == "windows";
+    let is_unix = target_family == "unix";
+    let can_enable_asm = (target_pointer_width == "64") && (is_windows || is_unix);
+
+    if cfg!(feature = "asm") && (!can_enable_asm) {
+        panic!("asm feature can only be enabled on 64-bit Linux, macOS and Windows platforms!");
+    }
+
+    if can_enable_asm {
+        println!("cargo:rustc-cfg=has_asm");
+    }
+}
