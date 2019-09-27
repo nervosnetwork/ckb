2cee3af1bb6705abad75aa2a3298769a70d0e0f8
shaojunda <shaojunda@gmail.com>
2019-09-27 11:54:35 +0800

    chore: update block assembler template note

--- ckb-bin/src/subcommand/init.rs
+++ ckb-bin/src/subcommand/init.rs
@@ -151,7 +151,7 @@ pub fn init(args: InitArgs) -> Result<(), ExitCode> {
                 "# secp256k1_blake160_sighash_all example:\n\
                  # [block_assembler]\n\
                  # code_hash = \"{}\"\n\
-                 # args = [ \"ckb cli blake160 <compressed-pubkey>\" ]\n\
+                 # args = \"ckb cli blake160 <compressed-pubkey>\"\n\
                  # hash_type = \"{}\"",
                 default_code_hash_option.unwrap_or_default(),
                 DEFAULT_LOCK_SCRIPT_HASH_TYPE,
