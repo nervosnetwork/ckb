e6442c3b1a05813d692c83b5eecfd0631f198943
ian <ian@nervos.org>
2019-11-07 15:50:22 +0800

    chore: update code name and testnet genesis message
    - Set code name to lina
    - Set testnet genesis message to rylai

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn get_version() -> Version {
     #[cfg(docker)]
     let commit_describe = commit_describe.map(|s| s.replace("-dirty", ""));
     let commit_date = option_env!("COMMIT_DATE").map(ToString::to_string);
-    let code_name = Some("rylai-v13".to_string());
+    let code_name = Some("lina".to_string());
     Version {
         major,
         minor,
