81828b0f566d1a34c93daf6edbe34bffa3af108f
zhangsoledad <787953403@qq.com>
2019-11-09 00:57:51 +0800

    fix: proposal table bound

--- util/proposal-table/src/lib.rs
+++ util/proposal-table/src/lib.rs
@@ -64,27 +64,52 @@ impl ProposalTable {
         origin: &ProposalView,
         number: BlockNumber,
     ) -> (HashSet<ProposalShortId>, ProposalView) {
-        let proposal_start = number.saturating_sub(self.proposal_window.farthest()) + 1;
-        let proposal_end = number.saturating_sub(self.proposal_window.closest()) + 1;
+        let candidate_number = number + 1;
+        let proposal_start = candidate_number.saturating_sub(self.proposal_window.farthest());
+        let proposal_end = candidate_number.saturating_sub(self.proposal_window.closest());
 
-        self.table = self.table.split_off(&proposal_start);
+        if proposal_start > 1 {
+            self.table = self.table.split_off(&proposal_start);
+        }
 
         ckb_logger::trace!("[proposal_finalize] table {:?}", self.table);
-        let new_ids = self
-            .table
-            .range((Bound::Unbounded, Bound::Included(&proposal_end)))
-            .map(|pair| pair.1)
-            .cloned()
-            .flatten()
-            .collect();
 
-        let gap = self
-            .table
-            .range((Bound::Excluded(&proposal_end), Bound::Unbounded))
-            .map(|pair| pair.1)
-            .cloned()
-            .flatten()
-            .collect();
+        // - if candidate_number <= self.proposal_window.closest()
+        //      new_ids = []
+        //      gap = [1..candidate_number]
+        // - else
+        //      new_ids = [candidate_number- farthest..= candidate_number- closest]
+        //      gap = [candidate_number- closest + 1..candidate_number]
+        // - end
+        let (new_ids, gap) = if candidate_number <= self.proposal_window.closest() {
+            (
+                HashSet::new(),
+                self.table
+                    .range((Bound::Unbounded, Bound::Included(&number)))
+                    .map(|pair| pair.1)
+                    .cloned()
+                    .flatten()
+                    .collect(),
+            )
+        } else {
+            (
+                self.table
+                    .range((
+                        Bound::Included(&proposal_start),
+                        Bound::Included(&proposal_end),
+                    ))
+                    .map(|pair| pair.1)
+                    .cloned()
+                    .flatten()
+                    .collect(),
+                self.table
+                    .range((Bound::Excluded(&proposal_end), Bound::Included(&number)))
+                    .map(|pair| pair.1)
+                    .cloned()
+                    .flatten()
+                    .collect(),
+            )
+        };
 
         let removed_ids: HashSet<ProposalShortId> =
             origin.set().difference(&new_ids).cloned().collect();
@@ -107,32 +132,84 @@ impl ProposalTable {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::iter::{self, FromIterator};
 
     #[test]
     fn test_finalize() {
-        let id = ProposalShortId::zero();
+        let proposals = vec![
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 2]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 3]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 4]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 5]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 6]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 7]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 8]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 9]),
+            ProposalShortId::new([0u8, 0, 0, 0, 0, 0, 0, 0, 0, 10]),
+        ];
+
         let window = ProposalWindow(2, 10);
         let mut table = ProposalTable::new(window);
-        let mut ids = HashSet::new();
-        ids.insert(id.clone());
-        table.insert(1, ids.clone());
-        let (_, mut view) = table.finalize(&ProposalView::default(), 1);
+
+        for (idx, id) in proposals.iter().skip(1).enumerate() {
+            let mut ids = HashSet::new();
+            ids.insert(id.clone());
+            table.insert((idx + 1) as u64, ids.clone());
+        }
+
+        let (removed_ids, mut view) = table.finalize(&ProposalView::default(), 1);
+        assert!(removed_ids.is_empty());
+        assert!(view.set().is_empty());
+        assert_eq!(
+            view.gap(),
+            &HashSet::from_iter(iter::once(proposals[1].clone()))
+        );
 
         // in window
-        for i in 2..10 {
-            let (removed_ids, new_view) = table.finalize(&view, i);
+        for i in 2..=10usize {
+            let (removed_ids, new_view) = table.finalize(&view, i as u64);
+            let c = i + 1;
+            assert_eq!(
+                new_view.gap(),
+                &HashSet::from_iter(proposals[(c - 2 + 1)..=i].iter().cloned())
+            );
+
+            let s = ::std::cmp::max(1, c.saturating_sub(10));
+            assert_eq!(
+                new_view.set(),
+                &HashSet::from_iter(proposals[s..=(c - 2)].iter().cloned())
+            );
+
             assert!(removed_ids.is_empty());
-            assert!(new_view.contains_proposed(&id));
             view = new_view;
         }
 
+        // finalize 11
         let (removed_ids, new_view) = table.finalize(&view, 11);
-        assert_eq!(removed_ids, ids);
-        assert!(!new_view.contains_proposed(&id));
+        assert_eq!(
+            removed_ids,
+            HashSet::from_iter(iter::once(proposals[1].clone()))
+        );
+        assert_eq!(
+            new_view.set(),
+            &HashSet::from_iter(proposals[2..=10].iter().cloned())
+        );
+        assert!(new_view.gap().is_empty());
+
         view = new_view;
 
+        // finalize 12
         let (removed_ids, new_view) = table.finalize(&view, 12);
-        assert!(removed_ids.is_empty());
-        assert!(!new_view.contains_proposed(&id));
+        assert_eq!(
+            removed_ids,
+            HashSet::from_iter(iter::once(proposals[2].clone()))
+        );
+        assert_eq!(
+            new_view.set(),
+            &HashSet::from_iter(proposals[3..=10].iter().cloned())
+        );
+        assert!(new_view.gap().is_empty());
     }
 }
