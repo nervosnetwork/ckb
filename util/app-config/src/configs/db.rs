732a779b930d2bc9143592628a611e16cd48b4f8
Boyu Yang <yangby@cryptape.com>
2020-08-04 16:11:34 +0800

    feat: load db options from a file; support configuring column families

--- util/app-config/src/configs/db.rs
+++ util/app-config/src/configs/db.rs
@@ -8,12 +8,24 @@ pub struct Config {
     pub path: PathBuf,
     #[serde(default)]
     pub options: HashMap<String, String>,
+    pub options_file: Option<PathBuf>,
 }
 
 impl Config {
-    pub fn update_path_if_not_set<P: AsRef<Path>>(&mut self, path: P) {
+    pub fn adjust<P: AsRef<Path>>(&mut self, root_dir: &Path, data_dir: P, name: &str) {
+        // If path is not set, use the default path
         if self.path.to_str().is_none() || self.path.to_str() == Some("") {
-            self.path = path.as_ref().to_path_buf();
+            self.path = data_dir.as_ref().to_path_buf().join(name);
+        } else if self.path.is_relative() {
+            // If the path is relative, set the base path to `ckb.toml`
+            self.path = root_dir.to_path_buf().join(&self.path)
+        }
+        // If options file is a relative path, set the base path to `ckb.toml`
+        if let Some(file) = self.options_file.iter_mut().next() {
+            if file.is_relative() {
+                let file_new = root_dir.to_path_buf().join(&file);
+                *file = file_new;
+            }
         }
     }
 }
