e16126a7e2303382ea1640d633b9e74848b0699a
Boyu Yang <yangby@cryptape.com>
2020-08-11 10:57:46 +0800

    chore: remove dependencies "proc-macro-hack" for crates in the workspace

--- util/fixed-hash/src/lib.rs
+++ util/fixed-hash/src/lib.rs
@@ -1,12 +1,2 @@
-use proc_macro_hack::proc_macro_hack;
-
 pub use ckb_fixed_hash_core::{error, H160, H256, H512, H520};
-
-#[proc_macro_hack]
-pub use ckb_fixed_hash_hack::h160;
-#[proc_macro_hack]
-pub use ckb_fixed_hash_hack::h256;
-#[proc_macro_hack]
-pub use ckb_fixed_hash_hack::h512;
-#[proc_macro_hack]
-pub use ckb_fixed_hash_hack::h520;
+pub use ckb_fixed_hash_macros::{h160, h256, h512, h520};
