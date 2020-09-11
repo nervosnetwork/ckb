f0f3f58bedbd52dabcc40a14960ea15e517eeb05
driftluo <driftluo@foxmail.com>
2020-09-11 14:51:01 +0800

    chore: all leftover registrations of `both` are cleared

--- network/src/network.rs
+++ network/src/network.rs
@@ -32,10 +32,7 @@ use p2p::{
     error::{DialerErrorKind, HandshakeErrorKind, ProtocolHandleErrorKind, SendErrorKind},
     multiaddr::{self, Multiaddr},
     secio::{self, error::SecioError, PeerId},
-    service::{
-        ProtocolEvent, ProtocolHandle, Service, ServiceError, ServiceEvent, TargetProtocol,
-        TargetSession,
-    },
+    service::{ProtocolHandle, Service, ServiceError, ServiceEvent, TargetProtocol, TargetSession},
     traits::ServiceHandle,
     utils::extract_peer_id,
     SessionId,
@@ -75,8 +72,6 @@ pub struct NetworkState {
     /// includes manually public addrs and remote peer observed addrs
     public_addrs: RwLock<HashMap<Multiaddr, u8>>,
     pending_observed_addrs: RwLock<HashSet<Multiaddr>>,
-    /// Send disconnect message but not disconnected yet
-    disconnecting_sessions: RwLock<HashSet<SessionId>>,
     local_private_key: secio::SecioKeyPair,
     local_peer_id: PeerId,
     bootnodes: Vec<(PeerId, Multiaddr)>,
@@ -124,7 +119,6 @@ impl NetworkState {
             public_addrs: RwLock::new(public_addrs),
             listened_addrs: RwLock::new(Vec::new()),
             pending_observed_addrs: RwLock::new(HashSet::default()),
-            disconnecting_sessions: RwLock::new(HashSet::default()),
             local_private_key: local_private_key.clone(),
             local_peer_id: local_private_key.public_key().peer_id(),
             active: AtomicBool::new(true),
@@ -744,10 +738,6 @@ impl<T: ExitHandler> ServiceHandle for EventHandler<T> {
                     .map(PublicKey::peer_id)
                     .expect("Secio must enabled");
 
-                self.network_state
-                    .disconnecting_sessions
-                    .write()
-                    .remove(&session_context.id);
                 let peer_exists = self
                     .network_state
                     .peer_registry
@@ -766,86 +756,6 @@ impl<T: ExitHandler> ServiceHandle for EventHandler<T> {
             }
             _ => {
                 info!("p2p service event: {:?}", event);
-            }
-        }
-    }
-
-    fn handle_proto(&mut self, context: &mut ServiceContext, event: ProtocolEvent) {
-        // For special protocols: disconnect_message
-        match event {
-            ProtocolEvent::Connected {
-                session_context,
-                proto_id,
-                version,
-            } => {
-                let peer_not_exists = self.network_state.with_peer_registry_mut(|reg| {
-                    reg.get_peer_mut(session_context.id)
-                        .map(|peer| {
-                            peer.protocols.insert(proto_id, version);
-                        })
-                        .is_none()
-                });
-                if peer_not_exists {
-                    let peer_id = session_context
-                        .remote_pubkey
-                        .as_ref()
-                        .map(PublicKey::peer_id)
-                        .expect("Secio must enabled");
-                    if !self
-                        .network_state
-                        .with_peer_registry(|reg| reg.is_feeler(&peer_id))
-                    {
-                        warn!(
-                            "Invalid session {}, protocol id {}",
-                            session_context.id, proto_id,
-                        );
-                    }
-                }
-            }
-            ProtocolEvent::Disconnected {
-                session_context,
-                proto_id,
-            } => {
-                self.network_state.with_peer_registry_mut(|reg| {
-                    let _ = reg.get_peer_mut(session_context.id).map(|peer| {
-                        peer.protocols.remove(&proto_id);
-                    });
-                });
-            }
-            ProtocolEvent::Received {
-                session_context, ..
-            } => {
-                let session_id = session_context.id;
-                let peer_not_exists = self.network_state.with_peer_registry_mut(|reg| {
-                    reg.get_peer_mut(session_id)
-                        .map(|peer| {
-                            peer.last_message_time = Some(Instant::now());
-                        })
-                        .is_none()
-                });
-                if peer_not_exists
-                    && !self
-                        .network_state
-                        .disconnecting_sessions
-                        .read()
-                        .contains(&session_id)
-                {
-                    debug!(
-                        "disconnect peer({}) already removed from registry",
-                        session_context.id
-                    );
-                    self.network_state
-                        .disconnecting_sessions
-                        .write()
-                        .insert(session_id);
-                    if let Err(err) = disconnect_with_message(
-                        context.control(),
-                        session_id,
-                        "already removed from registry",
-                    ) {
-                        debug!("Disconnect failed {:?}, error: {:?}", session_id, err);
-                    }
-                }
             }
         }
     }
@@ -907,9 +817,12 @@ impl<T: ExitHandler> NetworkService<T> {
             move || ProtocolHandle::Both(Box::new(Feeler::new(Arc::clone(&network_state))))
         });
 
+        let disconnect_message_state = Arc::clone(&network_state);
         let disconnect_message_meta = SupportProtocols::DisconnectMessage
             .build_meta_with_service_handle(move || {
-                ProtocolHandle::Both(Box::new(DisconnectMessageProtocol))
+                ProtocolHandle::Callback(Box::new(DisconnectMessageProtocol::new(
+                    disconnect_message_state,
+                )))
             });
 
         // == Build p2p service struct
