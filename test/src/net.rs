ca4e6a90dceb5b91847a2bb1dcf7d0b0468795fe
keroro <keroroxx520@gmail.com>
2020-09-21 16:43:52 +0800

    feat(test): make the communication channels be peer-level

--- test/src/net.rs
+++ test/src/net.rs
@@ -1,14 +1,14 @@
-use crate::utils::{find_available_port, temp_path, wait_until};
+use crate::utils::{find_available_port, message_name, temp_path, wait_until};
 use crate::Node;
 use ckb_app_config::NetworkConfig;
 use ckb_chain_spec::consensus::Consensus;
-
-use ckb_channel::{self as channel, Receiver, RecvTimeoutError, Sender};
+use ckb_channel::{self as channel, unbounded, Receiver, RecvTimeoutError, Sender};
 use ckb_network::{
     bytes::Bytes, CKBProtocol, CKBProtocolContext, CKBProtocolHandler, DefaultExitHandler,
     NetworkController, NetworkService, NetworkState, PeerIndex, ProtocolId, SupportProtocols,
 };
-
+use ckb_util::Mutex;
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
@@ -18,8 +18,10 @@ pub type NetMessage = (PeerIndex, ProtocolId, Bytes);
 pub struct Net {
     p2p_port: u16,
     working_dir: PathBuf,
+    protocols: Vec<SupportProtocols>,
     controller: NetworkController,
-    receiver: Receiver<NetMessage>,
+    register_rx: Receiver<(String, PeerIndex, Receiver<NetMessage>)>,
+    receivers: HashMap<String, (PeerIndex, Receiver<NetMessage>)>,
 }
 
 impl Net {
@@ -36,24 +38,30 @@ impl Net {
             NetworkState::from_config(NetworkConfig {
                 listen_addresses: vec![p2p_listen],
                 path: (&working_dir).into(),
+                max_peers: 128,
+                max_outbound_peers: 128,
+                discovery_local_address: true,
+                ping_interval_secs: 15,
+                ping_timeout_secs: 20,
                 ..Default::default()
             })
             .unwrap(),
         );
-        let (sender, receiver) = channel::unbounded();
-        let protocols = protocols
-            .into_iter()
+        let (register_tx, register_rx) = channel::unbounded();
+        let protocol_handler = DummyProtocolHandler::new(register_tx);
+        let ckb_protocols = protocols
+            .iter()
             .map(|protocol| {
                 CKBProtocol::new_with_support_protocol(
-                    protocol,
-                    Box::new(DummyProtocolHandler { tx: sender.clone() }),
+                    protocol.to_owned(),
+                    Box::new(protocol_handler.clone()),
                     Arc::clone(&network_state),
                 )
             })
             .collect();
         let controller = NetworkService::new(
             Arc::clone(&network_state),
-            protocols,
+            ckb_protocols,
             Vec::new(),
             consensus.identify_name(),
             "0.1.0".to_string(),
@@ -64,8 +72,10 @@ impl Net {
         Self {
             p2p_port,
             working_dir,
+            protocols,
             controller,
-            receiver,
+            register_rx,
+            receivers: Default::default(),
         }
     }
 
@@ -93,51 +103,132 @@ impl Net {
         &self.controller
     }
 
-    pub fn connect(&self, node: &Node) {
+    pub fn connect(&mut self, node: &Node) {
+        self.controller().add_node(
+            &node.node_id().parse().unwrap(),
+            node.p2p_address().parse().unwrap(),
+        );
+
+        let mut n_protocols_connected = 0;
+        while let Ok(connected) = self.register_rx.recv_timeout(Duration::from_secs(60)) {
+            log::info!("bilibili");
+            let (node_id, peer_index, receiver) = connected;
+            n_protocols_connected += 1;
+            if n_protocols_connected == 1 {
+                self.receivers
+                    .insert(node_id.clone(), (peer_index, receiver));
+            }
+            if node_id == node.node_id() && n_protocols_connected == self.protocols.len() {
+                return;
+            }
+        }
+        panic!("timeout to connect to {}", node.p2p_address());
+    }
+
+    pub fn connect_uncheck(&self, node: &Node) {
         self.controller().add_node(
             &node.node_id().parse().unwrap(),
             node.p2p_address().parse().unwrap(),
         );
     }
 
-    pub fn send(&self, protocol_id: ProtocolId, peer: PeerIndex, data: Bytes) {
+    pub fn send(&self, node: &Node, protocol: SupportProtocols, data: Bytes) {
+        let node_id = node.node_id();
+        let protocol_id = protocol.protocol_id();
+        let peer_index = self
+            .receivers
+            .get(node_id)
+            .map(|(peer_index, _)| *peer_index)
+            .unwrap_or_else(|| panic!("not connected peer {}", node.p2p_address()));
         self.controller()
-            .send_message_to(peer, protocol_id, data)
+            .send_message_to(peer_index, protocol_id, data)
             .expect("Send message to p2p network failed");
     }
 
-    pub fn receive(&self) -> NetMessage {
-        self.receiver.recv().unwrap()
+    pub fn receive_timeout(
+        &self,
+        node: &Node,
+        timeout: Duration,
+    ) -> Result<NetMessage, RecvTimeoutError> {
+        let node_id = node.node_id();
+        let (peer_index, receiver) = self
+            .receivers
+            .get(node_id)
+            .unwrap_or_else(|| panic!("not connected peer {}", node.p2p_address()));
+        let net_message = receiver.recv_timeout(timeout)?;
+        log::info!(
+            "received from peer-{}, message_name: {}",
+            peer_index,
+            message_name(&net_message.2)
+        );
+        Ok(net_message)
     }
 
-    pub fn receive_timeout(&self, timeout: Duration) -> Result<NetMessage, RecvTimeoutError> {
-        self.receiver.recv_timeout(timeout)
-    }
-
-    pub fn should_receive<F>(&self, f: F, message: &str) -> PeerIndex
+    pub fn should_receive<Predicate>(&self, node: &Node, predicate: Predicate) -> bool
     where
-        F: Fn(&Bytes) -> bool,
+        Predicate: Fn(&Bytes) -> bool,
     {
-        let mut peer_id: PeerIndex = Default::default();
-        let received = wait_until(30, || {
-            let (receive_peer_id, _, data) = self
-                .receive_timeout(Duration::from_secs(30))
-                .expect("receive msg");
-            peer_id = receive_peer_id;
-            f(&data)
-        });
-
-        assert!(received, message.to_string());
-        peer_id
+        let timeout = Duration::from_secs(30);
+        wait_until(30, || {
+            self.receive_timeout(node, timeout)
+                .map(|(_, _, data)| predicate(&data))
+                .unwrap_or(false)
+        })
     }
 }
 
 pub struct DummyProtocolHandler {
-    tx: Sender<NetMessage>,
+    // When a new peer connects, register to notice outside controller.
+    register_tx: Sender<(String, PeerIndex, Receiver<NetMessage>)>,
+
+    // #{node_id => receiver}
+    // shared between multiple protocol handlers
+    senders: Arc<Mutex<HashMap<String, Sender<NetMessage>>>>,
+}
+
+impl Clone for DummyProtocolHandler {
+    fn clone(&self) -> Self {
+        DummyProtocolHandler {
+            register_tx: self.register_tx.clone(),
+            senders: Arc::clone(&self.senders),
+        }
+    }
+}
+
+impl DummyProtocolHandler {
+    fn new(register_tx: Sender<(String, PeerIndex, Receiver<NetMessage>)>) -> Self {
+        Self {
+            register_tx,
+            senders: Arc::new(Mutex::new(Default::default())),
+        }
+    }
 }
 
 impl CKBProtocolHandler for DummyProtocolHandler {
     fn init(&mut self, _nc: Arc<dyn CKBProtocolContext + Sync>) {}
+
+    fn connected(
+        &mut self,
+        nc: Arc<dyn CKBProtocolContext + Sync>,
+        peer_index: PeerIndex,
+        _version: &str,
+    ) {
+        let peer = nc.get_peer(peer_index).unwrap();
+        let node_id = peer.peer_id.to_base58();
+        let (sender, receiver) = unbounded();
+        let mut senders = self.senders.lock();
+        if !senders.contains_key(&node_id) {
+            senders.insert(node_id.clone(), sender);
+        }
+        let _ = self.register_tx.send((node_id, peer_index, receiver));
+    }
+
+    fn disconnected(&mut self, nc: Arc<dyn CKBProtocolContext + Sync>, peer_index: PeerIndex) {
+        if let Some(peer) = nc.get_peer(peer_index) {
+            let node_id = peer.peer_id.to_base58();
+            self.senders.lock().remove(&node_id);
+        }
+    }
 
     fn received(
         &mut self,
@@ -145,6 +236,11 @@ impl CKBProtocolHandler for DummyProtocolHandler {
         peer_index: PeerIndex,
         data: Bytes,
     ) {
-        let _ = self.tx.send((peer_index, nc.protocol_id(), data));
+        if let Some(peer) = nc.get_peer(peer_index) {
+            let node_id = peer.peer_id.to_base58();
+            if let Some(sender) = self.senders.lock().get(&node_id) {
+                let _ = sender.send((peer_index, nc.protocol_id(), data));
+            }
+        }
     }
 }
