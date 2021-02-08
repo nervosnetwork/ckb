03de0df90430028b2dff0f64fb05a9d586655914
zhangsoledad <787953403@qq.com>
2021-02-08 10:40:32 +0800

    refactory: tx-pool callback

--- tx-pool/src/callback.rs
+++ tx-pool/src/callback.rs
@@ -1,15 +1,17 @@
 use super::component::TxEntry;
 use crate::error::Reject;
+use crate::pool::TxPool;
 
 /// Callback boxed fn pointer wrapper
-pub type Callback = Box<dyn Fn(TxEntry) + Sync + Send>;
+pub type Callback = Box<dyn Fn(&mut TxPool, TxEntry) + Sync + Send>;
 /// Reject Callback boxed fn pointer wrapper
-pub type RejectCallback = Box<dyn Fn(TxEntry, Reject) + Sync + Send>;
+pub type RejectCallback = Box<dyn Fn(&mut TxPool, TxEntry, Reject) + Sync + Send>;
 
 /// Struct hold callbacks
 pub struct Callbacks {
     pub(crate) pending: Vec<Callback>,
     pub(crate) proposed: Vec<Callback>,
+    pub(crate) committed: Vec<Callback>,
     pub(crate) reject: Vec<RejectCallback>,
 }
 
@@ -25,6 +27,7 @@ impl Callbacks {
         Callbacks {
             pending: Vec::new(),
             proposed: Vec::new(),
+            committed: Vec::new(),
             reject: Vec::new(),
         }
     }
@@ -39,25 +42,41 @@ impl Callbacks {
         self.proposed.push(callback);
     }
 
+    /// Register a new committed callback
+    pub fn register_committed(&mut self, callback: Callback) {
+        self.committed.push(callback);
+    }
+
     /// Register a new abandon callback
     pub fn register_reject(&mut self, callback: RejectCallback) {
         self.reject.push(callback);
     }
 
     /// Call on after pending
-    pub fn call_pending(&self, entry: TxEntry) {
-        self.pending.iter().for_each(|call| call(entry.clone()))
+    pub fn call_pending(&self, tx_pool: &mut TxPool, entry: TxEntry) {
+        self.pending
+            .iter()
+            .for_each(|call| call(tx_pool, entry.clone()))
     }
 
     /// Call on after proposed
-    pub fn call_proposed(&self, entry: TxEntry) {
-        self.proposed.iter().for_each(|call| call(entry.clone()))
+    pub fn call_proposed(&self, tx_pool: &mut TxPool, entry: TxEntry) {
+        self.proposed
+            .iter()
+            .for_each(|call| call(tx_pool, entry.clone()))
+    }
+
+    /// Call on after proposed
+    pub fn call_committed(&self, tx_pool: &mut TxPool, entry: TxEntry) {
+        self.committed
+            .iter()
+            .for_each(|call| call(tx_pool, entry.clone()))
     }
 
     /// Call on after reject
-    pub fn call_reject(&self, entry: TxEntry, reject: Reject) {
+    pub fn call_reject(&self, tx_pool: &mut TxPool, entry: TxEntry, reject: Reject) {
         self.reject
             .iter()
-            .for_each(|call| call(entry.clone(), reject.clone()))
+            .for_each(|call| call(tx_pool, entry.clone(), reject.clone()))
     }
 }
