59390a14f33c1debdcf20b1edd49d52489d3ac30
zhangsoledad <787953403@qq.com>
2021-03-12 13:55:53 +0800

    refactor: introduce launcher refactor node launch

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,10 +1,6 @@
 //! TODO(doc): @quake
 
 // num_cpus is used in proc_macro
-// declare here for mute ./devtools/ci/check-cargotoml.sh error
-extern crate num_cpus;
-
-mod migrations;
 pub mod shared;
 
 pub use ckb_snapshot::{Snapshot, SnapshotMgr};
