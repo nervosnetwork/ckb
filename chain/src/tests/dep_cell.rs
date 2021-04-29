8f09213a6f6e08738d112b4f0936e2515174bfcf
zhangsoledad <787953403@qq.com>
2021-04-29 16:50:02 +0800

    test: submit_txs_priority

--- chain/src/tests/dep_cell.rs
+++ chain/src/tests/dep_cell.rs
@@ -480,3 +480,121 @@ fn test_package_txs_with_deps2() {
         assert_eq!(tx.proposal_short_id(), txs[index].proposal_short_id());
     }
 }
+
+#[test]
+fn test_package_txs_with_deps_priority() {
+    let (_, _, always_success_script) = always_success_cell();
+    let always_success_tx = create_always_success_tx();
+    let issue_tx = TransactionBuilder::default()
+        .input(CellInput::new(OutPoint::null(), 0))
+        .output(
+            CellOutputBuilder::default()
+                .capacity(capacity_bytes!(5_000).pack())
+                .lock(always_success_script.clone())
+                .build(),
+        )
+        .output_data(Bytes::new().pack())
+        .output(
+            CellOutputBuilder::default()
+                .capacity(capacity_bytes!(5_000).pack())
+                .lock(always_success_script.clone())
+                .build(),
+        )
+        .output_data(Bytes::new().pack())
+        .build();
+
+    let dao = genesis_dao_data(vec![&always_success_tx, &issue_tx]).unwrap();
+
+    let genesis_block = BlockBuilder::default()
+        .transaction(always_success_tx)
+        .transaction(issue_tx.clone())
+        .compact_target(DIFF_TWO.pack())
+        .dao(dao)
+        .build();
+
+    let consensus = ConsensusBuilder::default()
+        .cellbase_maturity(EpochNumberWithFraction::new(0, 0, 1))
+        .genesis_block(genesis_block)
+        .build();
+
+    let (chain_controller, shared, _parent) = start_chain(Some(consensus));
+
+    let tx_pool = shared.tx_pool_controller();
+
+    let tx1 = build_tx(
+        (&issue_tx, &[0]),
+        (&issue_tx, &[1]),
+        2,
+        Capacity::shannons(400),
+    );
+    let tx2 = build_tx(
+        (&issue_tx, &[1]),
+        (&issue_tx, &[]),
+        2,
+        Capacity::shannons(10000),
+    );
+
+    let txs = vec![tx2.clone(), tx1];
+
+    for tx in &txs {
+        let ret = tx_pool.submit_local_tx(tx.clone()).unwrap();
+        assert!(ret.is_ok(), "submit {} {:?}", tx.proposal_short_id(), ret);
+    }
+
+    let mut block_template = tx_pool
+        .get_block_template(None, None, None)
+        .unwrap()
+        .unwrap();
+
+    // proposal txs
+    {
+        while (Into::<u64>::into(block_template.number)) != 1 {
+            block_template = tx_pool
+                .get_block_template(None, None, None)
+                .unwrap()
+                .unwrap()
+        }
+
+        let block: Block = block_template.clone().into();
+        println!("block proposal len {}", block.proposals().len());
+        let block = block.as_advanced_builder().build();
+        chain_controller
+            .internal_process_block(Arc::new(block), Switch::DISABLE_ALL)
+            .unwrap();
+    }
+
+    // skip gap
+    {
+        while (Into::<u64>::into(block_template.number)) != 2 {
+            block_template = tx_pool
+                .get_block_template(None, None, None)
+                .unwrap()
+                .unwrap()
+        }
+
+        let block: Block = block_template.clone().into();
+        let block = block.as_advanced_builder().build();
+        chain_controller
+            .internal_process_block(Arc::new(block), Switch::DISABLE_ALL)
+            .unwrap();
+    }
+
+    let mut tx_pool_info = tx_pool.get_tx_pool_info().unwrap();
+    while tx_pool_info.tip_number != 2 {
+        tx_pool_info = tx_pool.get_tx_pool_info().unwrap()
+    }
+
+    // get block template with txs
+    while (Into::<u64>::into(block_template.number)) != 3 {
+        block_template = tx_pool
+            .get_block_template(None, None, None)
+            .unwrap()
+            .unwrap()
+    }
+
+    let block: Block = block_template.into();
+    let block = block.as_advanced_builder().build();
+    assert_eq!(block.transactions().len(), 2);
+    // tx1 will be discard
+    assert_eq!(block.transactions()[1], tx2);
+}
