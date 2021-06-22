9b89f955644146a57988de39fa11322d7ad7c2ec
ian <ian@nervos.org>
2021-06-22 14:27:40 +0800

    chore: bundle ckb-cli v0.43.0

--- devtools/git/merge-master.sh
+++ devtools/git/merge-master.sh
@@ -3,4 +3,4 @@
 set -eu
 
 git merge --no-ff --no-commit -s ours master
-git checkout master -- CHANGELOG.md src/main.rs resource/specs/testnet.toml resource/specs/mainnet.toml resource/specs/mainnet.toml.asc .travis.yml azure-pipelines.yml
+git checkout master -- CHANGELOG.md .github/workflows/package.yaml
