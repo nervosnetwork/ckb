17c307894ea2612e23b42d9c974169b369b8ed63
driftluo <driftluo@foxmail.com>
2021-07-14 13:27:29 +0800

    fix: fix peer store evict

--- util/app-config/src/configs/memory_tracker.rs
+++ util/app-config/src/configs/memory_tracker.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// Memory tracker config options.
 #[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct Config {
     /// Tracking interval in seconds.
     pub interval: u64,
