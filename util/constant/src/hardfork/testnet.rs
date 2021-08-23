e50cfb64c5d64ac8810d66d4a6a37c888052e31e
ian <ian@nervos.org>
2021-08-23 10:45:10 +0800

    feat: activates ckb2021 since epoch 2782 in Aggron

--- util/constant/src/hardfork/testnet.rs
+++ util/constant/src/hardfork/testnet.rs
@@ -1,6 +1,5 @@
 /// The Chain Specification name.
 pub const CHAIN_SPEC_NAME: &str = "ckb_testnet";
 
-// TODO ckb2021 Update the epoch number for testnet.
-/// First epoch number for CKB v2021
-pub const CKB2021_START_EPOCH: u64 = u64::MAX;
+/// Estimated time for epoch 2782 is 2021-08-30 2AM in UTC.
+pub const CKB2021_START_EPOCH: u64 = 2782;
