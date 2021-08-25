ae67511584c7750700c6b4cf760880f37f0361d3
liya2017 <Liya@cryptape.com>
2021-08-26 01:50:15 +0800

    feat: improve ci workflows

--- devtools/ci/ci_epilogue.py
+++ devtools/ci/ci_epilogue.py
@@ -56,7 +56,7 @@ def get_check_runs(commit_sha):
     with open(job_info, 'w') as outfile:
             json.dump(job_data, outfile)
 #function to check job's conculusions
-def check_runs_conculusions(commit_sha):
+def check_runs_conculusions(commit_sha,expect_job_num,expect_jobs):
     print("check_runs_conculusions"+str(commit_sha))
     get_check_runs(commit_sha)
     f = open(job_info,"r")
@@ -65,101 +65,140 @@ def check_runs_conculusions(commit_sha):
     print(jobs_data)
     print("jobs_data done")
     CI_conclusion=""
-    required_jobs_count=0
+    excuted_jobs_count=0
     # UnitTest conclusion
     UnitTest_macOS_conclusion=""
     UnitTest_Linux_conclusion=""
     UnitTest_Windows_conclusion=""
     UnitTest_conclusion=""
+    is_UnitTest_run=False
     # Liners conclusion
     Liners_macOS_conclusion=""
     Liners_Linux_conclusion=""
     Liners_conclusion=""
+    is_Liners_run=False
     #Benchmark conclusion
     Benchmark_Linux_conclusion=""
     Benchmark_macOS_conclusion=""
     Benchmark_conclusion=""
+    is_Benchmark_run=False
     #Integration conclusion
     Integration_Linux_conclusion=""
-    global Integration_macOs_conclusion
+    Integration_macOS_conclusion=""
     Integration_Windows_conclusion=""
     Integration_conclusion=""
+    is_Integration_run=False
     #Quick check conclusion
     Quick_Check_conclusion=""
+    is_Quick_Check_run=False
     #Security Audit conclusion
     Security_Audit_Licenses_conclusion=""
+    is_Security_Audit_run=False
     #WASM build conclusion
     WASM_build_conclusion=""
+    is_WASM_build_run=False
 
 
     for i in range(len(jobs_data["job_details"])):
         #Unit test conclusion
         job_name=jobs_data["job_details"][i]["job_name"]
         job_conclusion=jobs_data["job_details"][i]["job_conclusion"]
-        if job_name.find("ci_unit_tests") != -1 & job_name.find("ubuntu") != -1:
-            UnitTest_Linux_conclusion=job_conclusion
-        if job_name.find("ci_unit_tests (macos") != -1:
-            UnitTest_macOS_conclusion=job_conclusion
-        if job_name.find("ci_unit_tests (windows") != -1:
-            UnitTest_Windows_conclusion=job_conclusion
-        #Integration test conclusion
-        if job_name.find("ci_integration_test") != -1 & job_name.find("ubuntu") != -1:
-            Integration_Linux_conclusion=job_conclusion
-        if job_name.find("ci_integration_test (macos") != -1:
-            Integration_macOS_conclusion=job_conclusion
-        if job_name.find("ci_integration_test (windows") != -1:
-            Integration_conclusion=job_conclusion
-        #liners test conclusion
-        if job_name.find("ci_liners") != -1 & job_name.find("ubuntu") != -1:
-            Liners_Linux_conclusion=job_conclusion
-        if job_name.find("ci_liners (macos") != -1:
-            Liners_macOS_conclusion=job_conclusion
-        #Benchmark test conclusion
-        if job_name.find("ci_benchmarks_test") != -1 & job_name.find("ubuntu") != -1:
-            Benchmark_Linux_conclusion=job_conclusion
-        if job_name.find("ci_benchmarks_test (macos") != -1:
-            Benchmark_macOS_conclusion=job_conclusion
-        #Quick check conclusion
-        if job_name.find("ci_quick_check") != -1:
-            Quick_Check_conclusion=job_conclusion
-        #Security Audit conclusion
-        if job_name.find("ci_security_audit_licenses") != -1:
-            Security_Audit_Licenses_conclusion=job_conclusion
-        #WASM build conclusion
-        if job_name.find("ci_WASM_build") != -1:
-            WASM_build_conclusion=job_conclusion
+        for j in range(len(expect_jobs)):
+           expected_name=str(expect_jobs[j])
+           if (expected_name.find("unit_test")):
+              is_UnitTest_run=True
+              if job_name.find("ci_unit_tests") != -1 & job_name.find("ubuntu") != -1:
+                  UnitTest_Linux_conclusion=job_conclusion
+              if job_name.find("ci_unit_tests (macos") != -1:
+                  UnitTest_macOS_conclusion=job_conclusion
+              if job_name.find("ci_unit_tests (windows") != -1:
+                  UnitTest_Windows_conclusion=job_conclusion
+            #Integration test conclusion
+           if (expected_name.find("integration_test")):
+              is_Integration_run=True
+              if job_name.find("ci_integration_test") != -1 & job_name.find("ubuntu") != -1:
+                    Integration_Linux_conclusion=job_conclusion
+              if job_name.find("ci_integration_test (macos") != -1:
+                    Integration_macOS_conclusion=job_conclusion
+              if job_name.find("ci_integration_test (windows") != -1:
+                    Integration_conclusion=job_conclusion
+            #liners test conclusion
+           if (expected_name.find("liners")):
+              is_Liners_run=True
+              if job_name.find("ci_liners") != -1 & job_name.find("ubuntu") != -1:
+                    Liners_Linux_conclusion=job_conclusion
+              if job_name.find("ci_liners (macos") != -1:
+                    Liners_macOS_conclusion=job_conclusion
+            #Benchmark test conclusion
+           if (expected_name.find("benchmarks_test")):
+               is_Benchmark_run=True
+               if job_name.find("ci_benchmarks_test") != -1 & job_name.find("ubuntu") != -1:
+                  Benchmark_Linux_conclusion=job_conclusion
+               if job_name.find("ci_benchmarks_test (macos") != -1:
+                  Benchmark_macOS_conclusion=job_conclusion
+            #Quick check conclusion
+           if (expected_name.find("quick_check")):
+               is_Quick_Check_run=False
+               if job_name.find("ci_quick_check") != -1:
+                  Quick_Check_conclusion=job_conclusion
+            #Security Audit conclusion
+           if (expected_name.find("security_audit")):
+               is_Security_Audit_run=True
+               if job_name.find("ci_security_audit_licenses") != -1:
+                  Security_Audit_Licenses_conclusion=job_conclusion
+            #WASM build conclusion
+           if (expected_name.find("WASM_build")):
+               is_Quick_Check_run=True
+               if job_name.find("ci_WASM_build") != -1:
+                  WASM_build_conclusion=job_conclusion
+
      #UnitTest check
-    if (UnitTest_macOS_conclusion == "success" ) | (UnitTest_Linux_conclusion == "success" ) | (UnitTest_Windows_conclusion == "success" ):
-        UnitTest_conclusion="success"
-        required_jobs_count +=1
-    elif (UnitTest_macOS_conclusion == "failure" ) | (UnitTest_Linux_conclusion == "failure" ) | (UnitTest_Windows_conclusion == "failure" ):
-        UnitTest_conclusion="failure"
-        required_jobs_count +=1
+    if (is_UnitTest_run == True):
+        if (UnitTest_macOS_conclusion == "success" ) | (UnitTest_Linux_conclusion == "success" ) | (UnitTest_Windows_conclusion == "success" ):
+            UnitTest_conclusion="success"
+            excuted_jobs_count +=1
+        elif (UnitTest_macOS_conclusion == "failure" ) | (UnitTest_Linux_conclusion == "failure" ) | (UnitTest_Windows_conclusion == "failure" ):
+            UnitTest_conclusion="failure"
+            excuted_jobs_count +=1
      #Integration check
-    if (Integration_macOS_conclusion == "success" ) | (Integration_Linux_conclusion == "success" ) | (Integration_Windows_conclusion == "success" ):
-        Integration_conclusion="success"
-        required_jobs_count +=1
-    elif (Integration_macOS_conclusion == "failure" ) | (Integration_Linux_conclusion == "failure" ) | (Integration_Windows_conclusion == "failure" ):
-        Integration_conclusion="failure"
-        required_jobs_count +=1
+    if (is_Integration_run == True):
+        if (Integration_macOS_conclusion == "success" ) | (Integration_Linux_conclusion == "success" ) | (Integration_Windows_conclusion == "success" ):
+            Integration_conclusion="success"
+            excuted_jobs_count +=1
+        elif (Integration_macOS_conclusion == "failure" ) | (Integration_Linux_conclusion == "failure" ) | (Integration_Windows_conclusion == "failure" ):
+            Integration_conclusion="failure"
+            excuted_jobs_count +=1
     # Liners check
-    if (Liners_macOS_conclusion == "success" ) | (Liners_Linux_conclusion == "success" ):
-        Liners_conclusion="success"
-        required_jobs_count +=1
-    elif (Liners_macOS_conclusion == "failure" ) | (Liners_Linux_conclusion == "failure" ):
-        Liners_conclusion="failure"
-        required_jobs_count +=1
+    if (is_Liners_run == True):
+        if (Liners_macOS_conclusion == "success" ) | (Liners_Linux_conclusion == "success" ):
+            Liners_conclusion="success"
+            excuted_jobs_count +=1
+        elif (Liners_macOS_conclusion == "failure" ) | (Liners_Linux_conclusion == "failure" ):
+            Liners_conclusion="failure"
+            excuted_jobs_count +=1
    # Benchmark check
-    if (Benchmark_Linux_conclusion == "success" ) | (Benchmark_macOS_conclusion == "success" ):
-        Benchmark_conclusion="success"
-        required_jobs_count +=1
-    elif (Benchmark_Linux_conclusion == "failure" ) | (Benchmark_macOS_conclusion == "failure" ):
-        Benchmark_conclusion="failure"
-        required_jobs_count +=1
+    if (is_Benchmark_run == True):
+        if (Benchmark_Linux_conclusion == "success" ) | (Benchmark_macOS_conclusion == "success" ):
+            Benchmark_conclusion="success"
+            excuted_jobs_count +=1
+        elif (Benchmark_Linux_conclusion == "failure" ) | (Benchmark_macOS_conclusion == "failure" ):
+            Benchmark_conclusion="failure"
+            excuted_jobs_count +=1 
+    # Quick_Check check
+    if (is_Quick_Check_run == True):
+        excuted_jobs_count +=1
+    # Security_Audit check
+    if (is_Security_Audit_run == True):
+        excuted_jobs_count +=1
+    # WASM_build check
+    if (is_WASM_build_run == True):
+        excuted_jobs_count +=1
 
     jobs_conclusion=[UnitTest_conclusion,Liners_conclusion,Benchmark_conclusion,Integration_conclusion,Quick_Check_conclusion,Security_Audit_Licenses_conclusion,WASM_build_conclusion]
     # check child jobs conclusions if all required jobs completed in one os
-    if ( required_jobs_count == 7 ):
+    print("excuted_jobs_count is "+str(excuted_jobs_count))
+    print("expect_job_num is "+str(expect_job_num))
+    if ( excuted_jobs_count == expect_job_num ):
         #set ci conclusion
         if  "failure" in jobs_conclusion:
             CI_conclusion="failure"
@@ -173,8 +212,6 @@ def check_runs_conculusions(commit_sha):
 def update_commit_state(COMMIT_SHA):
     g = Github(os.getenv('TOKEN'))
     repo = g.get_repo(os.getenv('REPOSITPRY'))
-    print("COMMIT_SHA"+str(COMMIT_SHA))
-    print("CI_conclusion:"+str(CI_conclusion))
     repo.get_commit(sha=COMMIT_SHA).create_status(
         state="success",
         description="ci",
@@ -193,9 +230,11 @@ if __name__ == '__main__':
       COMMIT_SHA=str(os.getenv('PR_COMMIT_SHA'))
       MESSAGE=str(os.getenv('PR_COMMONS_BODY'))
 
-   if ( "skip ci" in MESSAGE ) & ( ( os.getenv('EVENT_NAME') == "push" ) | ( ( os.getenv('EVENT_NAME') == "pull_request" ) & ( os.getenv('REPO_ACTOR') in REPO_LIST ) ) ):
-        print("skip ci")
+   if ( "ci:" in MESSAGE ) & ( ( os.getenv('EVENT_NAME') == "push" ) | ( ( os.getenv('REPO_OWNER') == "nervosnetwork" ) & ( os.getenv('EVENT_NAME') == "pull_request" ) & ( os.getenv('REPO_ACTOR') in REPO_LIST ) ) ):
+        print("spicific ci jobs run")
+        required_job=MESSAGE.split("ci:[")[1].split("]")[0].split(',')
         update_commit_state(COMMIT_SHA)
+        check_runs_conculusions(COMMIT_SHA,len(required_job),required_job)
    else :
-       check_runs_conculusions(COMMIT_SHA)
-
+       required_job=['ci_unit_tests','ci_integration_test','ci_liners','ci_benchmarks_test','ci_quick_check','ci_security_audit_licenses','ci_WASM_build']
+       check_runs_conculusions(COMMIT_SHA,7,required_job)
--- devtools/ci/ci_main.sh
+++ devtools/ci/ci_main.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 set -u
 set +e
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$workspace/target"}
+#function for different test by name
 func_ci() {
  name=${1}
  if [ $name == "ci_unit_tests" ];then
     echo "ci_unit_tests"
     make test
+    EXIT_CODE=$?
  fi
+
  if [ $name == "ci_benchmarks_test" ];then
     echo "ci_benchmarks_test"
     make bench-test
+    EXIT_CODE=$?
  fi
+
  if [ $name == "ci_integration_test" ];then
     echo "ci_integration_test"
+    git submodule update --init
+    cp -f Cargo.lock test/Cargo.lock
+    cargo build --release --target-dir $CARGO_TARGET_DIR
+    rm -rf test/target && ln -snf ${CARGO_TARGET_DIR} test/target
+    test_id=$(date +"%Y%m%d-%H%M%S")
+    echo $test_id
+    test_tmp_dir=${CKB_INTEGRATION_TEST_TMP:-"$workspace/target/ckb-test/${test_id}"}
+    mkdir -p "${test_tmp_dir}"
+    export CKB_INTEGRATION_TEST_TMP="${test_tmp_dir}"
+    test_log_file="${test_tmp_dir}/integration.log"
     cd test
-    cargo run -- --log-file target/integration.log $CKB_TEST_ARGS
+    cargo run -- --bin ${CARGO_TARGET_DIR}/release/ckb ${CKB_TEST_ARGS} 2>&1 | tee "${test_log_file}"
+   EXIT_CODE=$?
  fi
+
  if [ $name == "ci_quick_check" ];then
     echo "ci_quick_check"
     make check-cargotoml
@@ -23,6 +41,7 @@ func_ci() {
     make check-dirty-rpc-doc
     make check-dirty-hashes-toml
     devtools/ci/check-cyclic-dependencies.py
+    EXIT_CODE=$?
  fi
  if [ $name == "ci_linters" ];then
     echo "ci_linters"
@@ -31,45 +50,75 @@ func_ci() {
     make fmt
     make clippy
     git diff --exit-code Cargo.lock
+    EXIT_CODE=$?
  fi
+
  if [ $name == "ci_security_audit_licenses" ];then
     echo "ci_security_audit_licenses"
     cargo deny --version || cargo install cargo-deny --locked
     make security-audit
     make check-crates
     make check-licenses
+    EXIT_CODE=$?
  fi
+
  if [ $name == "ci_WASM_build" ];then
     echo "ci_WASM_build"
     rustup target add wasm32-unknown-unknown
     make wasm-build-test
+    EXIT_CODE=$?
  fi
- EXIT_CODE=$?
  echo $EXIT_CODE
 }
 
-
-name="$1"
+#Get commit sha and message by event name
 if [ $EVENT_NAME == "push" ];then
    COMMIT_SHA=$COMMIT_SHA
    MESSAGE="$COMMIT_MESSAGE"
 fi
+
 if [ $EVENT_NAME == "pull_request" ];then
     COMMIT_SHA=$PR_COMMIT_SHA
     MESSAGE="$PR_COMMONS_BODY"
 fi
+
 nervosnetwork_actor_list='"janx", "doitian", "quake", "xxuejie", "zhangsoledad", "jjyr", "TheWaWaR", "driftluo", "keroro520", "yangby-cryptape","liya2017"'
-echo $MESSAGE | grep -q "skip ci"
+export EXIT_CODE=0
+echo $MESSAGE | grep -q "ci:"
+#skip ci
 if [[ $? -eq 0 ]]; then
-   if [[ $EVENT_NAME == "push" ]]  || [[ $EVENT_NAME == "pull_request"  &&  $nervosnetwork_actor_list =~ $ACTOR ]];then
-   echo "skip ci"
-   EXIT_CODE="0"
+   if [[ $EVENT_NAME == "push" ]]  || [[ $REPO_OWNER == "nervosnetwork" && $EVENT_NAME == "pull_request" &&  $nervosnetwork_actor_list =~ $ACTOR ]];then
+      CI_JOB_LIST=`echo $MESSAGE | grep "ci:" | awk -F ":" '{print $2}'`
+      if [ $CI_JOB_LIST =~ "unit_tests" ];then
+         name="ci_unit_tests"
+      fi
+      if [ $CI_JOB_LIST =~ "integration_test" ];then
+         name="ci_integration_test"
+      fi
+      if [ $CI_JOB_LIST =~ "benchmark" ];then
+         name="ci_benchmarks_test"
+      fi
+      if [ $CI_JOB_LIST =~ "quick_check" ];then
+         name="ci_quick_check"
+      fi
+      if [ $CI_JOB_LIST =~ "linters" ];then
+         name="ci_linters"
+      fi
+      if [ $CI_JOB_LIST =~ "security_audit" ];then
+         name="ci_security_audit_licenses"
+      fi
+      if [ $CI_JOB_LIST =~ "WASM_build" ];then
+         name="ci_WASM_build"
+      fi
    fi
 else
-  func_ci $name
-  echo "EXIT_CODE is "$EXIT_CODE
+  name="$1"
 fi
+
+func_ci $name
+
 EXIT_STATUS=$EXIT_CODE
+
 set -e
 if [ "$EXIT_STATUS" = 0 ]; then
     echo "Check whether the ci succeeds"
