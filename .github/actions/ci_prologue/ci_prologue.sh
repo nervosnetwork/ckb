6d7a99882be5e69747655c9000ab2c31730fd8b8
liya2017 <Liya@cryptape.com>
2021-09-06 11:41:51 +0800

    feat: refine ci based on RFC v2

---
+++ .github/actions/ci_prologue/ci_prologue.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+set -e
+fun_run_os(){
+  runs_on="$1"
+  github_workflow_os=`echo $GITHUB_WORKFLOW | awk -F '_' '{print $NF}'`
+  echo "github_workflow_os is "$github_workflow_os
+  os_skip="skip"
+  echo "$runs_on" | sed "s/\[//g" | sed "s/\]//g" | sed "s/,/\n/g" > runs_on.txt
+  while read -r LINE;
+  do
+    LINE=$(echo "$LINE" | sed -e 's/\r//g')
+    echo "OS LINE is "$LINE
+      if [[ $github_workflow_os == $LINE  ]];then
+        os_skip="run"
+      fi
+  done < "runs_on.txt"
+  echo "OS skip is "$os_skip
+  echo "::set-output name=os_skip::$os_skip"
+}
+
+fun_jobs(){
+  job_list="$1"
+  job_skip="skip"
+  echo "$job_list" | sed "s/\[//g" | sed "s/\]//g" | sed "s/,/\n/g" > job_run.txt
+  while read -r LINE;
+  do
+    LINE="ci_"$(echo "$LINE" | sed -e 's/\r//g')
+    if [[ $GITHUB_WORKFLOW == "$LINE"* ]];then
+      echo "job_name is"$LINE
+      echo $GITHUB_WORKFLOW
+      job_skip="run"
+    fi
+  done < "job_run.txt"
+  echo "JOB skip is" $job_skip
+  echo "::set-output name=job_skip::$job_skip"
+}
+
+fun_pasing_message(){
+  set +e
+  MESSAGE=$1
+  #pass job run list
+  echo "$MESSAGE" | grep -q "ci-runs-only:"
+  if [ $? -eq 0 ]; then
+    job_run_list=`echo "${MESSAGE}"| grep "ci-runs-only" | awk -F ':' '{print $2}'`
+  else
+    job_run_list=" [ quick_checks,unit_tests,integration_tests,benchmarks,linters,wasm_build,cargo_deny ] "
+  fi
+  echo "job_run_list is ""$job_run_list"
+  #parsing runs os
+  echo "$MESSAGE" | grep -q "ci-runs-on:"
+  if [ $? -eq 0 ]; then
+     runs_on=` echo "${MESSAGE}"| grep "ci-runs-on" | awk -F ':' '{print $2}'`
+  else
+     runs_on=" [ ubuntu,macos,windows ] "
+  fi
+  echo "runs_on is ""$runs_on"
+
+  #parsing uses-self-runner
+  echo "$MESSAGE" | grep -q "ci-uses-self-runner:"
+  if [ $? -eq 0 ]; then
+     ci_uses_self_runner=` echo "${MESSAGE}"| grep "ci-uses-self-runner" | awk -F ':' '{print $2}' | awk '{gsub(/^\s+|\s+$/, "");print}'`
+  else
+     ci_uses_self_runner=false
+  fi
+  echo "ci_uses_self_runner is ""$ci_uses_self_runner"
+  set -e
+  #set reqiured output
+  fun_run_os "$runs_on"
+  fun_jobs "$job_run_list"
+  echo "GITHUB_REPOSITORY is "$GITHUB_REPOSITORY
+  if [[ "$ci_uses_self_runner" == "true" ]] || [[ "$GITHUB_REPOSITORY" == "nervosnetwork/ckb" ]];then
+    runner_label='self-hosted-ci-ubuntu-20.04'
+  else
+    runner_label='ubuntu-20.04'
+  fi
+  echo "runner_label is "$runner_label
+  echo "::set-output name=runner_label::$runner_label"
+}
+
+if [[ $GITHUB_EVENT_NAME == "push" ]];then
+  MESSAGE="$COMMIT_MESSAGE"
+  fun_pasing_message "$MESSAGE"
+fi
+if [[ $GITHUB_EVENT_NAME == "pull_request" ]];then
+  MESSAGE="$PR_COMMONS_BODY"
+  actor_permission=`curl -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/collaborators/$GITHUB_ACTOR/permission |jq .permission | sed 's/\"//g'`
+  echo "actor_permission is" $actor_permission
+  if [[ ${LABELS[@]} =~ "ci-trust" ]];then
+    echo "ci-trust"
+    fun_pasing_message "$MESSAGE"
+  elif [[ $actor_permission == "admin" || $actor_permission == "write" ]];then
+    echo "actor_permission"
+    fun_pasing_message "$MESSAGE"
+  else
+    runs_on=" [ ubuntu,macos,windows ] "
+    fun_run_os "$runs_on"
+    job_run_list=" [ quick_checks,unit_tests,integration_tests,benchmarks,linters,wasm_build,cargo_deny ] "
+    fun_jobs "$job_run_list"
+    if [[ "$GITHUB_REPOSITORY" == "nervosnetwork/ckb" ]];then
+      echo "::set-output name=runner_label::self-hosted-ci-ubuntu-20.04"
+    else
+      echo "::set-output name=runner_label::ubuntu-20.04"
+    fi
+  fi
+fi
+
+
--- devtools/ci/ci_main.sh
+++ devtools/ci/ci_main.sh
@@ -1,128 +1,71 @@
 #!/bin/bash
 set -u
 set +e
-CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$workspace/target"}
-#function for different test by name
-func_ci() {
- name=${1}
- if [ $name == "ci_unit_tests" ];then
+is_self_runner=`echo $RUNNER_LABEL | awk -F '-' '{print $1}'`
+if [[ $is_self_runner == "self" ]];then
+  CARGO_TARGET_DIR=$GITHUB_WORKSPACE/../target
+fi
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$GITHUB_WORKSPACE/target"}
+case $GITHUB_WORKFLOW in
+  ci_linters*)
+    echo "Hellow ci_linters!"
+    cargo fmt --version ||  rustup component add rustfmt
+    argo clippy --version ||  rustup component add clippy
+    make fmt
+    make clippy
+    git diff --exit-code Cargo.lock
+    EXIT_CODE="${PIPESTATUS[0]}"
+    ;;
+  ci_unit_test*)
     echo "ci_unit_tests"
     make test
-    EXIT_CODE=$?
- fi
-
- if [ $name == "ci_benchmarks_test" ];then
+    EXIT_CODE="${PIPESTATUS[0]}"
+    ;;
+  ci_benchmarks*)
     echo "ci_benchmarks_test"
     make bench-test
-    EXIT_CODE=$?
- fi
-
- if [ $name == "ci_integration_test" ];then
+    EXIT_CODE="${PIPESTATUS[0]}"
+    ;;
+ci_integration_tests*)
     echo "ci_integration_test"
+    github_workflow_os=`echo $GITHUB_WORKFLOW | awk -F '_' '{print $NF}'`
     git submodule update --init
     cp -f Cargo.lock test/Cargo.lock
-    cargo build --release --target-dir $CARGO_TARGET_DIR
+    cargo build --features deadlock_detection --target-dir $CARGO_TARGET_DIR
     rm -rf test/target && ln -snf ${CARGO_TARGET_DIR} test/target
-    test_id=$(date +"%Y%m%d-%H%M%S")
-    echo $test_id
-    test_tmp_dir=${CKB_INTEGRATION_TEST_TMP:-"$workspace/target/ckb-test/${test_id}"}
-    mkdir -p "${test_tmp_dir}"
-    export CKB_INTEGRATION_TEST_TMP="${test_tmp_dir}"
-    test_log_file="${test_tmp_dir}/integration.log"
     cd test
-    cargo run -- --bin ${CARGO_TARGET_DIR}/release/ckb ${CKB_TEST_ARGS} 2>&1 | tee "${test_log_file}"
-   EXIT_CODE=$?
- fi
-
- if [ $name == "ci_quick_check" ];then
+    if [[ $github_workflow_os == 'windows' ]];then
+      cargo run -- --bin ${CARGO_TARGET_DIR}/debug/ckb.exe --log-file target/integration.log ${CKB_TEST_ARGS}
+    else
+      cargo run -- --bin ${CARGO_TARGET_DIR}/debug/ckb --log-file target/integration.log ${CKB_TEST_ARGS}
+    fi
+    EXIT_CODE="${PIPESTATUS[0]}"
+    ;;
+  ci_quick_checks*)
     echo "ci_quick_check"
     make check-cargotoml
     make check-whitespaces
     make check-dirty-rpc-doc
     make check-dirty-hashes-toml
     devtools/ci/check-cyclic-dependencies.py
-    EXIT_CODE=$?
- fi
- if [ $name == "ci_linters" ];then
-    echo "ci_linters"
-    cargo fmt --version ||  rustup component add rustfmt
-    cargo clippy --version ||  rustup component add clippy
-    make fmt
-    make clippy
-    git diff --exit-code Cargo.lock
-    EXIT_CODE=$?
- fi
-
- if [ $name == "ci_security_audit_licenses" ];then
+    EXIT_CODE="${PIPESTATUS[0]}"
+    ;;
+  ci_wasm_build*)
+    echo "ci_WASM_build"
+    rustup target add wasm32-unknown-unknown
+    make wasm-build-test
+    EXIT_CODE="${PIPESTATUS[0]}"
+    ;;
+  ci_cargo_deny*)
     echo "ci_security_audit_licenses"
     cargo deny --version || cargo install cargo-deny --locked
     make security-audit
     make check-crates
     make check-licenses
-    EXIT_CODE=$?
- fi
-
- if [ $name == "ci_WASM_build" ];then
-    echo "ci_WASM_build"
-    rustup target add wasm32-unknown-unknown
-    make wasm-build-test
-    EXIT_CODE=$?
- fi
- echo $EXIT_CODE
-}
-
-#Get commit sha and message by event name
-if [ $EVENT_NAME == "push" ];then
-   COMMIT_SHA=$COMMIT_SHA
-   MESSAGE="$COMMIT_MESSAGE"
-fi
-
-if [ $EVENT_NAME == "pull_request" ];then
-    COMMIT_SHA=$PR_COMMIT_SHA
-    MESSAGE="$PR_COMMONS_BODY"
-fi
-
-nervosnetwork_actor_list='"janx", "doitian", "quake", "xxuejie", "zhangsoledad", "jjyr", "TheWaWaR", "driftluo", "keroro520", "yangby-cryptape","liya2017"'
-export EXIT_CODE=0
-echo $MESSAGE | grep -q "ci:"
-#skip ci
-if [[ $? -eq 0 ]]; then
-   if [[ $EVENT_NAME == "push" ]]  || [[ $REPO_OWNER == "nervosnetwork" && $EVENT_NAME == "pull_request" &&  $nervosnetwork_actor_list =~ $ACTOR ]];then
-      CI_JOB_LIST=`echo $MESSAGE | grep "ci:" | awk -F ":" '{print $2}'`
-      if [ $CI_JOB_LIST =~ "unit_tests" ];then
-         name="ci_unit_tests"
-      fi
-      if [ $CI_JOB_LIST =~ "integration_test" ];then
-         name="ci_integration_test"
-      fi
-      if [ $CI_JOB_LIST =~ "benchmark" ];then
-         name="ci_benchmarks_test"
-      fi
-      if [ $CI_JOB_LIST =~ "quick_check" ];then
-         name="ci_quick_check"
-      fi
-      if [ $CI_JOB_LIST =~ "linters" ];then
-         name="ci_linters"
-      fi
-      if [ $CI_JOB_LIST =~ "security_audit" ];then
-         name="ci_security_audit_licenses"
-      fi
-      if [ $CI_JOB_LIST =~ "WASM_build" ];then
-         name="ci_WASM_build"
-      fi
-   fi
-else
-  name="$1"
-fi
-
-func_ci $name
-
-EXIT_STATUS=$EXIT_CODE
-
-set -e
-if [ "$EXIT_STATUS" = 0 ]; then
-    echo "Check whether the ci succeeds"
-else
-    echo "Fail the ci"
-fi
-exit $EXIT_STATUS
+    EXIT_CODE="${PIPESTATUS[0]}"
+    ;;
+  *)
+    echo -n "unknown"
+    ;;
+esac
+exit $EXIT_CODE
