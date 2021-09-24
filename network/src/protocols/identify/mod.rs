a5b10d13363973511614a8f739586a0c4ee05446
driftluo <driftluo@foxmail.com>
2021-09-24 09:06:24 +0800

    fix: fix identify disconnect

--- network/src/protocols/identify/mod.rs
+++ network/src/protocols/identify/mod.rs
@@ -407,7 +407,9 @@ impl Callback for IdentifyCallback {
             // it is possible that the node will be accidentally evicted, so it is necessary
             // to reset the information of the node when disconnected.
             self.network_state.with_peer_store_mut(|peer_store| {
-                peer_store.add_outbound_addr(context.session.address.clone());
+                if !peer_store.is_addr_banned(&context.session.address) {
+                    peer_store.add_outbound_addr(context.session.address.clone());
+                }
             });
         }
     }
