765cfecf86d645cb08ab9527f01f4ae14a7b64ce
Xuejie Xiao <xxuejie@gmail.com>
2021-12-02 04:44:18 +0000

    chore: add aarch64 build workflow to CI
    Now that aarch64 build has been tested for a while, I propose we include
    aarch64 build workflow as part of the CI process

--- devtools/ci/ci_main.sh
+++ devtools/ci/ci_main.sh
@@ -49,6 +49,25 @@ ci_integration_tests*)
     rustup target add wasm32-unknown-unknown
     make wasm-build-test
     ;;
+  ci_aarch64_build*)
+    echo "ci_aarch64_build"
+    sudo apt-get install -y gcc-multilib
+    sudo apt-get install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu clang
+    rustup target add aarch64-unknown-linux-gnu
+    curl -LO https://www.openssl.org/source/openssl-1.1.1.tar.gz
+    tar -xvzf openssl-1.1.1.tar.gz
+    cd openssl-1.1.1
+    CC=aarch64-linux-gnu-gcc ./Configure linux-aarch64 shared
+    CC=aarch64-linux-gnu-gcc make
+    cd ..
+    export TOP
+    export OPENSSL_LIB_DIR=$(pwd)/openssl-1.1.1
+    export OPENSSL_INCLUDE_DIR=$(pwd)/openssl-1.1.1/include
+    export PKG_CONFIG_ALLOW_CROSS=1
+    export CC=gcc
+    export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
+    cargo build --target=aarch64-unknown-linux-gnu
+    ;;
   ci_cargo_deny*)
     echo "ci_security_audit_licenses"
     cargo deny --version || cargo +stable install cargo-deny --locked
