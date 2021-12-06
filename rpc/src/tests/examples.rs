162d0290b8b17f903e4f030c256ea5ecdf25da63
zhangsoledad <787953403@qq.com>
2021-12-06 22:58:26 +0800

    fix: rpc test example address

--- rpc/src/tests/examples.rs
+++ rpc/src/tests/examples.rs
@@ -206,7 +206,7 @@ fn setup_rpc_test_suite(height: u64) -> RpcTestSuite {
 
     // Start rpc services
     let rpc_config = RpcConfig {
-        listen_address: "127.0.0.01:0".to_owned(),
+        listen_address: "127.0.0.1:0".to_owned(),
         tcp_listen_address: None,
         ws_listen_address: None,
         max_request_body_size: 20_000_000,
