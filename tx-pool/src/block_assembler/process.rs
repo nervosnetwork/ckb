01777225baefca37f33aeb93455e2e5d822ade4c
zhangsoledad <787953403@qq.com>
2022-03-24 18:32:35 +0800

    chore: fix clippy lint

--- tx-pool/src/block_assembler/process.rs
+++ tx-pool/src/block_assembler/process.rs
@@ -20,7 +20,7 @@ pub(crate) async fn process(service: TxPoolService, message: &BlockAssemblerMess
         }
         BlockAssemblerMessage::Reset(snapshot) => {
             if let Some(ref block_assembler) = service.block_assembler {
-                if let Err(e) = block_assembler.update_blank(Arc::clone(&snapshot)).await {
+                if let Err(e) = block_assembler.update_blank(Arc::clone(snapshot)).await {
                     ckb_logger::error!("block_assembler update_blank error {}", e);
                 }
             }
