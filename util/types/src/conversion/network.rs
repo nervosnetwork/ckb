a3cd67635600eb80c53d3e7213413b40b4358888
Boyu Yang <yangby@cryptape.com>
2022-06-04 03:46:03 +0800

    feat: send chain root for all headers for block proof

--- util/types/src/conversion/network.rs
+++ util/types/src/conversion/network.rs
@@ -4,3 +4,4 @@ impl_conversion_for_packed_iterator_pack!(IndexTransaction, IndexTransactionVec)
 impl_conversion_for_packed_iterator_pack!(RelayTransaction, RelayTransactionVec);
 impl_conversion_for_packed_iterator_pack!(Uint256, Uint256Vec);
 impl_conversion_for_packed_iterator_pack!(HeaderDigest, HeaderDigestVec);
+impl_conversion_for_packed_iterator_pack!(HeaderWithChainRoot, HeaderWithChainRootVec);
