1f50ce401d9876e8a82af11737c255678c6f50a2
Linfeng Qian <thewawar@gmail.com>
2022-06-15 12:42:16 +0800

    fix: create header from HeaderWithChainRoot

--- util/types/src/utilities/merkle_mountain_range.rs
+++ util/types/src/utilities/merkle_mountain_range.rs
@@ -323,15 +323,20 @@ impl VerifiableHeader {
     /// Creates a new verifiable header from a header with chain root.
     pub fn new_from_header_with_chain_root(
         header_with_chain_root: packed::HeaderWithChainRoot,
+        mmr_activated_number: BlockNumber,
     ) -> Self {
         let header = header_with_chain_root.header().into_view();
         let uncles_hash = header_with_chain_root.uncles_hash();
-        let bytes = header_with_chain_root
-            .chain_root()
-            .calc_mmr_hash()
-            .as_bytes()
-            .pack();
-        let extension = Some(bytes);
+        let extension = if header.number() >= mmr_activated_number {
+            let bytes = header_with_chain_root
+                .chain_root()
+                .calc_mmr_hash()
+                .as_bytes()
+                .pack();
+            Some(bytes)
+        } else {
+            None
+        };
         Self::new(header, uncles_hash, extension)
     }
 
