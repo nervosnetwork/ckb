77452c87a0e0cd92195e75e3fb7c0cf094d7f23e
Linfeng Qian <thewawar@gmail.com>
2022-06-20 14:05:22 +0800

    refactor: update light-client schema
    * Add `last_hash` to field to GetLastState message
    * Add `proof` and `chain_root` fields to SendLastState message
    * Rename GetBlockProof to GetBlockSamples
    * Rename SendBlockProof to SendBlockSamples

--- util/light-client-protocol-server/src/components/mod.rs
+++ util/light-client-protocol-server/src/components/mod.rs
@@ -1,5 +1,5 @@
-mod get_block_proof;
+mod get_block_samples;
 mod get_last_state;
 
-pub(crate) use get_block_proof::GetBlockProofProcess;
+pub(crate) use get_block_samples::GetBlockSamplesProcess;
 pub(crate) use get_last_state::GetLastStateProcess;
