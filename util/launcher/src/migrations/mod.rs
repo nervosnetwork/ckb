64f994258ccc498d2f8ec5201b0014bd13634321
quake <quake.wang@gmail.com>
2022-08-03 14:36:49 +0900

    chore: upgrade gcs filter lib and rebuild data

--- util/launcher/src/migrations/mod.rs
+++ util/launcher/src/migrations/mod.rs
@@ -4,6 +4,7 @@ mod add_chain_root_mmr;
 mod add_extra_data_hash;
 mod add_number_hash_mapping;
 mod cell;
+mod rebuild_block_filter;
 mod table_to_struct;
 
 pub use add_block_extension_cf::AddBlockExtensionColumnFamily;
@@ -12,4 +13,5 @@ pub use add_chain_root_mmr::AddChainRootMMR;
 pub use add_extra_data_hash::AddExtraDataHash;
 pub use add_number_hash_mapping::AddNumberHashMapping;
 pub use cell::CellMigration;
+pub use rebuild_block_filter::RebuildBlockFilter;
 pub use table_to_struct::ChangeMoleculeTableToStruct;
