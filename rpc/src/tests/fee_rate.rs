4155290e26f44529603c703005784215c7f209c4
zhangsoledad <787953403@qq.com>
2022-12-15 10:41:00 +0800

    fix: fee_rate statistics target limit

--- rpc/src/tests/fee_rate.rs
+++ rpc/src/tests/fee_rate.rs
@@ -6,13 +6,15 @@ use std::collections::HashMap;
 struct DummyFeeRateProvider {
     tip_number: BlockNumber,
     block_exts: HashMap<BlockNumber, BlockExt>,
+    max_target: u64,
 }
 
 impl DummyFeeRateProvider {
-    pub fn new() -> DummyFeeRateProvider {
+    pub fn new(max_target: u64) -> DummyFeeRateProvider {
         DummyFeeRateProvider {
             tip_number: 0,
             block_exts: HashMap::new(),
+            max_target,
         }
     }
 
@@ -21,6 +23,10 @@ impl DummyFeeRateProvider {
             self.tip_number = number;
         }
         self.block_exts.insert(number, ext);
+    }
+
+    pub fn set_max_target(&mut self, max_target: u64) {
+        self.max_target = max_target
     }
 }
 
@@ -32,11 +38,15 @@ impl FeeRateProvider for DummyFeeRateProvider {
     fn get_block_ext_by_number(&self, number: BlockNumber) -> Option<BlockExt> {
         self.block_exts.get(&number).cloned()
     }
+
+    fn max_target(&self) -> u64 {
+        self.max_target
+    }
 }
 
 #[test]
 fn test_fee_rate_statics() {
-    let mut provider = DummyFeeRateProvider::new();
+    let mut provider = DummyFeeRateProvider::new(30);
     for i in 0..=21 {
         let ext = BlockExt {
             received_at: 0,
@@ -83,6 +93,18 @@ fn test_fee_rate_statics() {
         Some(FeeRateStatics {
             mean: 21_000.into(),
             median: 21_000.into(),
+        })
+    );
+
+    provider.set_max_target(10);
+    let statistics11 = FeeRateCollector::new(&provider).statistics(Some(11));
+    let statistics12 = FeeRateCollector::new(&provider).statistics(Some(12));
+    assert_eq!(statistics11, statistics12);
+    assert_eq!(
+        statistics11,
+        Some(FeeRateStatics {
+            mean: 16500.into(),
+            median: 16500.into(),
         })
     );
 }
