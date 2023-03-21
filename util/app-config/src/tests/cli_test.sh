8031cfc6c34cb1a082d1b7acd058c15174e03572
Eval EXEC <execvy@gmail.com>
2023-03-21 12:32:44 +0800

    ci: upgrade bats-core/bats and related tools version
    Signed-off-by: Eval EXEC <execvy@gmail.com>

--- util/app-config/src/tests/cli_test.sh
+++ util/app-config/src/tests/cli_test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+
 export PATH=$PATH:/tmp/bats_testbed
-export BATS_LIB_PATH=/usr/lib
+export BATS_LIB_PATH=/tmp/bats_testbed/bats-core/test_helper/
 export CKB_DIRNAME=/tmp/bats_testbed
 export TMP_DIR=/tmp
 
 for bats_cases in *.bats; do
-  bats "$bats_cases"
+  bats --trace "$bats_cases"
   ret=$?
   if [ "$ret" -ne "0" ]; then
     exit "$ret"
