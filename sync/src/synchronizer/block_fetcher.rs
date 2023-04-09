bd2d19a160ff09f100cc3f5f416721454dd91c0f
Eval EXEC <execvy@gmail.com>
2023-04-09 14:51:24 +0800

    docs: Improve code comments for sync/synchronizer
    Signed-off-by: Eval EXEC <execvy@gmail.com>

--- sync/src/synchronizer/block_fetcher.rs
+++ sync/src/synchronizer/block_fetcher.rs
@@ -43,7 +43,7 @@ impl<'a> BlockFetcher<'a> {
         &self,
         best_known: &BlockNumberAndHash,
     ) -> Option<BlockNumberAndHash> {
-        // Bootstrap quickly by guessing a parent of our best tip is t
+        // Bootstrap quickly by guessing an ancestor of our best tip is forking point.
         // Guessing wrong in either direction is not a problem.
         let mut last_common =
             if let Some(header) = self.synchronizer.peers().get_last_common_header(self.peer) {
