cf4dd8ccc5aedc61e763416f2441af4500eac905
zhangsoledad <787953403@qq.com>
2023-04-23 16:50:09 +0800

    feat: ckb2023 config init

--- util/types/src/core/hardfork/ckb2023.rs
+++ util/types/src/core/hardfork/ckb2023.rs
@@ -1,5 +1,6 @@
-// use crate::core::EpochNumber;
-// use paste::paste;
+use crate::core::EpochNumber;
+use ckb_constant::hardfork;
+use paste::paste;
 
 /// A switch to select hard fork features base on the epoch number.
 ///
@@ -8,13 +9,19 @@
 ///
 /// [`CKB2023Builder`]:  struct.CKB2023Builder.html
 #[derive(Debug, Clone)]
-pub struct CKB2023 {}
+pub struct CKB2023 {
+    rfc_0146: EpochNumber,
+    rfc_0148: EpochNumber,
+}
 
 /// Builder for [`CKB2023`].
 ///
 /// [`CKB2023`]:  struct.CKB2023.html
 #[derive(Debug, Clone, Default)]
-pub struct CKB2023Builder {}
+pub struct CKB2023Builder {
+    rfc_0146: Option<EpochNumber>,
+    rfc_0148: Option<EpochNumber>,
+}
 
 impl CKB2023 {
     /// Creates a new builder to build an instance.
@@ -25,8 +32,37 @@ impl CKB2023 {
     /// Creates a new builder based on the current instance.
     pub fn as_builder(&self) -> CKB2023Builder {
         Self::new_builder()
+            .rfc_0146(self.rfc_0146())
+            .rfc_0148(self.rfc_0148())
+    }
+
+    /// Creates a new mirana instance.
+    pub fn new() -> Self {
+        // Use a builder to ensure all features are set manually.
+        Self::new_builder()
+            .rfc_0146(hardfork::mainnet::CKB2023_START_EPOCH)
+            .rfc_0148(hardfork::mainnet::CKB2023_START_EPOCH)
+            .build()
+            .unwrap()
     }
 }
+
+define_methods!(
+    CKB2023,
+    rfc_0146,
+    remove_header_version_reservation_rule,
+    is_remove_header_version_reservation_rule_enabled,
+    disable_rfc_0146,
+    "RFC PR 0146"
+);
+define_methods!(
+    CKB2023,
+    rfc_0148,
+    vm_version_2_and_syscalls_3,
+    is_vm_version_2_and_syscalls_3_enabled,
+    disable_rfc_0148,
+    "RFC PR 0148"
+);
 
 impl CKB2023Builder {
     /// Build a new [`CKB2023`].
@@ -36,6 +72,9 @@ impl CKB2023Builder {
     ///
     /// [`CKB2023`]: struct.CKB2023.html
     pub fn build(self) -> Result<CKB2023, String> {
-        Ok(CKB2023 {})
+        let rfc_0146 = try_find!(self, rfc_0146);
+        let rfc_0148 = try_find!(self, rfc_0148);
+
+        Ok(CKB2023 { rfc_0146, rfc_0148 })
     }
 }
