9d375c8693f09055ee8c86bbce29d16065e095c1
yukang <moorekang@gmail.com>
2023-06-06 10:53:29 +0800

    enable PoolResolveConflictAfterReorg

--- test/src/main.rs
+++ test/src/main.rs
@@ -406,6 +406,7 @@ fn all_specs() -> Vec<Box<dyn Spec>> {
         Box::new(GetRawTxPool),
         Box::new(PoolReconcile),
         Box::new(PoolResurrect),
+        Box::new(PoolResolveConflictAfterReorg),
         Box::new(InvalidHeaderDep),
         #[cfg(not(target_os = "windows"))]
         Box::new(PoolPersisted),
