6f3ca122e25a8132a75104a1b2251fa298cdc02c
mohanson <mohanson@outlook.com>
2023-07-06 10:20:40 +0800

    User more strict checking in spawn_callee_current_cycles

--- script/testdata/spawn_callee_current_cycles.c
+++ script/testdata/spawn_callee_current_cycles.c
@@ -6,9 +6,9 @@
 
 int main(int argc, char *argv[]) {
   int caller_cycles = atoi(argv[0]);
-  // Callee's current cycles must >= caller's current cycles.
+  // Callee's current cycles must > caller's current cycles.
   int callee_cycles = ckb_current_cycles();
-  if (callee_cycles < caller_cycles) {
+  if (callee_cycles < caller_cycles + 100000) {
     return 1;
   }
   return 0;
