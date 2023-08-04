144faeb0a2265568e7bb7db9a5ba15421c444095
Eval EXEC <execvy@gmail.com>
2023-08-04 14:29:51 +0800

    Remove `SEARCH_ORPHAN_POOL_TOKEN`

--- sync/src/relayer/mod.rs
+++ sync/src/relayer/mod.rs
@@ -50,7 +50,6 @@ use std::time::{Duration, Instant};
 pub const TX_PROPOSAL_TOKEN: u64 = 0;
 pub const ASK_FOR_TXS_TOKEN: u64 = 1;
 pub const TX_HASHES_TOKEN: u64 = 2;
-pub const SEARCH_ORPHAN_POOL_TOKEN: u64 = 3;
 
 pub const MAX_RELAY_PEERS: usize = 128;
 pub const MAX_RELAY_TXS_NUM_PER_BATCH: usize = 32767;
@@ -788,10 +787,6 @@ impl CKBProtocolHandler for Relayer {
         nc.set_notify(Duration::from_millis(300), TX_HASHES_TOKEN)
             .await
             .expect("set_notify at init is ok");
-        // todo: remove when the asynchronous verification is completed
-        nc.set_notify(Duration::from_secs(5), SEARCH_ORPHAN_POOL_TOKEN)
-            .await
-            .expect("set_notify at init is ok");
     }
 
     async fn received(
@@ -940,9 +935,6 @@ impl CKBProtocolHandler for Relayer {
                 if nc.remove_notify(TX_HASHES_TOKEN).await.is_err() {
                     trace_target!(crate::LOG_TARGET_RELAY, "remove v2 relay notify fail");
                 }
-                if nc.remove_notify(SEARCH_ORPHAN_POOL_TOKEN).await.is_err() {
-                    trace_target!(crate::LOG_TARGET_RELAY, "remove v2 relay notify fail");
-                }
                 for kv_pair in self.shared().state().peers().state.iter() {
                     let (peer, state) = kv_pair.pair();
                     if !state.peer_flags.is_2023edition {
@@ -962,14 +954,6 @@ impl CKBProtocolHandler for Relayer {
             }
             ASK_FOR_TXS_TOKEN => self.ask_for_txs(nc.as_ref()),
             TX_HASHES_TOKEN => self.send_bulk_of_tx_hashes(nc.as_ref()),
-            // SEARCH_ORPHAN_POOL_TOKEN => {
-            //     if !self.shared.state().orphan_pool().is_empty() {
-            //         tokio::task::block_in_place(|| {
-            //             self.shared.try_search_orphan_pool(&self.chain);
-            //             self.shared.periodic_clean_orphan_pool();
-            //         })
-            //     }
-            // }
             _ => unreachable!(),
         }
         trace_target!(
