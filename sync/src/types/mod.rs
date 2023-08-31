830e0de4f1e1b1556108c1aef6a1f6b2aa807445
Eval EXEC <execvy@gmail.com>
2023-08-31 22:09:12 +0800

    Add debug log for protocol disconnect

--- sync/src/types/mod.rs
+++ sync/src/types/mod.rs
@@ -1727,6 +1727,7 @@ impl SyncState {
     pub fn disconnected(&self, pi: PeerIndex) {
         self.write_inflight_blocks().remove_by_peer(pi);
         self.peers().disconnected(pi);
+        debug!("peer {} disconnected", pi);
     }
 
     // pub fn get_orphan_block(&self, block_hash: &Byte32) -> Option<core::BlockView> {
