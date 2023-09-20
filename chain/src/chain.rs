8fefebcd1a034a8243fa33bbf13ac93306971ae8
Eval EXEC <execvy@gmail.com>
2023-09-20 12:56:40 +0800

    Fix ChainService error handle

--- chain/src/chain.rs
+++ chain/src/chain.rs
@@ -523,7 +523,9 @@ impl ChainService {
 
                             match unverified_block_tx.send(unverified_block) {
                                 Ok(_) => {}
-                                Err(err) => error!("send unverified_block_tx failed: {}", err),
+                                Err(err) => {
+                                    error!("send unverified_block_tx failed: {}", err)
+                                }
                             };
 
                             if total_difficulty
@@ -694,11 +696,9 @@ impl ChainService {
             Err(SendError(lonely_block)) => {
                 error!("notify new block to orphan pool err: {}", err);
                 if let Some(verify_callback) = lonely_block.verify_callback {
-                    verify_callback(
-                        InternalErrorKind::System
-                            .other("OrphanBlock broker disconnected")
-                            .into(),
-                    );
+                    verify_callback(Err(InternalErrorKind::System
+                        .other("OrphanBlock broker disconnected")
+                        .into()));
                 }
             }
         }
