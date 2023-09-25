641207b9f81918ff29ebc880f77d2ec00eb30e27
yukang <moorekang@gmail.com>
2023-09-25 14:36:22 +0800

    code refactor on rpc server

--- rpc/src/server.rs
+++ rpc/src/server.rs
@@ -2,8 +2,9 @@ use crate::IoHandler;
 use axum::routing::post;
 use axum::{Extension, Router};
 use ckb_app_config::RpcConfig;
+use ckb_error::AnyError;
 use ckb_logger::info;
-use ckb_notify::NotifyController;
+
 use ckb_stop_handler::{new_tokio_exit_rx, CancellationToken};
 use futures_util::{SinkExt, TryStreamExt};
 use jsonrpc_core::MetaIoHandler;
@@ -14,7 +15,7 @@ use std::net::{SocketAddr, ToSocketAddrs};
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::TcpListener;
-use tokio::runtime::Handle;
+
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec, LinesCodecError};
 use tower_http::timeout::TimeoutLayer;
 
@@ -33,75 +34,36 @@ impl RpcServer {
     ///
     /// * `config` - RPC config options.
     /// * `io_handler` - RPC methods handler. See [ServiceBuilder](../service_builder/struct.ServiceBuilder.html).
-    /// * `notify_controller` - Controller emitting notifications.
-    pub async fn new(
-        config: RpcConfig,
-        io_handler: IoHandler,
-        _handle: Handle,
-        _notify_controller: &NotifyController,
-    ) -> Self {
+    pub async fn new(config: RpcConfig, io_handler: IoHandler) -> Self {
         let rpc = Arc::new(io_handler);
 
-        let http_address = Self::start_server(&rpc, config.listen_address.to_owned()).await;
-        info!("Listen HTTP RPCServer on address {}", http_address);
+        let http_address = Self::start_server(&rpc, config.listen_address.to_owned())
+            .await
+            .and_then(|addr| {
+                info!("Listen HTTP RPCServer on address: {}", addr);
+                Ok(addr)
+            })
+            .unwrap();
 
         let ws_address = if let Some(addr) = config.ws_listen_address {
-            let local_addr = Self::start_server(&rpc, addr).await;
-            info!("Listen WebSocket RPCServer on address {}", local_addr);
-            Some(local_addr)
+            let r = Self::start_server(&rpc, addr).await.and_then(|addr| {
+                info!("Listen WebSocket RPCServer on address: {}", addr);
+                Ok(addr)
+            });
+            r.ok()
         } else {
             None
         };
 
-        // TCP server with line delimited json codec.
-        let mut tcp_address = None;
-        if let Some(tcp_listen_address) = config.tcp_listen_address {
-            let listener = TcpListener::bind(tcp_listen_address).await.unwrap();
-            tcp_address = listener.local_addr().ok();
-            info!("listen TCP RPCServer on address {:?}", tcp_address.unwrap());
-            tokio::spawn(async move {
-                let codec = LinesCodec::new_with_max_length(2 * 1024 * 1024);
-                let stream_config = StreamServerConfig::default()
-                    .with_channel_size(4)
-                    .with_keep_alive(true)
-                    .with_keep_alive_duration(Duration::from_secs(60))
-                    .with_pipeline_size(4);
-
-                let exit_signal: CancellationToken = new_tokio_exit_rx();
-                tokio::select! {
-                    _ = async {
-                            while let Ok((stream, _)) = listener.accept().await {
-                                let rpc = Arc::clone(&rpc);
-                                let stream_config = stream_config.clone();
-                                let codec = codec.clone();
-                                tokio::spawn(async move {
-                                    let (r, w) = stream.into_split();
-                                    let r = FramedRead::new(r, codec.clone()).map_ok(StreamMsg::Str);
-                                    let w = FramedWrite::new(w, codec).with(|msg| async move {
-                                        Ok::<_, LinesCodecError>(match msg {
-                                            StreamMsg::Str(msg) => msg,
-                                            _ => "".into(),
-                                        })
-                                    });
-                                    tokio::pin!(w);
-                                    let exit_signal: CancellationToken = new_tokio_exit_rx();
-                                    tokio::select! {
-                                        result = serve_stream_sink(&rpc, w, r, stream_config) => {
-                                            if let Err(err) = result {
-                                                info!("TCP RPCServer error: {:?}", err);
-                                            }
-                                        }
-                                        _ = exit_signal.cancelled() => {}
-                                    }
-                                });
-                            }
-                        } => {},
-                    _ = exit_signal.cancelled() => {
-                        info!("TCP RPCServer stopped");
-                    }
-                }
+        let tcp_address = if let Some(addr) = config.tcp_listen_address {
+            let local_addr = Self::start_tcp_server(rpc, addr).await.and_then(|addr| {
+                info!("Listen TCP RPCServer on address: {}", addr);
+                Ok(addr)
             });
-        }
+            local_addr.ok()
+        } else {
+            None
+        };
 
         Self {
             http_address,
@@ -113,7 +75,7 @@ impl RpcServer {
     async fn start_server(
         rpc: &Arc<MetaIoHandler<Option<Session>>>,
         address: String,
-    ) -> SocketAddr {
+    ) -> Result<SocketAddr, AnyError> {
         let stream_config = StreamServerConfig::default()
             .with_channel_size(4)
             .with_pipeline_size(4);
@@ -131,7 +93,7 @@ impl RpcServer {
             .layer(TimeoutLayer::new(Duration::from_secs(30)));
 
         let (tx_addr, rx_addr) = tokio::sync::oneshot::channel::<SocketAddr>();
-        let _http = tokio::spawn({
+        tokio::spawn({
             async move {
                 let server = axum::Server::bind(
                     &address
@@ -147,9 +109,61 @@ impl RpcServer {
                     let exit = new_tokio_exit_rx();
                     exit.cancelled().await;
                 });
-                graceful.await.unwrap();
+                drop(graceful.await);
             }
         });
-        rx_addr.await.unwrap()
+        Ok(rx_addr.await?)
+    }
+
+    async fn start_tcp_server(
+        rpc: Arc<MetaIoHandler<Option<Session>>>,
+        tcp_listen_address: String,
+    ) -> Result<SocketAddr, AnyError> {
+        // TCP server with line delimited json codec.
+        let listener = TcpListener::bind(tcp_listen_address).await?;
+        let tcp_address = listener.local_addr()?;
+        tokio::spawn(async move {
+            let codec = LinesCodec::new_with_max_length(2 * 1024 * 1024);
+            let stream_config = StreamServerConfig::default()
+                .with_channel_size(4)
+                .with_keep_alive(true)
+                .with_keep_alive_duration(Duration::from_secs(60))
+                .with_pipeline_size(4);
+
+            let exit_signal: CancellationToken = new_tokio_exit_rx();
+            tokio::select! {
+                _ = async {
+                        while let Ok((stream, _)) = listener.accept().await {
+                            let rpc = Arc::clone(&rpc);
+                            let stream_config = stream_config.clone();
+                            let codec = codec.clone();
+                            tokio::spawn(async move {
+                                let (r, w) = stream.into_split();
+                                let r = FramedRead::new(r, codec.clone()).map_ok(StreamMsg::Str);
+                                let w = FramedWrite::new(w, codec).with(|msg| async move {
+                                    Ok::<_, LinesCodecError>(match msg {
+                                        StreamMsg::Str(msg) => msg,
+                                        _ => "".into(),
+                                    })
+                                });
+                                tokio::pin!(w);
+                                let exit_signal: CancellationToken = new_tokio_exit_rx();
+                                tokio::select! {
+                                    result = serve_stream_sink(&rpc, w, r, stream_config) => {
+                                        if let Err(err) = result {
+                                            info!("TCP RPCServer error: {:?}", err);
+                                        }
+                                    }
+                                    _ = exit_signal.cancelled() => {}
+                                }
+                            });
+                        }
+                    } => {},
+                _ = exit_signal.cancelled() => {
+                    info!("TCP RPCServer stopped");
+                }
+            }
+        });
+        Ok(tcp_address)
     }
 }
