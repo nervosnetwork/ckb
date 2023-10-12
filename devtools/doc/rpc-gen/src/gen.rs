9e7e89a9c3f534e110e60e82e90ed27acfc0015a
yukang <moorekang@gmail.com>
2023-10-13 01:41:27 +0800

    use tera as template

--- devtools/doc/rpc-gen/src/gen.rs
+++ devtools/doc/rpc-gen/src/gen.rs
@@ -1,123 +1,118 @@
+extern crate tera;
 use ckb_rpc::RPCError;
 use schemars::schema_for;
 use serde_json::{Map, Value};
 use std::fs;
+use tera::Tera;
 
 struct RpcModule {
-    pub module_title: String,
-    pub module_description: String,
-    pub module_methods: Vec<serde_json::Value>,
+    pub title: String,
+    pub description: String,
+    pub methods: Vec<serde_json::Value>,
 }
 
 impl RpcModule {
-    pub fn gen_module_menu(&self) -> String {
-        let mut res = String::new();
-        let capitlized = self.module_title.to_string();
-        res.push_str(&format!(
-            "    * [Module {}](#module-{}) {}\n",
-            capitlized,
-            self.module_title.to_lowercase(),
-            gen_module_openrpc_playground(&capitlized)
-        ));
+    pub fn gen_menu(&self) -> (String, String, String, Vec<String>) {
+        let capitlized = self.title.to_string();
         let mut method_names = self
-            .module_methods
+            .methods
             .iter()
-            .map(|method| method["name"].as_str().unwrap())
+            .map(|method| method["name"].as_str().unwrap().to_owned())
             .collect::<Vec<_>>();
         if capitlized == "Subscription" {
-            method_names.push("subscribe");
-            method_names.push("unsubscribe");
+            method_names.push("subscribe".to_owned());
+            method_names.push("unsubscribe".to_owned());
         }
-        for name in method_names.iter() {
-            res.push_str(&format!(
-                "        * [Method `{}`](#method-{})\n",
-                name, name
-            ));
-        }
-        res
+        (
+            capitlized.to_owned(),
+            self.title.to_lowercase(),
+            gen_module_openrpc_playground(&capitlized),
+            method_names,
+        )
     }
 
     pub fn gen_module_content(&self) -> String {
-        let mut res = String::new();
-        let capitlized = self.module_title.to_string();
-        let description = self.module_description.replace("##", "#####");
-
-        res.push_str(&format!("### Module {}\n", capitlized));
-        res.push_str(&format!(
-            "- {}\n\n",
-            gen_module_openrpc_playground(&capitlized)
-        ));
-        res.push_str(&format!("{}\n\n", description));
-
-        for method in &self.module_methods {
-            // generate method signatures
-            let name = method["name"].as_str().unwrap();
-            let params = method["params"].as_array().unwrap();
-            let args = params
-                .iter()
-                .map(|arg| arg["name"].as_str().unwrap())
-                .collect::<Vec<_>>()
-                .join(", ");
-            let arg_lines = params
-                .iter()
-                .map(|arg| {
-                    let ty = gen_type(&arg["schema"]);
-                    format!("    * `{}`: {}", arg["name"].as_str().unwrap(), ty)
-                })
-                .collect::<Vec<_>>()
-                .join("\n");
-            let ret_ty = if let Some(value) = method.get("result") {
-                format!("* result: {}", gen_type(&value["schema"]))
-            } else {
-                "".to_string()
-            };
-            let signatures = format!("* `{}({})`\n{}\n{}", name, args, arg_lines, ret_ty);
-            let mut desc = method["description"]
-                .as_str()
-                .unwrap()
-                .replace("##", "######");
-            desc = strip_prefix_space(&desc);
-            res.push_str(&format!(
-                "#### Method `{}`\n{}\n\n{}\n",
-                name, signatures, desc,
-            ));
+        if self.title == "Subscription" {
+            return gen_subscription_rpc_doc();
         }
-        res + "\n"
+        let capitlized = self.title.to_string();
+        let description = self.description.replace("##", "#####");
+
+        let methods = self
+            .methods
+            .iter()
+            .map(|method| {
+                // generate method signatures
+                let name = method["name"].as_str().unwrap();
+                let params = method["params"].as_array().unwrap();
+                let args = params
+                    .iter()
+                    .map(|arg| arg["name"].as_str().unwrap())
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                let arg_lines = params
+                    .iter()
+                    .map(|arg| {
+                        let ty = gen_type(&arg["schema"]);
+                        format!("    * `{}`: {}", arg["name"].as_str().unwrap(), ty)
+                    })
+                    .collect::<Vec<_>>()
+                    .join("\n");
+                let ret_ty = if let Some(value) = method.get("result") {
+                    format!("* result: {}", gen_type(&value["schema"]))
+                } else {
+                    "".to_string()
+                };
+                let signatures = format!("* `{}({})`\n{}\n{}", name, args, arg_lines, ret_ty);
+                let mut desc = method["description"]
+                    .as_str()
+                    .unwrap()
+                    .replace("##", "######");
+                desc = strip_prefix_space(&desc);
+                format!("#### Method `{}`\n{}\n\n{}\n", name, signatures, desc,)
+            })
+            .collect::<Vec<_>>();
+        let mut context = tera::Context::new();
+        context.insert("name", &capitlized);
+        context.insert("link", &gen_module_openrpc_playground(&capitlized));
+        context.insert("desc", &description);
+        context.insert("methods", &methods);
+        render_tera(include_str!("../templates/module"), &context)
     }
 }
 
 pub(crate) struct RpcDocGenerator {
-    rpc_module_methods: Vec<RpcModule>,
+    rpc_methods: Vec<RpcModule>,
     types: Vec<(String, Value)>,
     file_path: String,
 }
 
 impl RpcDocGenerator {
     pub fn new(all_rpc: &Vec<Value>, readme_path: String) -> Self {
-        let mut rpc_module_methods = vec![];
+        let mut rpc_methods = vec![];
         let mut all_types: Vec<&Map<String, Value>> = vec![];
         for rpc in all_rpc {
             if let serde_json::Value::Object(map) = rpc {
-                let module_title = capitlize(
+                let title = capitlize(
                     map["info"]["title"]
                         .as_str()
                         .unwrap()
                         .trim_end_matches("_rpc"),
                 );
-                let module_description = get_description(&map["info"]["description"]);
-                let module_methods = map["methods"].as_array().unwrap();
+                let description = get_description(&map["info"]["description"]);
+                let methods = map["methods"].as_array().unwrap();
                 let types = map["components"]["schemas"].as_object().unwrap();
                 all_types.push(types);
-                rpc_module_methods.push(RpcModule {
-                    module_title,
-                    module_description,
-                    module_methods: module_methods.to_owned(),
+                rpc_methods.push(RpcModule {
+                    title,
+                    description,
+                    methods: methods.to_owned(),
                 });
             }
         }
 
-        // sort rpc_module_methods accoring to module_title
-        rpc_module_methods.sort_by(|a, b| a.module_title.cmp(&b.module_title));
+        // sort rpc_methods accoring to title
+        rpc_methods.sort_by(|a, b| a.title.cmp(&b.title));
 
         let mut types: Vec<(String, Value)> = vec![];
         for map in all_types.iter() {
@@ -130,85 +125,79 @@ impl RpcDocGenerator {
         types.sort_by(|(name1, _), (name2, _)| name1.cmp(name2));
 
         Self {
-            rpc_module_methods,
+            rpc_methods,
             types,
             file_path: readme_path,
         }
     }
 
     pub fn gen_markdown(self) -> String {
-        let mut res = String::new();
-
-        // strip lines below `**NOTE:**`
         let readme = fs::read_to_string(&self.file_path).unwrap_or("".to_string());
         let lines = readme.lines().collect::<Vec<_>>();
-        for &line in lines.iter() {
-            res.push_str(&(line.to_string() + "\n"));
-            if line.contains("**NOTE:** the content below is generated by gen_doc") {
-                break;
-            }
-        }
+        let summary = lines
+            .iter()
+            .take_while(|l| !l.contains("**NOTE:** the content below is generated by gen_doc"))
+            .map(|l| l.to_string())
+            .collect::<Vec<_>>()
+            .join("\n");
 
         // generate methods menu
-        res.push_str("\n\n* [RPC Methods](#rpc-methods)\n");
-        for rpc_module in self.rpc_module_methods.iter() {
-            res.push_str(&rpc_module.gen_module_menu());
-        }
+        let module_menus = self
+            .rpc_methods
+            .iter()
+            .map(|r| r.gen_menu())
+            .collect::<Vec<_>>();
 
-        // generate type menu
-        res.push_str("* [RPC Types](#rpc-types)\n");
-        for (name, _) in self.types.iter() {
-            let ty = format!(
-                "    * [Type `{}`](#type-{})\n",
-                capitlize(name),
-                name.to_lowercase()
-            );
-            res.push_str(&ty);
-        }
-        // generate error menu
-        res.push_str("* [RPC Errors](#rpc-errors)\n");
+        let type_menus = self
+            .types
+            .iter()
+            .map(|(name, _)| (capitlize(name), name.to_lowercase()))
+            .collect::<Vec<_>>();
 
         // generate module methods content
-        for rpc_module in self.rpc_module_methods.iter() {
-            if rpc_module.module_title == "Subscription" {
-                gen_subscription_rpc_doc(&mut res);
-            } else {
-                res.push_str(&rpc_module.gen_module_content());
-            }
-        }
+        let modules = self
+            .rpc_methods
+            .iter()
+            .map(|r| r.gen_module_content())
+            .collect::<Vec<_>>();
 
-        // generate type content
-        res.push_str("## RPC Types\n");
-        self.gen_type_content(&mut res);
+        let types = self.gen_type_contents();
 
-        gen_errors_content(&mut res);
-        res
+        let mut context = tera::Context::new();
+        context.insert("summary", &summary);
+        context.insert("module_menus", &module_menus);
+        context.insert("type_menus", &type_menus);
+        context.insert("modules", &modules);
+        context.insert("types", &types);
+        context.insert("errors", &gen_errors_contents());
+        render_tera(include_str!("../templates/markdown"), &context)
     }
 
-    fn gen_type_content(&self, res: &mut String) {
-        for (name, ty) in self.types.iter() {
-            let desc = if let Some(desc) = ty.get("description") {
-                desc.as_str().unwrap().to_string()
-            } else if let Some(desc) = ty.get("format") {
-                format!("`{}` is `{}`", name, desc.as_str().unwrap())
-            } else {
-                "".to_string()
-            };
-            let desc = desc.replace("##", "######");
-            // remove the inline code from comments
-            let desc = desc
-                .lines()
-                .filter(|l| !l.contains("serde_json::from_str") && !l.contains(".unwrap()"))
-                .collect::<Vec<_>>()
-                .join("\n");
+    fn gen_type_contents(&self) -> Vec<(String, String, String)> {
+        self.types
+            .iter()
+            .map(|(name, ty)| {
+                let desc = if let Some(desc) = ty.get("description") {
+                    desc.as_str().unwrap().to_string()
+                } else if let Some(desc) = ty.get("format") {
+                    format!("`{}` is `{}`", name, desc.as_str().unwrap())
+                } else {
+                    "".to_string()
+                };
+                let desc = desc.replace("##", "######");
+                // remove the inline code from comments
+                let desc = desc
+                    .lines()
+                    .filter(|l| !l.contains("serde_json::from_str") && !l.contains(".unwrap()"))
+                    .collect::<Vec<_>>()
+                    .join("\n");
 
-            // replace only the first ``` with ```json
-            let desc = desc.replacen("```\n", "```json\n", 1);
-
-            let fileds = gen_type_fields(ty);
-            let type_desc = format!("### Type `{}`\n{}\n{}\n\n", capitlize(name), desc, fileds);
-            res.push_str(&type_desc);
-        }
+                // replace only the first ``` with ```json
+                let desc = desc.replacen("```\n", "```json\n", 1);
+                let fields = gen_type_fields(ty);
+                (capitlize(name), desc, fields)
+            })
+            .collect::<Vec<_>>()
     }
 }
 
@@ -323,10 +312,8 @@ fn gen_type(ty: &Value) -> String {
                     } else {
                         gen_type(&map["items"])
                     }
-                } else if ty.as_array().is_some() {
-                    let ty = ty
-                        .as_array()
-                        .unwrap()
+                } else if let Some(arr) = ty.as_array() {
+                    let ty = arr
                         .iter()
                         .map(|t| format!("`{}`", gen_type(t)))
                         .collect::<Vec<_>>()
@@ -335,9 +322,8 @@ fn gen_type(ty: &Value) -> String {
                 } else {
                     format!("`{}`", ty.as_str().unwrap())
                 }
-            } else if map.get("anyOf").is_some() {
-                map["anyOf"]
-                    .as_array()
+            } else if let Some(arr) = map.get("anyOf") {
+                arr.as_array()
                     .unwrap()
                     .iter()
                     .map(gen_type)
@@ -358,24 +344,26 @@ fn gen_type(ty: &Value) -> String {
     }
 }
 
-fn gen_errors_content(res: &mut String) {
+fn gen_errors_contents() -> (String, Vec<(String, String)>) {
     let schema = schema_for!(RPCError);
     let value = serde_json::to_value(schema).unwrap();
     let summary = get_description(&value["description"]);
-    res.push_str("## RPC Errors\n");
-    res.push_str(&summary);
-
-    for error in value["oneOf"].as_array().unwrap().iter() {
-        let desc = get_description(&error["description"]);
-        let enum_ty = error["enum"].as_array().unwrap()[0].as_str().unwrap();
-        let doc = format!("\n### ERROR `{}`\n{}\n", enum_ty, desc);
-        res.push_str(&doc);
-    }
+    let errors = value["oneOf"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|error| {
+            let desc = get_description(&error["description"]);
+            let enum_ty = error["enum"].as_array().unwrap()[0].as_str().unwrap();
+            (enum_ty.to_string(), desc)
+        })
+        .collect::<Vec<_>>();
+    (summary, errors)
 }
 
 /// generate subscription module, which is handled specially here
 /// since jsonrpc-utils ignore the `SubscriptionRpc`
-fn gen_subscription_rpc_doc(res: &mut String) {
+fn gen_subscription_rpc_doc() -> String {
     let pubsub_module_source = include_str!("../../../../rpc/src/module/subscription.rs");
     // read comments before `pub trait SubscriptionRpc` and treat it as module summary
     let summary = pubsub_module_source
@@ -405,37 +393,20 @@ fn gen_subscription_rpc_doc(res: &mut String) {
         .join("\n");
     let sub_desc = strip_prefix_space(&sub_desc);
 
-    res.push_str(format!("{}\n\n", summary).as_str());
-    res.push_str(format!("{}\n", sub_desc).as_str());
+    format!("{}\n\n{}\n", summary, sub_desc)
 }
 
 /// generate openrpc playground urls
 fn gen_module_openrpc_playground(module: &str) -> String {
-    let logo = "https://raw.githubusercontent.com/chenyukang/ckb-rpc-resources/main/ckb-logo.jpg";
     let title = format!("CKB-{}", capitlize(module));
-    let json_url = format!(
-        "https://raw.githubusercontent.com/chenyukang/ckb-rpc-resources/main/json/{}_rpc_doc.json",
-        module.to_lowercase()
-    );
-
-    format!("[👉 OpenRPC spec](http://playground.open-rpc.org/?uiSchema[appBar][ui:title]={}&uiSchema[appBar][ui:splitView]=false", title) +
-    format!("&uiSchema[appBar][ui:examplesDropdown]=false&uiSchema[appBar][ui:logoUrl]={}&schemaUrl={})", logo, json_url).as_str()
+    let mut context = tera::Context::new();
+    context.insert("title", &title);
+    context.insert("module", &module.to_lowercase());
+    render_tera(include_str!("../templates/link"), &context)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_strip_prefix_space() {
-        assert_eq!(strip_prefix_space("   "), "".to_string());
-        assert_eq!(
-            strip_prefix_space("   \n   abc\n   cdf"),
-            "\nabc\ncdf".to_string()
-        );
-        assert_eq!(
-            strip_prefix_space("abc\n   cde\n  cdf"),
-            "abc\n   cde\n  cdf".to_string()
-        );
-    }
+fn render_tera(template: &str, context: &tera::Context) -> String {
+    let mut tera = Tera::default();
+    tera.add_raw_template("template", template).unwrap();
+    tera.render("template", context).unwrap()
 }
