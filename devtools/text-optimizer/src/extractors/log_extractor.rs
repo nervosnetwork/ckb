bdcd1645cb78d055e750cbfda3be4589340fb358
EthanYuan <yuanchengxing@hotmail.com>
2023-10-14 20:48:20 +0800

    Add get_macro_name to recognize logs like ckb_logger::info

--- devtools/text-optimizer/src/extractors/log_extractor.rs
+++ devtools/text-optimizer/src/extractors/log_extractor.rs
@@ -5,12 +5,12 @@ use syn::Macro;
 
 impl syn::visit::Visit<'_> for LogExtractor {
     fn visit_macro(&mut self, node: &Macro) {
-        if let Some(ident) = node.path.get_ident() {
-            if ident == "error"
-                || ident == "warn"
-                || ident == "info"
-                || ident == "debug"
-                || ident == "trace"
+        if let Some(name) = get_macro_name(node) {
+            if name == "error"
+                || name == "warn"
+                || name == "info"
+                || name == "debug"
+                || name == "trace"
             {
                 if let Some(lit) = node.tokens.clone().into_iter().next() {
                     if let Some(text) = extract_contents_in_brackets(lit.to_string()) {
@@ -18,12 +18,23 @@ impl syn::visit::Visit<'_> for LogExtractor {
 
                         let span = lit.span();
                         let start_line = span.start().line;
-                        let category = Category::from_str(ident.to_string().as_str()).unwrap();
+                        let category = Category::from_str(&name).unwrap();
                         let meta = Meta::new(category, self.file_path.to_owned(), start_line);
                         self.add_text_info(TextInfo::new(text, meta));
                     }
                 }
             }
         }
+    }
+}
+
+fn get_macro_name(node: &Macro) -> Option<String> {
+    if let Some(ident) = node.path.get_ident() {
+        Some(ident.to_string())
+    } else {
+        node.path
+            .segments
+            .last()
+            .map(|segment| segment.ident.to_string())
     }
 }
