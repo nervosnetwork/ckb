eabecd9889fd081d071deea5d821bbc54f3ca3ef
Eval EXEC <execvy@gmail.com>
2023-10-18 22:22:26 +0800

    Synchronizer should pass Switch::NONE to ChainService

--- sync/src/types/mod.rs
+++ sync/src/types/mod.rs
@@ -1207,11 +1207,10 @@ impl SyncShared {
         //     }
         // };
 
-        // TODO move switch logic to ckb-chain
         let lonely_block_with_callback = LonelyBlock {
             block,
             peer_id: Some(peer_id),
-            switch: Some(Switch::NONE),
+            switch: None,
         }
         .with_callback(verify_callback);
 
