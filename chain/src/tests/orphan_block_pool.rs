685387f2ecc9a20f1e935a1e43ae0cbadb8590e0
Eval EXEC <execvy@gmail.com>
2023-10-19 23:17:10 +0800

    Fix Unit test: chain/src/tests/orphan_block_pool.rs

--- chain/src/tests/orphan_block_pool.rs
+++ chain/src/tests/orphan_block_pool.rs
@@ -1,4 +1,4 @@
-use crate::chain::LonelyBlockWithCallback;
+use crate::chain::{LonelyBlock, LonelyBlockWithCallback};
 use ckb_chain_spec::consensus::ConsensusBuilder;
 use ckb_systemtime::unix_time_as_millis;
 use ckb_types::core::{BlockBuilder, BlockView, EpochNumberWithFraction, HeaderView};
@@ -9,7 +9,7 @@ use std::thread;
 
 use crate::orphan_block_pool::OrphanBlockPool;
 
-fn gen_lonely_block_with_callback(parent_header: &HeaderView) -> LonelyBlockWithCallback {
+fn gen_lonely_block(parent_header: &HeaderView) -> LonelyBlock {
     let number = parent_header.number() + 1;
     let block = BlockBuilder::default()
         .parent_hash(parent_header.hash())
@@ -18,14 +18,15 @@ fn gen_lonely_block_with_callback(parent_header: &HeaderView) -> LonelyBlockWith
         .epoch(EpochNumberWithFraction::new(number / 1000, number % 1000, 1000).pack())
         .nonce((parent_header.nonce() + 1).pack())
         .build();
-    LonelyBlockWithCallback {
-        lonely_block: LonelyBlock {
-            block: Arc::new(block),
-            peer_id: None,
-            switch: None,
-        },
-        verify_callback: None,
+    LonelyBlock {
+        block: Arc::new(block),
+        peer_id: None,
+        switch: None,
     }
+}
+
+fn gen_lonely_block_with_callback(parent_header: &HeaderView) -> LonelyBlockWithCallback {
+    gen_lonely_block(parent_header).without_callback()
 }
 
 #[test]
@@ -36,15 +37,18 @@ fn test_remove_blocks_by_parent() {
     let mut parent = consensus.genesis_block().header();
     let pool = OrphanBlockPool::with_capacity(200);
     for _ in 1..block_number {
-        let new_block = gen_lonely_block_with_callback(&parent);
-        blocks.push(new_block.clone());
-        pool.insert(new_block.clone());
-        parent = new_block.header();
+        let lonely_block = gen_lonely_block(&parent);
+        let new_block_clone = lonely_block.clone().without_callback();
+        let new_block = lonely_block.without_callback();
+        blocks.push(new_block_clone);
+
+        parent = new_block.block().header();
+        pool.insert(new_block);
     }
 
     let orphan = pool.remove_blocks_by_parent(&consensus.genesis_block().hash());
-    let orphan_set: HashSet<BlockView> = orphan.into_iter().collect();
-    let blocks_set: HashSet<BlockView> = blocks.into_iter().collect();
+    let orphan_set: HashSet<_> = orphan.into_iter().map(|b| b.lonely_block.block).collect();
+    let blocks_set: HashSet<_> = blocks.into_iter().map(|b| b.lonely_block.block).collect();
     assert_eq!(orphan_set, blocks_set)
 }
 
@@ -55,9 +59,11 @@ fn test_remove_blocks_by_parent_and_get_block_should_not_deadlock() {
     let mut header = consensus.genesis_block().header();
     let mut hashes = Vec::new();
     for _ in 1..1024 {
-        let new_block = gen_lonely_block_with_callback(&header);
-        pool.insert(new_block.clone());
-        header = new_block.header();
+        let lonely_block = gen_lonely_block(&header);
+        let new_block = lonely_block.clone().without_callback();
+        let new_block_clone = lonely_block.without_callback();
+        pool.insert(new_block_clone);
+        header = new_block.block().header();
         hashes.push(header.hash());
     }
 
@@ -83,22 +89,23 @@ fn test_leaders() {
     let mut parent = consensus.genesis_block().header();
     let pool = OrphanBlockPool::with_capacity(20);
     for i in 0..block_number - 1 {
-        let new_block = gen_lonely_block_with_callback(&parent);
-        blocks.push(new_block.clone());
-        parent = new_block.header();
+        let lonely_block = gen_lonely_block(&parent);
+        let new_block = lonely_block.clone().without_callback();
+        blocks.push(lonely_block);
+        parent = new_block.block().header();
         if i % 5 != 0 {
-            pool.insert(new_block.clone());
+            pool.insert(new_block);
         }
     }
 
     assert_eq!(pool.len(), 15);
     assert_eq!(pool.leaders_len(), 4);
 
-    pool.insert(blocks[5].clone());
+    pool.insert(blocks[5].clone().without_callback());
     assert_eq!(pool.len(), 16);
     assert_eq!(pool.leaders_len(), 3);
 
-    pool.insert(blocks[10].clone());
+    pool.insert(blocks[10].clone().without_callback());
     assert_eq!(pool.len(), 17);
     assert_eq!(pool.leaders_len(), 2);
 
@@ -108,7 +115,7 @@ fn test_leaders() {
     assert_eq!(pool.len(), 17);
     assert_eq!(pool.leaders_len(), 2);
 
-    pool.insert(blocks[0].clone());
+    pool.insert(blocks[0].clone().without_callback());
     assert_eq!(pool.len(), 18);
     assert_eq!(pool.leaders_len(), 2);
 
@@ -116,14 +123,18 @@ fn test_leaders() {
     assert_eq!(pool.len(), 3);
     assert_eq!(pool.leaders_len(), 1);
 
-    pool.insert(blocks[15].clone());
+    pool.insert(blocks[15].clone().without_callback());
     assert_eq!(pool.len(), 4);
     assert_eq!(pool.leaders_len(), 1);
 
-    let orphan_1 = pool.remove_blocks_by_parent(&blocks[14].hash());
+    let orphan_1 = pool.remove_blocks_by_parent(&blocks[14].block.hash());
 
-    let orphan_set: HashSet<BlockView> = orphan.into_iter().chain(orphan_1.into_iter()).collect();
-    let blocks_set: HashSet<BlockView> = blocks.into_iter().collect();
+    let orphan_set: HashSet<Arc<BlockView>> = orphan
+        .into_iter()
+        .map(|b| b.lonely_block.block)
+        .chain(orphan_1.into_iter().map(|b| b.lonely_block.block))
+        .collect();
+    let blocks_set: HashSet<Arc<BlockView>> = blocks.into_iter().map(|b| b.block).collect();
     assert_eq!(orphan_set, blocks_set);
     assert_eq!(pool.len(), 0);
     assert_eq!(pool.leaders_len(), 0);
