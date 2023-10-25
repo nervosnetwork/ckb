369249b4f893ec7870a1f43b57bc33147b6fbea2
Eval EXEC <execvy@gmail.com>
2023-10-25 11:03:09 +0800

    Fix Unit test: ckb-light-client-protocol-server::tests/utils/chain.rs

--- util/light-client-protocol-server/src/tests/utils/chain.rs
+++ util/light-client-protocol-server/src/tests/utils/chain.rs
@@ -87,7 +87,11 @@ impl MockChain {
         let network = dummy_network(&shared);
         pack.take_tx_pool_builder().start(network);
 
-        let chain_service = ChainService::new(shared.clone(), pack.take_proposal_table(), None);
+        let chain_service = ChainService::new(
+            shared.clone(),
+            pack.take_proposal_table(),
+            pack.take_verify_failed_block_tx(),
+        );
         let chain_controller = chain_service.start::<&str>(Some(
             "ckb-light-client-protocol-server::tests::ChainService",
         ));
