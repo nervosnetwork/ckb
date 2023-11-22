6cbfc73be52a75be188633a2ee7dfc5f1ce1bc5e
Eval EXEC <execvy@gmail.com>
2023-11-22 10:36:38 +0800

    Re-export ChainServicesBuilder in ckb_shared

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,9 +1,11 @@
 //! TODO(doc): @quake
 
 // num_cpus is used in proc_macro
+pub mod chain_services_builder;
 pub mod shared;
 pub mod shared_builder;
 
+pub use chain_services_builder::ChainServicesBuilder;
 pub use ckb_snapshot::{Snapshot, SnapshotMgr};
 pub use shared::Shared;
 pub use shared_builder::{SharedBuilder, SharedPackage};
