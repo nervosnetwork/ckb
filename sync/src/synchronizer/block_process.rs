f3b4fa354e526044fbb13af9fb1cfb7e1785ccf4
Eval EXEC <execvy@gmail.com>
2023-11-29 12:16:17 +0800

    Remove Synchronizer::process_new_block to asynchronous_process_new_block

--- sync/src/synchronizer/block_process.rs
+++ sync/src/synchronizer/block_process.rs
@@ -1,4 +1,5 @@
 use crate::synchronizer::Synchronizer;
+use ckb_chain::VerifyResult;
 use ckb_logger::debug;
 use ckb_network::PeerIndex;
 use ckb_types::{packed, prelude::*};
@@ -35,26 +36,11 @@ impl<'a> BlockProcess<'a> {
         let shared = self.synchronizer.shared();
 
         if shared.new_block_received(&block) {
-            self.synchronizer
-                .process_new_block(block.clone(), self.peer, self.message_bytes);
-            // {
-            //     Ok(verify_failed_peers) => {
-            //         return verify_failed_peers;
-            //     }
-            //     Err(err) => {
-            //         error!("BlockProcess process_new_block error: {:?}", err);
-            //     }
-            // }
-
-            // if let Err(err) = this_block_verify_result {
-            //     if !is_internal_db_error(&err) {
-            //         return StatusCode::BlockIsInvalid.with_context(format!(
-            //             "{}, error: {}",
-            //             block.hash(),
-            //             err,
-            //         ));
-            //     }
-            // }
+            self.synchronizer.asynchronous_process_new_block(
+                block.clone(),
+                self.peer,
+                self.message_bytes,
+            );
         }
     }
 }
