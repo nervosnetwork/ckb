f95baad648098fbfb43f09669f7b4fed9a9656a9
Eval EXEC <execvy@gmail.com>
2023-11-29 12:45:37 +0800

    Add `blocking_execute` method for `BlockProcess` for the purpose of unit test

--- sync/src/synchronizer/block_process.rs
+++ sync/src/synchronizer/block_process.rs
@@ -1,5 +1,4 @@
 use crate::synchronizer::Synchronizer;
-use ckb_chain::VerifyResult;
 use ckb_logger::debug;
 use ckb_network::PeerIndex;
 use ckb_types::{packed, prelude::*};
@@ -42,5 +41,33 @@ impl<'a> BlockProcess<'a> {
                 self.message_bytes,
             );
         }
+    }
+
+    #[cfg(test)]
+    pub fn blocking_execute(self) -> crate::Status {
+        let block = self.message.block().to_entity().into_view();
+        debug!(
+            "BlockProcess received block {} {}",
+            block.number(),
+            block.hash(),
+        );
+        let shared = self.synchronizer.shared();
+
+        if shared.new_block_received(&block) {
+            if let Err(err) = self.synchronizer.blocking_process_new_block(
+                block.clone(),
+                self.peer,
+                self.message_bytes,
+            ) {
+                if !ckb_error::is_internal_db_error(&err) {
+                    return crate::StatusCode::BlockIsInvalid.with_context(format!(
+                        "{}, error: {}",
+                        block.hash(),
+                        err,
+                    ));
+                }
+            }
+        }
+        crate::Status::ok()
     }
 }
