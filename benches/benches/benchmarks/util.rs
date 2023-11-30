73ee6b1c047480a2ceb26d4694828a7dd00168bc
Eval EXEC <execvy@gmail.com>
2023-11-30 17:35:08 +0800

    Fix cargo clippy warnings for ckb-benches

--- benches/benches/benchmarks/util.rs
+++ benches/benches/benchmarks/util.rs
@@ -1,4 +1,4 @@
-use ckb_chain::ChainController;
+use ckb_chain::{start_chain_services, ChainController};
 use ckb_chain_spec::consensus::{ConsensusBuilder, ProposalWindow};
 use ckb_crypto::secp::Privkey;
 use ckb_dao::DaoCalculator;
