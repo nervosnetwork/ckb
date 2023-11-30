a68e8373a1e35ae4781d4ffdba7641f5aafe94f2
Eval EXEC <execvy@gmail.com>
2023-11-30 17:27:56 +0800

    Add docs for ckb-chain, fix clippy warnings

--- chain/src/lib.rs
+++ chain/src/lib.rs
@@ -5,7 +5,6 @@
 //!
 //! [`ChainService`]: chain/struct.ChainService.html
 //! [`ChainController`]: chain/struct.ChainController.html
-
 use ckb_error::{is_internal_db_error, Error};
 use ckb_logger::{debug, error};
 use ckb_network::PeerIndex;
@@ -27,33 +26,41 @@ pub use chain_service::{start_chain_services, ChainController};
 type ProcessBlockRequest = Request<LonelyBlockWithCallback, ()>;
 type TruncateRequest = Request<Byte32, Result<(), Error>>;
 
+/// VerifyResult is the result type to represent the result of block verification
 pub type VerifyResult = Result<VerifiedBlockStatus, Error>;
 
+/// VerifyCallback is the callback type to be called after block verification
 pub type VerifyCallback = Box<dyn FnOnce(VerifyResult) + Send + Sync>;
 
 /// VerifiedBlockStatus is
 #[derive(Debug, Clone, PartialEq)]
 pub enum VerifiedBlockStatus {
-    // The block is being seen for the first time, and VM have verified it
+    /// The block is being seen for the first time, and VM have verified it
     FirstSeenAndVerified,
 
-    // The block is being seen for the first time
-    // but VM have not verified it since its a uncle block
+    /// The block is being seen for the first time
+    /// but VM have not verified it since its a uncle block
     UncleBlockNotVerified,
 
-    // The block has been verified before.
+    /// The block has been verified before.
     PreviouslySeenAndVerified,
 }
 
+/// LonelyBlock is the block which we have not check weather its parent is stored yet
 #[derive(Clone)]
 pub struct LonelyBlock {
+    /// block
     pub block: Arc<BlockView>,
 
+    /// This block is received from which peer, and the message bytes size
     pub peer_id_with_msg_bytes: Option<(PeerIndex, u64)>,
+
+    /// The Switch to control the verification process
     pub switch: Option<Switch>,
 }
 
 impl LonelyBlock {
+    /// Combine with verify_callback, convert it to LonelyBlockWithCallback
     pub fn with_callback(self, verify_callback: Option<VerifyCallback>) -> LonelyBlockWithCallback {
         LonelyBlockWithCallback {
             lonely_block: self,
@@ -61,32 +68,38 @@ impl LonelyBlock {
         }
     }
 
+    /// Combine with empty verify_callback, convert it to LonelyBlockWithCallback
     pub fn without_callback(self) -> LonelyBlockWithCallback {
         self.with_callback(None)
     }
 }
 
+/// LonelyBlockWithCallback Combine LonelyBlock with an optional verify_callback
 pub struct LonelyBlockWithCallback {
+    /// The LonelyBlock
     pub lonely_block: LonelyBlock,
+    /// The optional verify_callback
     pub verify_callback: Option<VerifyCallback>,
 }
 
 impl LonelyBlockWithCallback {
     pub(crate) fn execute_callback(self, verify_result: VerifyResult) {
-        match self.verify_callback {
-            Some(verify_callback) => {
-                verify_callback(verify_result);
-            }
-            None => {}
+        if let Some(verify_callback) = self.verify_callback {
+            verify_callback(verify_result);
         }
     }
 
+    /// Get reference to block
     pub fn block(&self) -> &Arc<BlockView> {
         &self.lonely_block.block
     }
+
+    /// get peer_id and msg_bytes
     pub fn peer_id_with_msg_bytes(&self) -> Option<(PeerIndex, u64)> {
         self.lonely_block.peer_id_with_msg_bytes
     }
+
+    /// get switch param
     pub fn switch(&self) -> Option<Switch> {
         self.lonely_block.switch
     }
@@ -147,7 +160,7 @@ pub(crate) fn tell_synchronizer_to_punish_the_bad_peer(
     block_hash: Byte32,
     err: &Error,
 ) {
-    let is_internal_db_error = is_internal_db_error(&err);
+    let is_internal_db_error = is_internal_db_error(err);
     match peer_id_with_msg_bytes {
         Some((peer_id, msg_bytes)) => {
             let verify_failed_block_info = VerifyFailedBlockInfo {
@@ -157,11 +170,8 @@ pub(crate) fn tell_synchronizer_to_punish_the_bad_peer(
                 reason: err.to_string(),
                 is_internal_db_error,
             };
-            match verify_failed_blocks_tx.send(verify_failed_block_info) {
-                Err(_err) => {
-                    error!("ChainService failed to send verify failed block info to Synchronizer, the receiver side may have been closed, this shouldn't happen")
-                }
-                _ => {}
+            if let Err(_err) = verify_failed_blocks_tx.send(verify_failed_block_info) {
+                error!("ChainService failed to send verify failed block info to Synchronizer, the receiver side may have been closed, this shouldn't happen")
             }
         }
         _ => {
