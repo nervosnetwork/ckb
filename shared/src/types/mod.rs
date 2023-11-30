ae2bd2644752fb3fc57ee53ad861676256b1d11a
Eval EXEC <execvy@gmail.com>
2023-11-30 12:06:55 +0800

    Modify VerifyFailedBlockInfo::message_bytes to msg_bytes

--- shared/src/types/mod.rs
+++ shared/src/types/mod.rs
@@ -310,7 +310,7 @@ pub const SHRINK_THRESHOLD: usize = 300;
 pub struct VerifyFailedBlockInfo {
     pub block_hash: Byte32,
     pub peer_id: PeerIndex,
-    pub message_bytes: u64,
+    pub msg_bytes: u64,
     pub reason: String,
     pub is_internal_db_error: bool,
 }
