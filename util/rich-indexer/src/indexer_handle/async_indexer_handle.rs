3054e0bc497e9547949a59bb46265969ac4862c6
EthanYuan <yuanchengxing@hotmail.com>
2023-12-26 23:43:13 +0800

    impl the main body of the method query_cells.

--- util/rich-indexer/src/indexer_handle/async_indexer_handle.rs
+++ util/rich-indexer/src/indexer_handle/async_indexer_handle.rs
@@ -1,11 +1,14 @@
 use crate::store::SQLXPool;
 
 use ckb_indexer_sync::{Error, Pool};
-use ckb_jsonrpc_types::IndexerTip;
 use ckb_jsonrpc_types::{
     IndexerCell, IndexerOrder, IndexerPagination, IndexerSearchKey, JsonBytes, Uint32,
 };
+use ckb_jsonrpc_types::{IndexerScriptType, IndexerSearchMode, IndexerTip};
+use ckb_types::packed::{CellOutputBuilder, OutPointBuilder, ScriptBuilder};
+use ckb_types::prelude::*;
 use ckb_types::H256;
+use sql_builder::{name, name::SqlName, SqlBuilder};
 use sqlx::Row;
 
 use std::sync::{Arc, RwLock};
@@ -49,12 +52,156 @@ impl AsyncRichIndexerHandle {
     /// Get cells
     pub async fn query_cells(
         &self,
-        _search_key: IndexerSearchKey,
+        search_key: IndexerSearchKey,
         _order: IndexerOrder,
         _limit: Uint32,
         _after: Option<JsonBytes>,
     ) -> Result<IndexerPagination<IndexerCell>, Error> {
-        unimplemented!()
+        // sub query for script
+        let mut query_builder = SqlBuilder::select_from("script");
+        query_builder
+            .field("script.id")
+            .and_where_eq("code_hash", "$1")
+            .and_where_eq("hash_type", "$2");
+        match search_key.script_search_mode {
+            Some(IndexerSearchMode::Prefix) | None => {
+                // default is prefix
+                query_builder.and_where_like_left("args", "$3");
+            }
+            Some(IndexerSearchMode::Exact) => {
+                query_builder.and_where_eq("args", "$3");
+            }
+            Some(IndexerSearchMode::Partial) => {
+                query_builder.and_where_like_any("args", "$3");
+            }
+        }
+        let sql_sub_query = query_builder
+            .subquery()
+            .map_err(|err| Error::DB(err.to_string()))?;
+
+        // query output
+        let mut query_builder = SqlBuilder::select_from("output");
+        query_builder
+            .field("ckb_transaction.tx_index")
+            .field("ckb_transaction.tx_hash")
+            .field("output.output_index")
+            .field("output.capacity")
+            .field("lock_script.code_hash AS lock_code_hash")
+            .field("lock_script.hash_type AS lock_hash_type")
+            .field("lock_script.args AS lock_args")
+            .field("type_script.code_hash AS type_code_hash")
+            .field("type_script.hash_type AS type_hash_type")
+            .field("type_script.args AS type_args")
+            .field("block.block_number");
+        match search_key.with_data {
+            Some(true) | None => {
+                query_builder.field("data as output_data");
+            }
+            Some(false) => {
+                query_builder.field("NULL as output_data");
+            }
+        }
+        query_builder.join(&format!("{} script_res", sql_sub_query));
+        match search_key.script_type {
+            IndexerScriptType::Lock => {
+                query_builder.on("output.lock_script_id = script_res.id");
+            }
+            IndexerScriptType::Type => {
+                query_builder.on("output.type_script_id = script_res.id");
+            }
+        }
+        query_builder
+            .join(name!("script";"lock_script"))
+            .on("output.lock_script_id = lock_script.id")
+            .left_outer()
+            .join(name!("script";"type_script"))
+            .on("output.type_script_id = type_script.id")
+            .join("ckb_transaction")
+            .on("output.tx_id = ckb_transaction.id")
+            .join("block")
+            .on("ckb_transaction.block_id = block.id");
+
+        // sql string
+        let sql = query_builder
+            .sql()
+            .map_err(|err| Error::DB(err.to_string()))?
+            .trim_end_matches(';')
+            .to_string();
+
+        // bind
+        let mut query = SQLXPool::new_query(&sql);
+        query = query
+            .bind(search_key.script.code_hash.as_bytes())
+            .bind(search_key.script.hash_type as i16)
+            .bind(search_key.script.args.as_bytes());
+
+        // fetch
+        let cells = self
+            .store
+            .fetch_all(query)
+            .await
+            .map_err(|err| Error::DB(err.to_string()))
+            .map(|rows| {
+                rows.iter()
+                    .map(|row| {
+                        (
+                            row.get::<i32, _>("tx_index"),
+                            row.get::<Vec<u8>, _>("tx_hash"),
+                            row.get::<i32, _>("output_index"),
+                            row.get::<i64, _>("capacity"),
+                            row.get::<Vec<u8>, _>("lock_code_hash"),
+                            row.get::<i16, _>("lock_hash_type"),
+                            row.get::<Vec<u8>, _>("lock_args"),
+                            row.get::<Option<Vec<u8>>, _>("type_code_hash"),
+                            row.get::<Option<i16>, _>("type_hash_type"),
+                            row.get::<Option<Vec<u8>>, _>("type_args"),
+                            row.get::<Option<Vec<u8>>, _>("output_data"),
+                            row.get::<i64, _>("block_number"),
+                        )
+                    })
+                    .collect::<Vec<_>>()
+            })?;
+
+        let cells = cells
+            .into_iter()
+            .map(|row| {
+                let out_point = OutPointBuilder::default()
+                    .tx_hash(to_fixed_array::<32>(&row.1).pack())
+                    .index((row.2 as u32).pack())
+                    .build();
+
+                let lock_script = ScriptBuilder::default()
+                    .code_hash(to_fixed_array::<32>(&row.4).pack())
+                    .hash_type((row.5 as u8).into())
+                    .args(row.6.pack())
+                    .build();
+                let type_script = row.7.as_ref().map(|value| {
+                    ScriptBuilder::default()
+                        .code_hash(to_fixed_array::<32>(value).pack())
+                        .hash_type((row.8.unwrap() as u8).into())
+                        .args(row.9.unwrap().pack())
+                        .build()
+                });
+                let output = CellOutputBuilder::default()
+                    .capacity((row.3 as u64).pack())
+                    .lock(lock_script)
+                    .type_(type_script.pack())
+                    .build();
+
+                IndexerCell {
+                    output: output.into(),
+                    output_data: row.10.map(|value| JsonBytes::from_vec(value)),
+                    out_point: out_point.into(),
+                    block_number: (row.11 as u64).into(),
+                    tx_index: (row.0 as u32).into(),
+                }
+            })
+            .collect::<Vec<_>>();
+
+        Ok(IndexerPagination {
+            objects: cells,
+            last_cursor: JsonBytes::from_vec(vec![]),
+        })
     }
 }
 
@@ -69,4 +216,11 @@ pub(crate) fn sqlx_param_placeholders(range: std::ops::Range<usize>) -> Result<V
     Ok((1..=range.end)
         .map(|i| format!("${}", i))
         .collect::<Vec<String>>())
+}
+
+fn to_fixed_array<const LEN: usize>(input: &[u8]) -> [u8; LEN] {
+    assert_eq!(input.len(), LEN);
+    let mut list = [0; LEN];
+    list.copy_from_slice(input);
+    list
 }
--- util/rich-indexer/src/tests/mod.rs
+++ util/rich-indexer/src/tests/mod.rs
@@ -3,7 +3,13 @@ use crate::{AsyncRichIndexer, AsyncRichIndexerHandle};
 
 use ckb_app_config::RichIndexerConfig;
 use ckb_indexer_sync::CustomFilters;
-use ckb_jsonrpc_types::BlockView as JsonBlockView;
+use ckb_jsonrpc_types::{
+    BlockView as JsonBlockView, IndexerOrder, IndexerScriptType, IndexerSearchKey,
+    IndexerSearchMode, ScriptHashType,
+};
+use ckb_types::packed::ScriptBuilder;
+use ckb_types::prelude::*;
+use ckb_types::{h160, h256};
 
 const MEMORY_DB: &str = ":memory:";
 const BLOCK_DIR: &str = "./src/tests/data/blocks/";
@@ -284,4 +290,45 @@ async fn test_block_filter_and_rollback_block() {
     assert_eq!(0, storage.fetch_count("output").await.unwrap());
     assert_eq!(0, storage.fetch_count("input").await.unwrap());
     assert_eq!(0, storage.fetch_count("script").await.unwrap());
+}
+
+#[tokio::test]
+async fn get_cells() {
+    let pool = connect_sqlite(MEMORY_DB).await;
+    let indexer = AsyncRichIndexerHandle::new(pool.clone(), None);
+    let res = indexer.query_indexer_tip().await.unwrap();
+    assert!(res.is_none());
+
+    insert_blocks(pool.clone()).await;
+
+    let search_key = IndexerSearchKey {
+        script: ScriptBuilder::default()
+            .code_hash(
+                h256!("0x709f3fda12f561cfacf92273c57a98fede188a3f1a59b1f888d113f9cce08649").pack(),
+            )
+            .hash_type((ScriptHashType::Data as u8).into())
+            .args(
+                h160!("0xb73961e46d9eb118d3de1d1e8f30b3af7bbf3160")
+                    .as_bytes()
+                    .pack(),
+            )
+            .build()
+            .into(),
+        script_type: IndexerScriptType::Lock,
+        script_search_mode: Some(IndexerSearchMode::Exact),
+        filter: None,
+        with_data: Some(false),
+        group_by_transaction: None,
+    };
+    let cells = indexer
+        .query_cells(search_key, IndexerOrder::Asc, 100u32.into(), None)
+        .await
+        .unwrap();
+    assert_eq!(1, cells.objects.len());
+    let cell = &cells.objects[0];
+    assert_eq!(cell.block_number, 0u64.into());
+    assert_eq!(cell.tx_index, 0u32.into());
+    assert_eq!(cell.out_point.index, 5u32.into());
+    assert_eq!(cell.output.type_, None);
+    assert_eq!(cell.output_data, None);
 }
