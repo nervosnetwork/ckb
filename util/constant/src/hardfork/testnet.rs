9d2b2014f14e366c25fd0c54e2d467e40cefe66b
yukang <moorekang@gmail.com>
2023-12-26 13:11:31 +0800

    rollback spec

--- util/constant/src/hardfork/testnet.rs
+++ util/constant/src/hardfork/testnet.rs
@@ -4,7 +4,8 @@ pub const CHAIN_SPEC_NAME: &str = "ckb_testnet";
 /// hardcode rfc0028/rfc0032/rfc0033/rfc0034 epoch
 pub const RFC0028_RFC0032_RFC0033_RFC0034_START_EPOCH: u64 = 3113;
 /// First epoch number for CKB v2021, at about 2021/10/24 3:15 UTC.
-pub const CKB2021_START_EPOCH: u64 = 3113;
+// pub const CKB2021_START_EPOCH: u64 = 3113;
+pub const CKB2021_START_EPOCH: u64 = 0;
 
 /// hardcode ckb2023 epoch
 pub const CKB2023_START_EPOCH: u64 = u64::MAX;
