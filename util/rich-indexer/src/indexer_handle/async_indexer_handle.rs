70086eecd9a4e3048c6cf2b5eca68e2316af09fb
EthanYuan <yuanchengxing@hotmail.com>
2023-12-28 19:37:41 +0800

    finish rich indexer query_cells.

--- util/rich-indexer/src/indexer_handle/async_indexer_handle.rs
+++ util/rich-indexer/src/indexer_handle/async_indexer_handle.rs
@@ -62,18 +62,14 @@ impl AsyncRichIndexerHandle {
         let mut query_builder = SqlBuilder::select_from("script");
         query_builder
             .field("script.id")
-            .and_where_eq("code_hash", "$1")
-            .and_where_eq("hash_type", "$2");
+            .and_where_eq("code_hash", "?")
+            .and_where_eq("hash_type", "?");
         match search_key.script_search_mode {
-            Some(IndexerSearchMode::Prefix) | None => {
-                // default is prefix
-                query_builder.and_where_like_left("args", "$3");
+            Some(IndexerSearchMode::Prefix) | None | Some(IndexerSearchMode::Partial) => {
+                query_builder.and_where("args LIKE ?");
             }
             Some(IndexerSearchMode::Exact) => {
-                query_builder.and_where_eq("args", "$3");
-            }
-            Some(IndexerSearchMode::Partial) => {
-                query_builder.and_where_like_any("args", "$3");
+                query_builder.and_where_eq("args", "?");
             }
         }
         let sql_sub_query = query_builder
@@ -97,7 +93,7 @@ impl AsyncRichIndexerHandle {
             .field("block.block_number");
         match search_key.with_data {
             Some(true) | None => {
-                query_builder.field("data as output_data");
+                query_builder.field("output.data as output_data");
             }
             Some(false) => {
                 query_builder.field("NULL as output_data");
@@ -113,21 +109,86 @@ impl AsyncRichIndexerHandle {
             }
         }
         query_builder
-            .join(name!("script";"lock_script"))
-            .on("output.lock_script_id = lock_script.id")
-            .left_outer()
-            .join(name!("script";"type_script"))
-            .on("output.type_script_id = type_script.id")
             .join("ckb_transaction")
             .on("output.tx_id = ckb_transaction.id")
             .join("block")
-            .on("ckb_transaction.block_id = block.id");
+            .on("ckb_transaction.block_id = block.id")
+            .left()
+            .join(name!("script";"lock_script"))
+            .on("output.lock_script_id = lock_script.id")   
+            .join(name!("script";"type_script"))
+            .on("output.type_script_id = type_script.id")
+            .join("input")
+            .on("output.id = input.output_id");
+        query_builder.and_where("input.output_id IS NULL");
         if let Some(after) = after {
             let after = decode_i64(after.as_bytes())?;
             match order {
                 IndexerOrder::Asc => query_builder.and_where_gt("output.id", after),
                 IndexerOrder::Desc => query_builder.and_where_lt("output.id", after),
             };
+        }
+        if let Some(ref filter) = search_key.filter {
+            if filter.script.is_some() {
+                match search_key.script_type {
+                    IndexerScriptType::Lock => {
+                        query_builder.and_where_eq("type_script.code_hash", "?");
+                        query_builder.and_where_eq("type_script.hash_type", "?");
+                        query_builder.and_where("type_script.args LIKE ?");
+                    }
+                    IndexerScriptType::Type => {
+                        query_builder.and_where_eq("lock_script.code_hash", "?");
+                        query_builder.and_where_eq("lock_script.hash_type", "?");
+                        query_builder.and_where("lock_script.args LIKE ?");
+                    }
+                }
+            }
+            if let Some(script_len_range) = &filter.script_len_range {
+                match search_key.script_type {
+                    IndexerScriptType::Lock => {
+                        query_builder.and_where_ge(
+                            "length(lock_script.code_hash)+1+LENGTH(lock_script.args)",
+                            script_len_range.start(),
+                        );
+                        query_builder.and_where_lt(
+                            "length(lock_script.code_hash)+1+LENGTH(lock_script.args)",
+                            script_len_range.end(),
+                        );
+                    }
+                    IndexerScriptType::Type => {
+                        query_builder.and_where_ge(
+                            "LENGTH(type_script.code_hash)+1+LENGTH(type_script.args)",
+                            script_len_range.start(),
+                        );
+                        query_builder.and_where_lt(
+                            "LENGTH(type_script.code_hash)+1+LENGTH(type_script.args)",
+                            script_len_range.end(),
+                        );
+                    }
+                }
+            }
+            if let Some(data_len_range) = &filter.output_data_len_range {
+                query_builder.and_where_ge("LENGTH(output.data)", data_len_range.start());
+                query_builder.and_where_lt("LENGTH(output.data)", data_len_range.end());
+            }
+            if let Some(capacity_range) = &filter.output_capacity_range {
+                query_builder.and_where_ge("output.capacity", capacity_range.start());
+                query_builder.and_where_lt("output.capacity", capacity_range.end());
+            }
+            if let Some(block_range) = &filter.block_range {
+                query_builder.and_where_ge("block.block_number", block_range.start());
+                query_builder.and_where_lt("block.block_number", block_range.end());
+            }
+            if filter.data.is_some() {
+                match filter.data_filter_mode {
+                    Some(IndexerSearchMode::Prefix) | None | Some(IndexerSearchMode::Partial) => {
+                        query_builder.and_where("output.data LIKE ?");
+                    }
+                    Some(IndexerSearchMode::Exact) => {
+                        query_builder.and_where_eq("output.data", "?");
+                    }
+                }
+            }
         }
         match order {
             IndexerOrder::Asc => query_builder.order_by("output.id", false),
@@ -146,8 +207,52 @@ impl AsyncRichIndexerHandle {
         let mut query = SQLXPool::new_query(&sql);
         query = query
             .bind(search_key.script.code_hash.as_bytes())
-            .bind(search_key.script.hash_type as i16)
-            .bind(search_key.script.args.as_bytes());
+            .bind(search_key.script.hash_type as i16);
+        match search_key.script_search_mode {
+            Some(IndexerSearchMode::Prefix) | None => {
+                let mut new_args = search_key.script.args.as_bytes().to_vec();
+                new_args.push(0x25); // End with %
+                query = query.bind(new_args);
+            }
+            Some(IndexerSearchMode::Exact) => {
+                query = query.bind(search_key.script.args.as_bytes());
+            }
+            Some(IndexerSearchMode::Partial) => {
+                let mut new_args = vec![0x25]; // Start with %
+                new_args.extend_from_slice(search_key.script.args.as_bytes());
+                new_args.push(0x25); // End with %
+                query = query.bind(new_args);
+            }
+        }
+        if let Some(filter) = search_key.filter.as_ref() {
+            if let Some(script) = filter.script.as_ref() {
+                query = query
+                    .bind(script.code_hash.as_bytes())
+                    .bind(script.hash_type.clone() as i16);
+                // Default prefix search
+                let mut new_args = script.args.as_bytes().to_vec();
+                new_args.push(0x25); // End with %
+                query = query.bind(new_args);
+            }
+            if let Some(data) = &filter.data {
+                match filter.data_filter_mode {
+                    Some(IndexerSearchMode::Prefix) | None => {
+                        let mut new_data = data.as_bytes().to_vec();
+                        new_data.push(0x25); // End with %
+                        query = query.bind(new_data);
+                    }
+                    Some(IndexerSearchMode::Exact) => {
+                        query = query.bind(data.as_bytes());
+                    }
+                    Some(IndexerSearchMode::Partial) => {
+                        let mut new_data = vec![0x25]; // Start with %
+                        new_data.extend_from_slice(data.as_bytes());
+                        new_data.push(0x25); // End with %
+                        query = query.bind(new_data);
+                    }
+                }
+            }
+        }
 
         // fetch
         let mut last_cursor = Vec::new();
--- util/rich-indexer/src/tests/query.rs
+++ util/rich-indexer/src/tests/query.rs
@@ -1,5 +1,8 @@
 use super::*;
 
+use ckb_jsonrpc_types::{IndexerRange, IndexerSearchKeyFilter};
+use ckb_types::packed::Script;
+
 #[tokio::test]
 async fn test_query_tip() {
     let pool = connect_sqlite(MEMORY_DB).await;
@@ -17,7 +20,7 @@ async fn test_query_tip() {
 }
 
 #[tokio::test]
-async fn get_cells() {
+async fn query_cells() {
     let pool = connect_sqlite(MEMORY_DB).await;
     let indexer = AsyncRichIndexerHandle::new(pool.clone(), None);
     let res = indexer.query_indexer_tip().await.unwrap();
@@ -25,22 +28,34 @@ async fn get_cells() {
 
     insert_blocks(pool.clone()).await;
 
+    let lock_script = ScriptBuilder::default()
+        .code_hash(
+            h256!("0x0000000000000000000000000000000000000000000000000000000000000000").pack(),
+        )
+        .hash_type((ScriptHashType::Data as u8).into())
+        .args(
+            h160!("0x62e907b15cbf27d5425399ebf6f0fb50ebb88f18")
+                .as_bytes()
+                .pack(),
+        )
+        .build();
+    let script_len = extract_raw_data(&lock_script).len() as u64;
     let search_key = IndexerSearchKey {
-        script: ScriptBuilder::default()
-            .code_hash(
-                h256!("0x709f3fda12f561cfacf92273c57a98fede188a3f1a59b1f888d113f9cce08649").pack(),
-            )
-            .hash_type((ScriptHashType::Data as u8).into())
-            .args(
-                h160!("0xb73961e46d9eb118d3de1d1e8f30b3af7bbf3160")
-                    .as_bytes()
-                    .pack(),
-            )
-            .build()
-            .into(),
+        script: lock_script.into(),
         script_type: IndexerScriptType::Lock,
-        script_search_mode: Some(IndexerSearchMode::Exact),
-        filter: None,
+        script_search_mode: Some(IndexerSearchMode::Prefix),
+        filter: Some(IndexerSearchKeyFilter {
+            script: None,
+            script_len_range: Some(IndexerRange::new(script_len, script_len + 10)),
+            output_data_len_range: Some(IndexerRange::new(0u64, 10u64)),
+            output_capacity_range: Some(IndexerRange::new(
+                840_000_000_000_000_000_u64,
+                840_000_000_100_000_000_u64,
+            )),
+            block_range: Some(IndexerRange::new(0u64, 10u64)),
+            data: None,
+            data_filter_mode: None,
+        }),
         with_data: Some(false),
         group_by_transaction: None,
     };
@@ -57,13 +72,144 @@ async fn get_cells() {
     assert_eq!(cells.objects.len(), 1);
     assert_eq!(
         cells.last_cursor,
-        JsonBytes::from_vec(vec![6u8, 0, 0, 0, 0, 0, 0, 0])
+        JsonBytes::from_vec(vec![7u8, 0, 0, 0, 0, 0, 0, 0])
     );
-    
+
     let cell = &cells.objects[0];
     assert_eq!(cell.block_number, 0u64.into());
     assert_eq!(cell.tx_index, 0u32.into());
-    assert_eq!(cell.out_point.index, 5u32.into());
+    assert_eq!(cell.out_point.index, 6u32.into());
     assert_eq!(cell.output.type_, None);
     assert_eq!(cell.output_data, None);
+}
+
+#[tokio::test]
+async fn query_cells_filter_data() {
+    let pool = connect_sqlite(MEMORY_DB).await;
+    let indexer = AsyncRichIndexerHandle::new(pool.clone(), None);
+    let res = indexer.query_indexer_tip().await.unwrap();
+    assert!(res.is_none());
+
+    insert_blocks(pool.clone()).await;
+
+    let search_key = IndexerSearchKey {
+        script: ScriptBuilder::default()
+            .code_hash(
+                h256!("0x00000000000000000000000000000000000000000000000000545950455f4944").pack(),
+            )
+            .hash_type((ScriptHashType::Type as u8).into())
+            .args(
+                hex::decode("b2a8500929d6a1294bf9bf1bf565f549fa4a5f1316a3306ad3d4783e64bcf626")
+                    .expect("Decoding failed")
+                    .pack(),
+            )
+            .build()
+            .into(),
+        script_type: IndexerScriptType::Type,
+        script_search_mode: Some(IndexerSearchMode::Exact),
+        filter: Some(IndexerSearchKeyFilter {
+            script: None,
+            script_len_range: None,
+            output_data_len_range: None,
+            output_capacity_range: None,
+            block_range: None,
+            data: Some(JsonBytes::from_vec(vec![127, 69, 76])),
+            data_filter_mode: Some(IndexerSearchMode::Prefix),
+        }),
+        with_data: Some(false),
+        group_by_transaction: None,
+    };
+    let cells = indexer
+        .query_cells(
+            search_key,
+            IndexerOrder::Asc,
+            100u32.into(),
+            Some(vec![2u8, 0, 0, 0, 0, 0, 0, 0].pack().into()),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(cells.objects.len(), 1);
+    assert_eq!(
+        cells.last_cursor,
+        JsonBytes::from_vec(vec![3u8, 0, 0, 0, 0, 0, 0, 0])
+    );
+
+    let cell = &cells.objects[0];
+    assert_eq!(cell.block_number, 0u64.into());
+    assert_eq!(cell.tx_index, 0u32.into());
+    assert_eq!(cell.out_point.index, 2u32.into());
+    assert_eq!(cell.output_data, None);
+}
+
+#[tokio::test]
+async fn query_cells_by_cursor() {
+    let pool = connect_sqlite(MEMORY_DB).await;
+    let indexer = AsyncRichIndexerHandle::new(pool.clone(), None);
+    let res = indexer.query_indexer_tip().await.unwrap();
+    assert!(res.is_none());
+
+    insert_blocks(pool.clone()).await;
+
+    let lock_script = ScriptBuilder::default()
+        .code_hash(
+            h256!("0x0000000000000000000000000000000000000000000000000000000000000000").pack(),
+        )
+        .hash_type((ScriptHashType::Data as u8).into())
+        .args(hex::decode("").expect("Decoding failed").pack())
+        .build();
+    let search_key = IndexerSearchKey {
+        script: lock_script.clone().into(),
+        script_type: IndexerScriptType::Lock,
+        script_search_mode: Some(IndexerSearchMode::Exact),
+        filter: None,
+        with_data: Some(false),
+        group_by_transaction: None,
+    };
+    let first_query_cells = indexer
+        .query_cells(
+            search_key,
+            IndexerOrder::Asc,
+            3u32.into(),
+            Some(vec![0u8, 0, 0, 0, 0, 0, 0, 0].pack().into()),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(first_query_cells.objects.len(), 3);
+    assert_eq!(
+        first_query_cells.last_cursor,
+        JsonBytes::from_vec(vec![3u8, 0, 0, 0, 0, 0, 0, 0])
+    );
+
+    // query using last_cursor
+    let search_key = IndexerSearchKey {
+        script: lock_script.into(),
+        script_type: IndexerScriptType::Lock,
+        script_search_mode: Some(IndexerSearchMode::Exact),
+        filter: None,
+        with_data: Some(false),
+        group_by_transaction: None,
+    };
+    let second_query_cells = indexer
+        .query_cells(
+            search_key,
+            IndexerOrder::Asc,
+            100u32.into(),
+            Some(first_query_cells.last_cursor),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(second_query_cells.objects.len(), 4);
+}
+
+/// helper fn extracts script fields raw data
+fn extract_raw_data(script: &Script) -> Vec<u8> {
+    [
+        script.code_hash().as_slice(),
+        script.hash_type().as_slice(),
+        &script.args().raw_data(),
+    ]
+    .concat()
 }
