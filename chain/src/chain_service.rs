733223ccbe3e56a66f305c61c403fd2c10da161e
Eval EXEC <execvy@gmail.com>
2024-01-11 12:25:13 +0800

    Split ChainController out chain_service.rs
    Signed-off-by: Eval EXEC <execvy@gmail.com>

--- chain/src/chain_service.rs
+++ chain/src/chain_service.rs
@@ -4,8 +4,8 @@
 use crate::consume_unverified::ConsumeUnverifiedBlocks;
 use crate::utils::orphan_block_pool::OrphanBlockPool;
 use crate::{
-    tell_synchronizer_to_punish_the_bad_peer, LonelyBlock, LonelyBlockWithCallback,
-    ProcessBlockRequest, TruncateRequest, UnverifiedBlock, VerifyCallback, VerifyResult,
+    tell_synchronizer_to_punish_the_bad_peer, ChainController, LonelyBlockWithCallback,
+    ProcessBlockRequest, UnverifiedBlock,
 };
 use ckb_channel::{self as channel, select, Receiver, SendError, Sender};
 use ckb_constant::sync::BLOCK_DOWNLOAD_WINDOW;
@@ -16,161 +16,13 @@ use ckb_shared::shared::Shared;
 use ckb_shared::types::VerifyFailedBlockInfo;
 use ckb_shared::ChainServicesBuilder;
 use ckb_stop_handler::{new_crossbeam_exit_rx, register_thread};
-use ckb_types::{
-    core::{service::Request, BlockView},
-    packed::Byte32,
-};
+use ckb_types::core::{service::Request, BlockView};
 use ckb_verification::{BlockVerifier, NonContextualBlockTxsVerifier};
-use ckb_verification_traits::{Switch, Verifier};
+use ckb_verification_traits::Verifier;
 use std::sync::Arc;
 use std::thread;
 
 const ORPHAN_BLOCK_SIZE: usize = (BLOCK_DOWNLOAD_WINDOW * 2) as usize;
-
-/// Controller to the chain service.
-///
-/// The controller is internally reference-counted and can be freely cloned.
-///
-/// A controller can invoke ChainService methods.
-#[cfg_attr(feature = "mock", faux::create)]
-#[derive(Clone)]
-pub struct ChainController {
-    process_block_sender: Sender<ProcessBlockRequest>,
-    truncate_sender: Sender<TruncateRequest>,
-    orphan_block_broker: Arc<OrphanBlockPool>,
-}
-
-#[cfg_attr(feature = "mock", faux::methods)]
-impl ChainController {
-    fn new(
-        process_block_sender: Sender<ProcessBlockRequest>,
-        truncate_sender: Sender<TruncateRequest>,
-        orphan_block_broker: Arc<OrphanBlockPool>,
-    ) -> Self {
-        ChainController {
-            process_block_sender,
-            truncate_sender,
-            orphan_block_broker,
-        }
-    }
-
-    pub fn asynchronous_process_block_with_switch(&self, block: Arc<BlockView>, switch: Switch) {
-        self.asynchronous_process_lonely_block(LonelyBlock {
-            block,
-            peer_id_with_msg_bytes: None,
-            switch: Some(switch),
-        })
-    }
-
-    pub fn asynchronous_process_block(&self, block: Arc<BlockView>) {
-        self.asynchronous_process_lonely_block_with_callback(
-            LonelyBlock {
-                block,
-                peer_id_with_msg_bytes: None,
-                switch: None,
-            }
-            .without_callback(),
-        )
-    }
-
-    pub fn asynchronous_process_block_with_callback(
-        &self,
-        block: Arc<BlockView>,
-        verify_callback: VerifyCallback,
-    ) {
-        self.asynchronous_process_lonely_block_with_callback(
-            LonelyBlock {
-                block,
-                peer_id_with_msg_bytes: None,
-                switch: None,
-            }
-            .with_callback(Some(verify_callback)),
-        )
-    }
-
-    pub fn asynchronous_process_lonely_block(&self, lonely_block: LonelyBlock) {
-        let lonely_block_without_callback: LonelyBlockWithCallback =
-            lonely_block.without_callback();
-
-        self.asynchronous_process_lonely_block_with_callback(lonely_block_without_callback);
-    }
-
-    /// Internal method insert block for test
-    ///
-    /// switch bit flags for particular verify, make easier to generating test data
-    pub fn asynchronous_process_lonely_block_with_callback(
-        &self,
-        lonely_block_with_callback: LonelyBlockWithCallback,
-    ) {
-        if Request::call(&self.process_block_sender, lonely_block_with_callback).is_none() {
-            error!("Chain service has gone")
-        }
-    }
-
-    pub fn blocking_process_block(&self, block: Arc<BlockView>) -> VerifyResult {
-        self.blocking_process_lonely_block(LonelyBlock {
-            block,
-            peer_id_with_msg_bytes: None,
-            switch: None,
-        })
-    }
-
-    pub fn blocking_process_block_with_switch(
-        &self,
-        block: Arc<BlockView>,
-        switch: Switch,
-    ) -> VerifyResult {
-        self.blocking_process_lonely_block(LonelyBlock {
-            block,
-            peer_id_with_msg_bytes: None,
-            switch: Some(switch),
-        })
-    }
-
-    pub fn blocking_process_lonely_block(&self, lonely_block: LonelyBlock) -> VerifyResult {
-        let (verify_result_tx, verify_result_rx) = ckb_channel::oneshot::channel::<VerifyResult>();
-
-        let verify_callback = {
-            move |result: VerifyResult| {
-                if let Err(err) = verify_result_tx.send(result) {
-                    error!(
-                        "blocking send verify_result failed: {}, this shouldn't happen",
-                        err
-                    )
-                }
-            }
-        };
-
-        let lonely_block_with_callback =
-            lonely_block.with_callback(Some(Box::new(verify_callback)));
-        self.asynchronous_process_lonely_block_with_callback(lonely_block_with_callback);
-        verify_result_rx.recv().unwrap_or_else(|err| {
-            Err(InternalErrorKind::System
-                .other(format!("blocking recv verify_result failed: {}", err))
-                .into())
-        })
-    }
-
-    /// Truncate chain to specified target
-    ///
-    /// Should use for testing only
-    pub fn truncate(&self, target_tip_hash: Byte32) -> Result<(), Error> {
-        Request::call(&self.truncate_sender, target_tip_hash).unwrap_or_else(|| {
-            Err(InternalErrorKind::System
-                .other("Chain service has gone")
-                .into())
-        })
-    }
-
-    // Relay need this
-    pub fn get_orphan_block(&self, hash: &Byte32) -> Option<Arc<BlockView>> {
-        self.orphan_block_broker.get_block(hash)
-    }
-
-    pub fn orphan_blocks_len(&self) -> usize {
-        self.orphan_block_broker.len()
-    }
-}
 
 pub fn start_chain_services(builder: ChainServicesBuilder) -> ChainController {
     let orphan_blocks_broker = Arc::new(OrphanBlockPool::with_capacity(ORPHAN_BLOCK_SIZE));
