8557c473fa3b9637d1fee10277b874cb4c26e5b1
Eval EXEC <execvy@gmail.com>
2024-01-17 17:54:48 +0800

    Comment out shrink_to_fit for header_map's MemoryMap

--- shared/src/types/header_map/memory.rs
+++ shared/src/types/header_map/memory.rs
@@ -101,7 +101,7 @@ impl MemoryMap {
     pub(crate) fn remove(&self, key: &Byte32) -> Option<HeaderIndexView> {
         let mut guard = self.0.write();
         let ret = guard.remove(key);
-        // shrink_to_fit!(guard, SHRINK_THRESHOLD);
+        shrink_to_fit!(guard, SHRINK_THRESHOLD);
         ret.map(|inner| (key.clone(), inner).into())
     }
 
