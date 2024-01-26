2b20fe0175602de7e85eb5659a1aeb332f922ece
zhangsoledad <787953403@qq.com>
2024-01-26 10:40:17 +0800

    chore: delayed 2nd hardfork testnet activation
    This reverts commit f2890dfa7847eca36e11f8dc26e9e2214a4a0777.

--- util/constant/src/hardfork/testnet.rs
+++ util/constant/src/hardfork/testnet.rs
@@ -8,8 +8,4 @@ pub const RFC0028_RFC0032_RFC0033_RFC0034_START_EPOCH: u64 = 3113;
 pub const CKB2021_START_EPOCH: u64 = 0;
 
 /// hardcode ckb2023 epoch
-/// 2024/02/05 7:44 utc
-/// |            hash                                                    |  number   |    timestamp    | epoch |
-/// | 0x4014244087d989355c1d06311f3f73582607d4c991e03296f703f9221f77aabe | 11937425  |  1704944649417  |  9074 |
-/// 1704944649417 + 151 * 4 * 60 * 60 * 1000 = 1707119049417  2024/02/05 7:44 utc
-pub const CKB2023_START_EPOCH: u64 = 9225;
+pub const CKB2023_START_EPOCH: u64 = u64::MAX;
