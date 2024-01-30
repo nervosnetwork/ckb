b0d8ea786261214d480b3cebce13b80c7d2f6664
Eval EXEC <execvy@gmail.com>
2024-01-30 21:43:21 +0800

    Export mock_chain and mock_utils from ckb-test-chain-utils

--- util/test-chain-utils/src/lib.rs
+++ util/test-chain-utils/src/lib.rs
@@ -1,7 +1,9 @@
 //! Provide several functions used for testing.
 mod chain;
 mod median_time;
+mod mock_chain;
 mod mock_store;
+mod mock_utils;
 
 pub use chain::{
     always_success_cell, always_success_cellbase, always_success_consensus, ckb_testnet_consensus,
@@ -9,4 +11,12 @@ pub use chain::{
     secp256k1_blake160_sighash_cell, secp256k1_data_cell, type_lock_script_code_hash,
 };
 pub use median_time::{MockMedianTime, MOCK_MEDIAN_TIME_COUNT};
+pub use mock_chain::MockChain;
 pub use mock_store::MockStore;
+pub use mock_utils::{
+    calculate_reward, create_always_success_out_point, create_always_success_tx, create_cellbase,
+    create_load_input_data_hash_cell_out_point, create_load_input_data_hash_cell_tx,
+    create_load_input_one_byte_cell_tx, create_load_input_one_byte_out_point,
+    create_multi_outputs_transaction, create_transaction, create_transaction_with_out_point,
+    dao_data,
+};
