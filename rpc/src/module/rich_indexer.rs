4b3522af730634148eb0afac1920a89ec49bbf21
EthanYuan <yuanchengxing@hotmail.com>
2024-01-31 22:27:13 +0800

    update rpc doc.

--- rpc/src/module/rich_indexer.rs
+++ rpc/src/module/rich_indexer.rs
@@ -8,7 +8,7 @@ use ckb_rich_indexer::AsyncRichIndexerHandle;
 use jsonrpc_core::Result;
 use jsonrpc_utils::rpc;
 
-/// RPC Module Indexer.
+/// RPC Module Rich Indexer.
 #[rpc]
 #[async_trait]
 pub trait RichIndexerRpc {
@@ -20,31 +20,46 @@ pub trait RichIndexerRpc {
     ///
     /// ## Examples
     ///
-    /// Request
-    ///
-    /// ```json
-    /// {
-    ///     "id": 2,
-    ///     "jsonrpc": "2.0",
-    ///     "method": "get_rich_indexer_tip"
-    /// }
-    /// ```
-    ///
-    /// Response
-    ///
-    /// ```json
-    /// {
-    ///   "jsonrpc": "2.0",
-    ///   "result": {
-    ///     "block_hash": "0x4959d6e764a2edc6038dbf03d61ebcc99371115627b186fdcccb2161fbd26edc",
-    ///     "block_number": "0x5b513e"
-    ///   },
-    ///   "id": 2
-    /// }
-    /// ```
+    /// Same as CKB Indexer.
     #[rpc(name = "get_indexer_tip")]
     async fn get_indexer_tip(&self) -> Result<Option<IndexerTip>>;
 
+    /// Returns the live cells collection by the lock or type script.
+    ///
+    /// ## Params
+    ///
+    /// * search_key:
+    ///     - script - Script, supports prefix search
+    ///     - script_type - enum, lock | type
+    ///     - script_search_mode - enum, prefix | exact | partial
+    ///     - filter - filter cells by following conditions, all conditions are optional
+    ///          - script: if search script type is lock, filter cells by type script prefix, and vice versa
+    ///          - script_len_range: [u64; 2], filter cells by script len range, [inclusive, exclusive]
+    ///          - output_data: filter cells by output data
+    ///          - output_data_filter_mode: enum, prefix | exact | partial
+    ///          - output_data_len_range: [u64; 2], filter cells by output data len range, [inclusive, exclusive]
+    ///          - output_capacity_range: [u64; 2], filter cells by output capacity range, [inclusive, exclusive]
+    ///          - block_range: [u64; 2], filter cells by block number range, [inclusive, exclusive]
+    ///     - with_data - bool, optional default is `true`, if with_data is set to false, the field of returning cell.output_data is null in the result
+    /// * order: enum, asc | desc
+    /// * limit: result size limit
+    /// * after: pagination parameter, optional
+    ///
+    /// ## Returns
+    ///
+    /// If the number of objects is less than the requested `limit`, it indicates that these are the last page of get_cells.
+    ///
+    /// * objects:
+    ///     - output: the fields of an output cell
+    ///     - output_data: the cell data
+    ///     - out_point: reference to a cell via transaction hash and output index
+    ///     - block_number: the number of the transaction committed in the block
+    ///     - tx_index: the position index of the transaction committed in the block
+    /// * last_cursor: pagination parameter
+    ///
+    /// ## Examples
+    ///
+    /// Same as CKB Indexer.
     #[rpc(name = "get_cells")]
     async fn get_cells(
         &self,
@@ -54,6 +69,46 @@ pub trait RichIndexerRpc {
         after: Option<JsonBytes>,
     ) -> Result<IndexerPagination<IndexerCell>>;
 
+    /// Returns the transactions collection by the lock or type script.
+    ///
+    /// * search_key:
+    ///     - script - Script, supports prefix search when group_by_transaction is false
+    ///     - script_type - enum, lock | type
+    ///     - script_search_mode - enum, prefix | exact | partial
+    ///     - filter - filter cells by following conditions, all conditions are optional
+    ///         - script: if search script type is lock, filter cells by type script, and vice versa
+    ///         - script_len_range: [u64; 2], filter cells by script len range, [inclusive, exclusive]
+    ///         - output_data: filter cells by output data
+    ///         - output_data_filter_mode: enum, prefix | exact | partial
+    ///         - output_data_len_range: [u64; 2], filter cells by output data len range, [inclusive, exclusive]
+    ///         - output_capacity_range: [u64; 2], filter cells by output capacity range, [inclusive, exclusive]
+    ///         - block_range: [u64; 2], filter cells by block number range, [inclusive, exclusive]
+    ///     - group_by_transaction - bool, optional default is `false`, if group_by_transaction is set to true, the returning objects will be grouped by the tx hash
+    /// * order: enum, asc | desc
+    /// * limit: result size limit
+    /// * after: pagination parameter, optional
+    ///
+    /// ## Returns
+    ///
+    /// If the number of objects is less than the requested `limit`, it indicates that these are the last page of get_transactions.
+    ///
+    ///  * objects - enum, ungrouped TxWithCell | grouped TxWithCells
+    ///     - TxWithCell:
+    ///         - tx_hash: transaction hash,
+    ///         - block_number: the number of the transaction committed in the block
+    ///         - tx_index: the position index of the transaction committed in the block
+    ///         - io_type: enum, input | output
+    ///         - io_index: the position index of the cell in the transaction inputs or outputs
+    ///     - TxWithCells:
+    ///         - tx_hash: transaction hash,
+    ///         - block_number: the number of the transaction committed in the block
+    ///         - tx_index: the position index of the transaction committed in the block
+    ///         - cells: Array [[io_type, io_index]]
+    ///  * last_cursor - pagination parameter
+    ///
+    /// ## Examples
+    ///
+    /// Same as CKB Indexer.
     #[rpc(name = "get_transactions")]
     async fn get_transactions(
         &self,
@@ -63,6 +118,32 @@ pub trait RichIndexerRpc {
         after: Option<JsonBytes>,
     ) -> Result<IndexerPagination<IndexerTx>>;
 
+    /// Returns the live cells capacity by the lock or type script.
+    ///
+    /// ## Parameters
+    ///
+    /// * search_key:
+    ///     - script - Script
+    ///     - script_type - enum, lock | type
+    ///     - script_search_mode - enum, prefix | exact | partial
+    ///     - filter - filter cells by following conditions, all conditions are optional
+    ///         - script: if search script type is lock, filter cells by type script prefix, and vice versa
+    ///         - script_len_range: [u64; 2], filter cells by script len range, [inclusive, exclusive]
+    ///         - output_data: filter cells by output data
+    ///         - output_data_filter_mode: enum, prefix | exact | partial
+    ///         - output_data_len_range: [u64; 2], filter cells by output data len range, [inclusive, exclusive]
+    ///         - output_capacity_range: [u64; 2], filter cells by output capacity range, [inclusive, exclusive]
+    ///         - block_range: [u64; 2], filter cells by block number range, [inclusive, exclusive]
+    ///
+    /// ## Returns
+    ///
+    ///  * capacity - total capacity
+    ///  * block_hash - indexed tip block hash
+    ///  * block_number - indexed tip block number
+    ///
+    /// ## Examples
+    ///
+    /// Same as CKB Indexer.
     #[rpc(name = "get_cells_capacity")]
     async fn get_cells_capacity(
         &self,
