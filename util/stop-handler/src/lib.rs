3ad9b8977514a2b13393a286b9b532e00243b836
Eval EXEC <execvy@gmail.com>
2024-02-18 10:35:21 +0800

    Add has_received_stop_signal function to check if the ckb process has received stop signal

--- util/stop-handler/src/lib.rs
+++ util/stop-handler/src/lib.rs
@@ -1,8 +1,8 @@
 //! TODO(doc): @keroro520
 
 pub use stop_register::{
-    broadcast_exit_signals, new_crossbeam_exit_rx, new_tokio_exit_rx, register_thread,
-    wait_all_ckb_services_exit,
+    broadcast_exit_signals, has_received_stop_signal, new_crossbeam_exit_rx, new_tokio_exit_rx,
+    register_thread, wait_all_ckb_services_exit,
 };
 
 pub use tokio_util::sync::CancellationToken;
