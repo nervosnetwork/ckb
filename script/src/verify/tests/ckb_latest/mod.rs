da968bef2122f620a3cb1284ab518a0b93a02436
Mohanson <mohanson@outlook.com>
2024-03-06 15:12:43 +0800

    Enable 2019/2021 test cases (#9)

--- script/src/verify/tests/ckb_latest/mod.rs
+++ script/src/verify/tests/ckb_latest/mod.rs
@@ -1,6 +1,5 @@
 const SCRIPT_VERSION: crate::ScriptVersion = crate::ScriptVersion::latest();
 
-// TODO: enable it again
-// mod features_since_v2019;
-// mod features_since_v2021;
+mod features_since_v2019;
+mod features_since_v2021;
 mod features_since_v2023;
