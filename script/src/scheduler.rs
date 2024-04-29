f9a7c176e0530bd4ede08e91540fdf2992cab19e
mohanson <mohanson@outlook.com>
2024-04-29 09:27:49 +0800

    Replace + with checked_add

--- script/src/scheduler.rs
+++ script/src/scheduler.rs
@@ -438,7 +438,7 @@ where
                     let copy_length = u64::min(full_length, real_length);
                     for i in 0..copy_length {
                         let fd = inherited_fd[i as usize].0;
-                        let addr = buffer_addr + i * 8;
+                        let addr = buffer_addr.checked_add(i * 8).ok_or(Error::MemOutOfBound)?;
                         machine
                             .machine
                             .inner_mut()
