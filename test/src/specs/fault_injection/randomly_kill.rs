e48d1a1ed7de62729a314e9c2beda9d5c1eb57f6
Eval EXEC <execvy@gmail.com>
2024-08-29 21:19:04 +0800

    test

--- test/src/specs/fault_injection/randomly_kill.rs
+++ test/src/specs/fault_injection/randomly_kill.rs
@@ -11,8 +11,23 @@ impl Spec for RandomlyKill {
     fn run(&self, nodes: &mut Vec<Node>) {
         let mut rng = thread_rng();
         let node = &mut nodes[0];
-        for _ in 0..rng.gen_range(10..20) {
-            node.rpc_client().wait_rpc_ready();
+        let max_restart_times = rng.gen_range(10..20);
+
+        let mut node_crash_times = 0;
+
+        let mut randomly_kill_times = 0;
+        while randomly_kill_times < max_restart_times {
+            node.rpc_client().wait_rpc_ready_internal(|| {});
+
+            if !node.is_alive() {
+                node.start();
+                node_crash_times += 1;
+
+                if node_crash_times > 3 {
+                    panic!("Node crash too many times");
+                }
+            }
+
             let n = rng.gen_range(0..10);
             // TODO: the kill of child process and mining are actually sequential here
             // We need to find some way to so these two things in parallel.
@@ -25,6 +40,7 @@ impl Spec for RandomlyKill {
             }
             info!("Stop the node");
             node.stop_gracefully();
+            randomly_kill_times += 1;
             info!("Start the node");
             node.start();
         }
