bff4ada9123075d4dd09db6fc68eca897d8c3097
zhangsoledad <787953403@qq.com>
2025-02-19 17:00:57 +0800

    chore: 2nd hardfork mainnet activation params

--- util/constant/src/hardfork/mainnet.rs
+++ util/constant/src/hardfork/mainnet.rs
@@ -7,5 +7,8 @@ pub const RFC0028_RFC0032_RFC0033_RFC0034_START_EPOCH: u64 = 5414;
 // pub const CKB2021_START_EPOCH: u64 = 5414;
 pub const CKB2021_START_EPOCH: u64 = 0;
 
-/// hardcode ckb2023 epoch
-pub const CKB2023_START_EPOCH: u64 = u64::MAX;
+/// 2025-07-01 06:32:53 utc
+/// |            hash                                                    |  number   |    timestamp    | epoch |
+/// | 0xf959f70e487bc3073374d148ef0df713e6060542b84d89a3318bf18edbacdf94 | 15,414,776  |  1739773973982  |  11,489 (1800/1800)|
+/// 1739773973982 + 804 * (4 * 60 * 60 * 1000) = 1751351573982  2024-10-25 05:43 utc
+pub const CKB2023_START_EPOCH: u64 = 12_293;
