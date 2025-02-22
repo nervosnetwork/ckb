567079c34c9ba295045a1e3c791ef506e2d71c80
Eval EXEC <execvy@gmail.com>
2025-02-22 18:39:46 +0800

    clippy: fix precedence and apply clippy suggests to add parentheses

--- shared/src/block_status.rs
+++ shared/src/block_status.rs
@@ -8,9 +8,9 @@ bitflags! {
         const UNKNOWN                 =     0;
 
         const HEADER_VALID            =     1;
-        const BLOCK_RECEIVED          =     1 | Self::HEADER_VALID.bits << 1;
-        const BLOCK_STORED            =     1 | Self::BLOCK_RECEIVED.bits << 1;
-        const BLOCK_VALID             =     1 | Self::BLOCK_STORED.bits << 1;
+        const BLOCK_RECEIVED          =     1 | (Self::HEADER_VALID.bits << 1);
+        const BLOCK_STORED            =     1 | (Self::BLOCK_RECEIVED.bits << 1);
+        const BLOCK_VALID             =     1 | (Self::BLOCK_STORED.bits << 1);
 
         const BLOCK_INVALID           =     1 << 12;
     }
