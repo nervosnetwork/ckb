5faebe7c4e17b1f82b4ef2586bdaea3bc725772d
ian <ian@nervos.org>
2025-06-06 10:46:39 +0000

    chore: update assume valid target

--- util/constant/src/latest_assume_valid_target.rs
+++ util/constant/src/latest_assume_valid_target.rs
@@ -8,12 +8,12 @@ pub mod mainnet {
     ///
     /// Need to update when CKB's new release
     /// in mainnet: the 60 days ago block is:
-    /// height: 15190307
-    /// hash: 0xaf83c61fc14e6aa49111e18e3c466cf0923dfe1f4c8e91ab8c853625fb745c4c
-    /// date: Wed Jan 22 11:30:02 AM CST 2025
-    /// you can view this block in https://explorer.nervos.org/block/0xaf83c61fc14e6aa49111e18e3c466cf0923dfe1f4c8e91ab8c853625fb745c4c
+    /// height: 15836361
+    /// hash: 0x404b0dde051c49ea989fbc85c86aac6aba0f9ce38f5cdbfdec23493fb8b52e80
+    /// date: Sun 06 Apr 2025 09:48:35 AM UTC
+    /// you can view this block in https://explorer.nervos.org/block/0x404b0dde051c49ea989fbc85c86aac6aba0f9ce38f5cdbfdec23493fb8b52e80
     pub const DEFAULT_ASSUME_VALID_TARGET: &str =
-        "0xaf83c61fc14e6aa49111e18e3c466cf0923dfe1f4c8e91ab8c853625fb745c4c";
+        "0x404b0dde051c49ea989fbc85c86aac6aba0f9ce38f5cdbfdec23493fb8b52e80";
 }
 /// sync config related to testnet
 pub mod testnet {
@@ -21,10 +21,10 @@ pub mod testnet {
     ///
     /// Need to update when CKB's new release
     /// in testnet: the 60 days ago block is:
-    /// height: 15974951
-    /// hash: 0x60dce52f616ca2d98f5e97727cac16a9641e7ba388127904e993152ca51dbcb9
-    /// date: Wed Jan 22 11:31:10 AM CST 2025
-    /// you can view this block in https://pudge.explorer.nervos.org/block/0x60dce52f616ca2d98f5e97727cac16a9641e7ba388127904e993152ca51dbcb9
+    /// height: 16776647
+    /// hash: 0xe68d17c4c2b5f5fba7b9af2875d40f2da4b506e9b46930e9774f18a3d9b79381
+    /// date: Sun 06 Apr 2025 09:48:40 AM UTC
+    /// you can view this block in https://pudge.explorer.nervos.org/block/0xe68d17c4c2b5f5fba7b9af2875d40f2da4b506e9b46930e9774f18a3d9b79381
     pub const DEFAULT_ASSUME_VALID_TARGET: &str =
-        "0x60dce52f616ca2d98f5e97727cac16a9641e7ba388127904e993152ca51dbcb9";
+        "0xe68d17c4c2b5f5fba7b9af2875d40f2da4b506e9b46930e9774f18a3d9b79381";
 }
