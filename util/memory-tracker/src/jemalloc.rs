557ddfec40a81b93a281c1e868400acb5f0fc93e
Eval Exec <execvy@gmail.com>
2026-07-24 17:09:29 +0800

    fix: correct jemalloc profiling dump FFI (#5291)

--- util/memory-tracker/src/jemalloc.rs
+++ util/memory-tracker/src/jemalloc.rs
@@ -1,5 +1,5 @@
 use ckb_logger::info;
-use std::{ffi, mem, ptr};
+use std::{ffi, io, mem, ptr};
 
 /// Dumps the heap through Jemalloc's API.
 ///
@@ -9,19 +9,28 @@ use std::{ffi, mem, ptr};
 ///
 /// [Jemallocator]: https://docs.rs/jemallocator/*/jemallocator/index.html
 pub fn jemalloc_profiling_dump(filename: &str) -> Result<(), String> {
-    let mut filename0 = format!("{filename}\0");
+    let filename_c = ffi::CString::new(filename)
+        .map_err(|err| format!("invalid jemalloc profiling dump filename: {err}"))?;
+    let mut filename_ptr = filename_c.as_ptr();
     let opt_name = "prof.dump";
     let opt_c_name = ffi::CString::new(opt_name).unwrap();
     info!("jemalloc profiling dump: {filename}");
-    unsafe {
+    let result = unsafe {
         jemalloc_sys::mallctl(
             opt_c_name.as_ptr(),
             ptr::null_mut(),
             ptr::null_mut(),
-            &mut filename0 as *mut _ as *mut _,
-            mem::size_of::<*mut ffi::c_void>(),
-        );
-    }
+            ptr::from_mut(&mut filename_ptr).cast(),
+            mem::size_of_val(&filename_ptr),
+        )
+    };
 
-    Ok(())
+    if result == 0 {
+        Ok(())
+    } else {
+        Err(format!(
+            "jemalloc profiling dump failed: {}",
+            io::Error::from_raw_os_error(result)
+        ))
+    }
 }
