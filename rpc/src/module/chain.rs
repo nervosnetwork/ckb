63588a6a3e81a2ba41f524c8312d4bced5400259
zhangsoledad <787953403@qq.com>
2026-09-24 19:41:23 +0800

    refactor(rpc): propagate transaction lookup errors directly
    Remove redundant unwrap branches while retaining distinct transport and lookup error logs.

--- rpc/src/module/chain.rs
+++ rpc/src/module/chain.rs
@@ -2220,18 +2220,16 @@ impl ChainRpcImpl {
         }
 
         let tx_pool = self.shared.tx_pool_controller();
-        let tx_status = tx_pool.get_tx_status(tx_hash);
-        if let Err(e) = tx_status {
-            error!("Send get_tx_status request error {}", e);
-            return Err(RPCError::ckb_internal_error(e));
-        };
-        let tx_status = tx_status.unwrap();
-
-        if let Err(e) = tx_status {
-            error!("get_tx_status from db error {}", e);
-            return Err(RPCError::ckb_internal_error(e));
-        };
-        let (tx_status, cycles) = tx_status.unwrap();
+        let (tx_status, cycles) = tx_pool
+            .get_tx_status(tx_hash)
+            .map_err(|e| {
+                error!("Send get_tx_status request error {}", e);
+                RPCError::ckb_internal_error(e)
+            })?
+            .map_err(|e| {
+                error!("get_tx_status from db error {}", e);
+                RPCError::ckb_internal_error(e)
+            })?;
         Ok(TransactionWithStatus::omit_transaction(tx_status, cycles))
     }
 
@@ -2269,19 +2267,16 @@ impl ChainRpcImpl {
         }
 
         let tx_pool = self.shared.tx_pool_controller();
-        let transaction_with_status = tx_pool.get_transaction_with_status(tx_hash);
-        if let Err(e) = transaction_with_status {
-            error!("Send get_transaction_with_status request error {}", e);
-            return Err(RPCError::ckb_internal_error(e));
-        };
-        let transaction_with_status = transaction_with_status.unwrap();
-
-        if let Err(e) = transaction_with_status {
-            error!("Get transaction_with_status from db error {}", e);
-            return Err(RPCError::ckb_internal_error(e));
-        };
-        let transaction_with_status = transaction_with_status.unwrap();
-        Ok(transaction_with_status)
+        tx_pool
+            .get_transaction_with_status(tx_hash)
+            .map_err(|e| {
+                error!("Send get_transaction_with_status request error {}", e);
+                RPCError::ckb_internal_error(e)
+            })?
+            .map_err(|e| {
+                error!("Get transaction_with_status from db error {}", e);
+                RPCError::ckb_internal_error(e)
+            })
     }
 
     fn get_block_by_hash(
